7522ae0fcaf66450541ee1be00e4033ab593cc99
HsuTing <hsuting0106@gmail.com>
2018-06-14 13:56:47 +0800

    test(packages/eslint-config-cat): fix test of jsDoc

--- packages/eslint-config-cat/src/__tests__/__ignore__/jsDoc.js
+++ packages/eslint-config-cat/src/__tests__/__ignore__/jsDoc.js
@@ -9,31 +9,82 @@
 */
 const correctFunction = (argu: string): number => 10;
 
+// $expectError jsdoc/newline-after-description
 // $expectError jsdoc/require-example
 /**
+ * desc
  * @param {number} argu - example argu
  * @return {10} - example return
 */
 const noExample = (argu: string): number => 10;
 
+// $expectError jsdoc/check-tag-names
 // $expectError jsdoc/require-param
-// $expectError valid-jsdoc
 /**
  * @example
  * correctFunction('test');
  *
+ * @Param {string} argu - example argu
  * @return {10} - example return
 */
 const noParams = (argu: string): number => 10;
 
+// $expectError jsdoc/require-hyphen-before-param-description
 // $expectError valid-jsdoc
 /**
  * @example
  * correctFunction('test');
  *
- * @param {number} argu - example argu
+ * @param {number} argu example argu
 */
 const noReturns = (argu: string): number => 10;
+
+// $expectError jsdoc/check-param-names
+// $expectError valid-jsdoc
+/**
+ * @example
+ * checkParamNames();
+ *
+ * @param {string} Argu - example argu
+*/
+const checkParamNames = (argu: string) => {};
+
+// $expectError jsdoc/check-types
+/**
+ * @example
+ * checkTypes('test');
+ *
+ * @param {Number} argu - example argu
+ * @return {10} - example return
+*/
+const checkTypes = (argu: string): number => 10;
+
+// $expectError jsdoc/check-param-names
+// $expectError jsdoc/require-param
+// $expectError jsdoc/require-param-description
+// $expectError jsdoc/require-param-name
+// $expectError jsdoc/require-param-type
+// $expectError valid-jsdoc
+/**
+ * @example
+ * requireParamName('test');
+ *
+ * @param
+ * @return {10} - example return
+*/
+const requireParam = (argu: string): number => 10;
+
+// $expectError jsdoc/require-returns-description
+// $expectError jsdoc/require-returns-type
+// $expectError valid-jsdoc
+/**
+ * @example
+ * requireReturn('test');
+ *
+ * @param {number} argu - example argu
+ * @return
+*/
+const requireReturn = (argu: string): number => 10;
 
 // $expectError require-jsdoc
 const functionDeclarationRequiredJsDoc = (): number => 10;

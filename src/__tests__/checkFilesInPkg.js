a39e9b60b2d53276071218b89c800f9bafa9e262
HsuTing <hsuting0106@gmail.com>
2018-06-23 14:02:48 +0800

    test(src/__tests__): check files in packages
    avoid unused files in packages

---
+++ src/__tests__/checkFilesInPkg.js
@@ -0,0 +1,32 @@
+// @flow
+
+import path from 'path';
+
+import d3DirTree from '../../packages/utils/lib/d3DirTree';
+// eslint-disable-next-line max-len
+import type { d3DirTreeType } from '../../packages/utils/lib/definitions/d3DirTree.js.flow';
+
+const packages = d3DirTree(path.resolve(__dirname, './../../packages'));
+
+describe('check files in packages', () => {
+  packages
+    .children
+    .forEach(({ data, children }: d3DirTreeType) => {
+      const { name } = data;
+
+      it(name, () => {
+        const files = children
+          .map(({ data: childData }: d3DirTreeType): string => childData.name)
+          .sort();
+
+        expect(files)
+          .toEqual([
+            '.npmignore',
+            'lib',
+            'node_modules',
+            'package.json',
+            'src',
+          ]);
+      });
+    });
+});

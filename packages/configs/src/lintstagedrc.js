784121900ea6298716957bf73736fb5149f93c5e
HsuTing <hsuting0106@gmail.com>
2018-06-24 01:53:58 +0800

    feat(packages/configs): add parser to prettier

--- packages/configs/src/lintstagedrc.js
+++ packages/configs/src/lintstagedrc.js
@@ -2,5 +2,10 @@
 
 export default {
   '*.js': ['yarn prettier --write', 'git add'],
-  'package.json': ['yarn prettier-package-json --write', 'git add'],
+  'package.json': [
+    'yarn prettier-package-json --write',
+    'yarn prettier --parser json --write',
+    'git add',
+  ],
+  '*.md': ['yarn prettier --parser markdown --write', 'git add'],
 };

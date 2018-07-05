9b9ecbbec6a033d482074d0111bd0e13a13247cc
HsuTing <hsuting0106@gmail.com>
2018-07-06 00:58:10 +0800

    perf(utils): add more type definitions with d3DirTree

--- packages/utils/src/d3DirTree.js
+++ packages/utils/src/d3DirTree.js
@@ -3,18 +3,20 @@
 import dirTree from 'directory-tree';
 import * as d3 from 'd3-hierarchy';
 
-// eslint-disable-next-line max-len
-import type { d3DirTreeNodeType as _d3DirTreeNodeType } from './definitions/d3DirTree.js.flow';
+import type {
+  d3DirTreeNodeType as _d3DirTreeNodeType,
+  d3DirTreeOptionsType as _d3DirTreeOptionsType,
+  d3DirTreeType as _d3DirTreeType,
+} from './definitions/d3DirTree.js.flow';
 
+/* eslint-disable import/group-exports */
 export type d3DirTreeNodeType = _d3DirTreeNodeType;
+export type d3DirTreeOptionsType = _d3DirTreeOptionsType;
+export type d3DirTreeType = _d3DirTreeType;
+/* eslint-enable import/group-exports */
 
-export default (
+export default ((
   filePath: string,
-  options?: {|
-    normalizePath?: (path: string) => string,
-    // eslint-disable-next-line flowtype/no-mutable-array
-    exclude?: RegExp | Array<RegExp>,
-    // eslint-disable-next-line flowtype/no-mutable-array
-    extensions?: RegExp,
-  |},
-): d3DirTreeNodeType => d3.hierarchy(dirTree(filePath, options));
+  options?: d3DirTreeOptionsType,
+): d3DirTreeNodeType =>
+  d3.hierarchy(dirTree(filePath, options)): d3DirTreeType);

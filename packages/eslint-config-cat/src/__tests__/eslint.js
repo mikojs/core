93092a4d0ca7606bfaabc5991820e3d2b68d6c83
HsuTing <hsuting0106@gmail.com>
2018-07-08 00:52:37 +0800

    perf(utils): remove export type in d3DirTree

--- packages/eslint-config-cat/src/__tests__/eslint.js
+++ packages/eslint-config-cat/src/__tests__/eslint.js
@@ -7,6 +7,7 @@ import { CLIEngine } from 'eslint';
 import { hyphenate } from 'fbjs';
 
 import { d3DirTree } from '@cat-org/utils';
+import type { d3DirTreeNodeType } from '@cat-org/utils/lib/d3DirTree';
 
 import configs from '..';
 
@@ -51,17 +52,7 @@ const files = d3DirTree(root, {
 const ruleIds = [];
 
 const testData = files.map(
-  ({
-    data,
-  }: {
-    data: {|
-      path: string,
-      name: string,
-      size: number,
-      type: 'directory' | 'file',
-      extension: mixed,
-    |},
-  }): testDataType => {
+  ({ data }: d3DirTreeNodeType): testDataType => {
     const { path: filePath, name } = data;
 
     const { messages = [] } =
--- packages/utils/src/d3DirTree.js
+++ packages/utils/src/d3DirTree.js
@@ -4,16 +4,10 @@ import dirTree from 'directory-tree';
 import * as d3 from 'd3-hierarchy';
 
 import type {
-  d3DirTreeNodeType as _d3DirTreeNodeType,
-  d3DirTreeOptionsType as _d3DirTreeOptionsType,
-  d3DirTreeType as _d3DirTreeType,
+  d3DirTreeNodeType,
+  d3DirTreeOptionsType,
+  d3DirTreeType,
 } from './definitions/d3DirTree.js.flow';
-
-/* eslint-disable import/group-exports */
-export type d3DirTreeNodeType = _d3DirTreeNodeType;
-export type d3DirTreeOptionsType = _d3DirTreeOptionsType;
-export type d3DirTreeType = _d3DirTreeType;
-/* eslint-enable import/group-exports */
 
 export default ((
   filePath: string,

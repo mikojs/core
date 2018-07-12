135c223fb0968f9a6a620e09beb98fd4d8eae73e
HsuTing <hsuting0106@gmail.com>
2018-07-12 13:21:10 +0800

    refactor(root): remove modifyPackagesPkg

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -9,11 +9,4 @@ lintStagedConfigs['package.json'].splice(
   'node ./lib/bin/copyDependencies',
 );
 
-// copy dependencies before modifying package.json
-lintStagedConfigs['package.json'].splice(
-  0,
-  0,
-  'node ./lib/bin/modifyPackagesPkg',
-);
-
 module.exports = lintStagedConfigs;

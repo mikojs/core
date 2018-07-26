eddeaf7f66946495da919a6c8c6afbb77ddcd7fd
HsuTing <hsuting0106@gmail.com>
2018-07-26 12:47:15 +0800

    chore(babel-plugin-transform-flow): remove __tests__

--- packages/babel-plugin-transform-flow/src/__tests__/__ignore__/src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import type { testType } from './definitions/index.js.flow';
-import type { notCheckFlowFileType } from './definitions/notCheckFlowFile.js.flow';
-
-('test': testType);
-('no check flow file': notCheckFlowFileType);
-
-export default 'test';

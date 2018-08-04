10993294612cfcd5214281785f41ed64fd8c59ac
HsuTing <hsuting0106@gmail.com>
2018-08-04 23:50:23 +0800

    test(babel-plugin-transform-flow): checking files with transforming folder

--- packages/babel-plugin-transform-flow/src/__tests__/__ignore__/constants.js
+++ packages/babel-plugin-transform-flow/src/__tests__/__ignore__/constants.js
@@ -1,4 +1,5 @@
 // @flow
 
 export const indexFiles = ['lib/justDefinition.js.flow', 'lib/index.js.flow'];
+export const hasFlowFileFiles = ['lib/hasFlowFile.js.flow'];
 export default null;
--- packages/babel-plugin-transform-flow/src/__tests__/transformFolder.js
+++ packages/babel-plugin-transform-flow/src/__tests__/transformFolder.js
@@ -1,27 +1,39 @@
 // @flow
 
+import path from 'path';
+
+import { resetDestPaths, getDestPaths } from 'output-file-sync';
+
 import reset from './__ignore__/reset';
 import babel from './__ignore__/babel';
+import { indexFiles, hasFlowFileFiles } from './__ignore__/constants';
 
 describe('transform folder', () => {
   beforeEach(() => {
     reset({
-      filenames: ['not use, just mock'],
+      filenames: [
+        path
+          .resolve(__dirname, './__ignore__/files')
+          .replace(process.cwd(), '.'),
+      ],
       outDir: 'lib',
     });
+    resetDestPaths();
   });
 
   it('transform file which do not have the same name file and extension is `.js.flow`', () => {
-    // TODO should check with file-output-sync
     babel();
-    // TODO should test not write again
+    expect(getDestPaths()).toEqual(indexFiles);
+
     babel();
+    expect(getDestPaths()).toEqual([...indexFiles, 'lib/index.js.flow']);
   });
 
   it('transform file which have the same name file and extension is `.js.flow`', () => {
-    // TODO should check with file-output-sync
     babel('hasFlowFile.js');
-    // TODO should test not write again
+    expect(getDestPaths()).toEqual(hasFlowFileFiles);
+
     babel('hasFlowFile.js');
+    expect(getDestPaths()).toEqual(hasFlowFileFiles);
   });
 });

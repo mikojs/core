bbebaf941cd3a45b282fa6dbd6658f63e9a016b8
HsuTing <hsuting0106@gmail.com>
2018-11-28 01:35:00 +0800

    feat(create-app): add constants cache

--- packages/create-app/src/caches/pkg.js
+++ packages/create-app/src/caches/pkg.js
@@ -7,6 +7,7 @@ import inquirer from 'inquirer';
 import chalk from 'chalk';
 import { isURL } from 'validator';
 
+import constants from './constants';
 import user from './user';
 import Cache from './index';
 
@@ -121,28 +122,34 @@ class Pkg extends Cache<storeType> {
    * @param {string} projectDir - project dir path
    */
   init = async (projectDir: string): Promise<void> => {
+    // store name
     this.store.name = path.basename(projectDir);
 
     // store engines
-    const { Binaries } = JSON.parse(
-      await envinfo.run(
-        {
-          Binaries: ['Node', 'Yarn', 'npm'],
-        },
-        { json: true },
-      ),
-    );
+    const { useLerna } = await constants.get(projectDir);
 
-    this.store.engines = Object.keys(Binaries)
-      .filter((key: string) => Binaries[key])
-      .reduce(
-        (result: {}, key: string) => ({
-          ...result,
-          [key.toLowerCase()]: `>= ${Binaries[key].version}`,
-        }),
-        {},
+    if (!useLerna) {
+      const { Binaries } = JSON.parse(
+        await envinfo.run(
+          {
+            Binaries: ['Node', 'Yarn', 'npm'],
+          },
+          { json: true },
+        ),
       );
 
+      this.store.engines = Object.keys(Binaries)
+        .filter((key: string) => Binaries[key])
+        .reduce(
+          (result: {}, key: string) => ({
+            ...result,
+            [key.toLowerCase()]: `>= ${Binaries[key].version}`,
+          }),
+          {},
+        );
+    }
+
+    // store author
     const { username, email } = await user.get(projectDir);
 
     this.store.author = `${username} <${email}>`;

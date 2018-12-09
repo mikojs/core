a0842f1f162c4613dcee94707f7317533f50dbf2
HsuTing <hsuting0106@gmail.com>
2018-12-09 22:25:57 +0800

    perf(lerna-create): move cli lerna-create to new project

---
+++ packages/lerna-create/src/utils/cliOptions.js
@@ -0,0 +1,40 @@
+// @flow
+
+import commander from 'commander';
+import chalk from 'chalk';
+import debug from 'debug';
+
+import { version } from '../../package.json';
+
+import logger from './logger';
+
+const debugLog = debug('lerna-create:cliOptions');
+
+export default (argv: $ReadOnlyArray<string>): {} => {
+  const program = new commander.Command('lerna-create')
+    .version(version, '-v, --version')
+    .arguments('<existing project, new project>')
+    .usage(chalk`{green <existing project, new project>}`)
+    .description(
+      chalk`Example:
+  lerna-create {green existing-project new-project}`,
+    );
+
+  const {
+    args: [existingProject, newProject],
+  } = program.parse([...argv]);
+  const cliOptions = {
+    existingProject,
+    newProject,
+  };
+
+  if (!existingProject || !newProject)
+    logger.fail(
+      chalk`Must give {green existing project} and {green new project}`,
+      chalk`Use {green \`--info\`} to get the more information`,
+    );
+
+  debugLog(cliOptions);
+
+  return cliOptions;
+};

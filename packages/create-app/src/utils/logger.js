7999ac6407f9df00d5011d91d2f1c8d546ea1621
HsuTing <hsuting0106@gmail.com>
2018-12-11 15:10:52 +0800

    feat(create-app): add default utils

---
+++ packages/create-app/src/utils/logger.js
@@ -0,0 +1,21 @@
+// @flow
+
+import { mockChoice } from '@cat-org/utils';
+import logger, { findSettings } from '@cat-org/logger';
+
+const oraSettings = findSettings('ora');
+const { error } = console;
+
+oraSettings.fail.after = () => {
+  error();
+  mockChoice(
+    process.env.NODE_ENV === 'test',
+    () => {
+      throw new Error('process exit');
+    },
+    process.exit,
+    1,
+  );
+};
+
+export default logger('@cat-org/create-app', oraSettings).init();

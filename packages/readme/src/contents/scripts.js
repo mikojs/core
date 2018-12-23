64412fb653c4fbeb6d83b9ec62b01afd29ee37a2
HsuTing <hsuting0106@gmail.com>
2018-12-23 10:07:29 +0800

    chore(readme): rewrite readme

--- packages/readme/src/contents/scripts.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import type { ctxType } from './index';
-
-export default ({ pkg: { scripts } }: ctxType) =>
-  Object.keys(scripts)
-    .map((key: string) => `- ${key}`)
-    .join('\n');

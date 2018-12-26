94e1e22d183864be9cd1609d8f133a27698d012c
HsuTing <hsuting0106@gmail.com>
2018-12-26 10:38:26 +0800

    feat(badges): can read more readme path with cliOptions

---
+++ packages/badges/src/utils/cliOptions.js
@@ -0,0 +1,34 @@
+// @flow
+
+import commander from 'commander';
+import chalk from 'chalk';
+import debug from 'debug';
+
+import { version } from '../../package.json';
+
+import logger from './logger';
+
+const debugLog = debug('badges:cliOptions');
+
+export default (argv: $ReadOnlyArray<string>): $ReadOnlyArray<string> => {
+  const program = new commander.Command('badges')
+    .version(version, '-v, --version')
+    .arguments('[readme path]')
+    .usage(chalk`{green [readme path]}`)
+    .description(
+      chalk`Example:
+  badges {green readme path}`,
+    );
+
+  const { args } = program.parse([...argv]);
+
+  if (args.length === 0)
+    logger.fail(
+      chalk`Must give {green readme path}`,
+      chalk`Use {green \`--help\`} to get the more information`,
+    );
+
+  debugLog(args);
+
+  return args;
+};

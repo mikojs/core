37ff910ec8bbcd8acf0762ebd4b337d1c2f968ab
HsuTing <hsuting0106@gmail.com>
2019-01-03 11:29:01 +0800

    fix(core): remove not needing functions

--- packages/badges/src/utils/logger.js
+++ packages/badges/src/utils/logger.js
@@ -1,21 +1,5 @@
 // @flow
 
-import { mockChoice } from '@cat-org/utils';
-import logger, { findSettings } from '@cat-org/logger';
+import logger from '@cat-org/logger';
 
-const logSettings = findSettings('log');
-const { error } = console;
-
-logSettings.fail.after = () => {
-  error();
-  mockChoice(
-    process.env.NODE_ENV === 'test',
-    () => {
-      throw new Error('process exit');
-    },
-    process.exit,
-    1,
-  );
-};
-
-export default logger('@cat-org/badges', logSettings);
+export default logger('@cat-org/badges');

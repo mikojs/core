e1d15f500a17345ef62a03345a7c96bcb8b6fc5b
HsuTing <hsuting0106@gmail.com>
2019-01-09 01:38:13 +0800

    style(core): rewrite checkFilesInPackages testing

--- __tests__/checkFilesInPackages.js
+++ __tests__/checkFilesInPackages.js
@@ -6,58 +6,54 @@ import { d3DirTree } from '@cat-org/utils';
 import { type d3DirTreeNodeType } from '@cat-org/utils/lib/d3DirTree';
 
 describe('check files in packages', () => {
-  [
-    ...d3DirTree(path.resolve(__dirname, '../packages'), {
+  ['packages', 'server'].forEach((packagesName: string) => {
+    d3DirTree(path.resolve(__dirname, '..', packagesName), {
       exclude: [
         /node_modules/,
         ...(process.env.TEST_PRODUCTION ? [] : [/lib/]),
       ],
-    }).children,
-    ...d3DirTree(path.resolve(__dirname, '../server'), {
-      exclude: [
-        /node_modules/,
-        ...(process.env.TEST_PRODUCTION ? [] : [/lib/]),
-      ],
-    }).children,
-  ].forEach(
-    ({ data: { name, path: filePath }, children }: d3DirTreeNodeType) => {
-      describe(name, () => {
-        test('files in package root', () => {
-          const files = children
-            .map(
-              ({ data: { name: childName } }: d3DirTreeNodeType): string =>
-                childName,
-            )
-            .filter((folderName: string) => !['templates'].includes(folderName))
-            .sort();
+    }).children.forEach(
+      ({ data: { name, path: filePath }, children }: d3DirTreeNodeType) => {
+        describe(name, () => {
+          test('files in package root', () => {
+            const files = children
+              .map(
+                ({ data: { name: childName } }: d3DirTreeNodeType): string =>
+                  childName,
+              )
+              .filter(
+                (folderName: string) => !['templates'].includes(folderName),
+              )
+              .sort();
 
-          expect(files).toEqual(
-            [
-              '.npmignore',
-              'package.json',
-              'README.md',
-              'src',
-              ...(process.env.TEST_PRODUCTION ? ['lib'] : []),
-            ].sort(),
-          );
+            expect(files).toEqual(
+              [
+                '.npmignore',
+                'package.json',
+                'README.md',
+                'src',
+                ...(process.env.TEST_PRODUCTION ? ['lib'] : []),
+              ].sort(),
+            );
+          });
+
+          // TODO check packages json key
+          // TODO check npm ignore
+          // TODO check pacakges dev not in root package
+
+          switch (name) {
+            case 'babel-plugin-transform-flow':
+              // TODO check babel version
+              break;
+            case 'eslint-config-cat':
+              // TODO check dependices
+              // TODO check eslint version
+              break;
+            default:
+              break;
+          }
         });
-
-        // TODO check packages json key
-        // TODO check npm ignore
-        // TODO check pacakges dev not in root package
-
-        switch (name) {
-          case 'babel-plugin-transform-flow':
-            // TODO check babel version
-            break;
-          case 'eslint-config-cat':
-            // TODO check dependices
-            // TODO check eslint version
-            break;
-          default:
-            break;
-        }
-      });
-    },
-  );
+      },
+    );
+  });
 });

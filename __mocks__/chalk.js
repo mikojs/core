34e9a5d9699ce48993a00eedc0f22f8ee7f4ff2d
HsuTing <hsuting0106@gmail.com>
2019-01-17 00:33:33 +0800

    test(react-middleware): fix testing when using async

--- __mocks__/chalk.js
+++ __mocks__/chalk.js
@@ -1,6 +1,17 @@
 // @flow
 
-export default (
+import { emptyFunction } from 'fbjs';
+
+/**
+ * @example
+ * chalk`{green test}`
+ *
+ * @param {Array} texts - text array
+ * @param {Array} keys - key array
+ *
+ * @return {string} - chalk string
+ */
+const chalk = (
   texts: $ReadOnlyArray<string>,
   ...keys: $ReadOnlyArray<string>
 ): string =>
@@ -9,3 +20,13 @@ export default (
       `${result}${text}${keys[index] || ''}`,
     '',
   );
+
+chalk.blue = emptyFunction.thatReturnsArgument;
+chalk.green = emptyFunction.thatReturnsArgument;
+chalk.yellow = emptyFunction.thatReturnsArgument;
+chalk.red = emptyFunction.thatReturnsArgument;
+chalk.gray = emptyFunction.thatReturnsArgument;
+chalk.cyan = emptyFunction.thatReturnsArgument;
+chalk.grey = emptyFunction.thatReturnsArgument;
+
+export default chalk;

9a4b699a2ece8d962b9a37722da565084d8607d9
HsuTing <hsuting0106@gmail.com>
2019-01-31 15:58:06 +0800

    chore(react-middleware): simplify Main template

--- server/react-middleware/src/templates/Main.js
+++ server/react-middleware/src/templates/Main.js
@@ -2,16 +2,8 @@
 /* eslint-disable require-jsdoc, flowtype/require-return-type */
 // TODO component should be ignored
 
-import React, { type Node as NodeType } from 'react';
+import { type Node as NodeType } from 'react';
 
-type propsType = {|
-  children: NodeType,
-|};
+const Main = ({ children }: {| children: NodeType |}) => children;
 
-export default class Main extends React.PureComponent<propsType> {
-  render() {
-    const { children } = this.props;
-
-    return children;
-  }
-}
+export default Main;

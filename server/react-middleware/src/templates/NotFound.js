5e687960d8993ef7fb8485dcaaf5de3dafee0f17
HsuTing <hsuting0106@gmail.com>
2019-02-27 13:47:43 +0800

    feat(react-middleware): add new not found page

--- server/react-middleware/src/templates/NotFound.js
+++ server/react-middleware/src/templates/NotFound.js
@@ -7,6 +7,8 @@ import { Helmet } from 'react-helmet';
 
 import { type ctxType } from '../types';
 
+import * as styles from './styles/notFound';
+
 export default class NotFound extends React.PureComponent<*> {
   static getInitialProps = ({ ctx }: ctxType<{ status: number }>) => {
     ctx.status = 404;
@@ -21,6 +23,12 @@ export default class NotFound extends React.PureComponent<*> {
   };
 
   render() {
-    return <div>Page not found</div>;
+    return (
+      <div style={styles.root}>
+        <h1 style={styles.h1}>404</h1>
+
+        <h2 style={styles.h2}>Page not found</h2>
+      </div>
+    );
   }
 }

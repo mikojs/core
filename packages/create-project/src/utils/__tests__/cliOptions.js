49476203abb57a57936d491eef50bcc4e54f80af
HsuTing <hsuting0106@gmail.com>
2019-03-26 11:32:42 +0800

    test(create-project): add utils testing

---
+++ packages/create-project/src/utils/__tests__/cliOptions.js
@@ -0,0 +1,21 @@
+// @flow
+
+import path from 'path';
+
+import cliOptions from '../cliOptions';
+
+const defaultArgv = ['node', 'create-project'];
+
+describe('cli options', () => {
+  test('work', () => {
+    expect(cliOptions([...defaultArgv, 'projectDir'])).toEqual({
+      projectDir: path.resolve('projectDir'),
+    });
+  });
+
+  test('no project directory', () => {
+    expect(() => {
+      cliOptions(defaultArgv);
+    }).toThrow('process exit');
+  });
+});
---
+++ packages/create-project/src/utils/__tests__/validateProject.js
@@ -0,0 +1,59 @@
+// @flow
+
+// $FlowFixMe jest mock
+import { fs } from 'fs';
+// $FlowFixMe jest mock
+import { execa } from 'execa';
+import { emptyFunction } from 'fbjs';
+
+import validateProject from '../validateProject';
+
+jest.mock('fs');
+
+describe('validate project', () => {
+  test('project dir pass', async () => {
+    fs.exist = false;
+    execa.mainFunction = () => {
+      const error = new Error('error');
+
+      // eslint-disable-next-line flowtype/no-weak-types
+      (error: any).stderr =
+        'fatal: not a git repository (or any of the parent directories): .git';
+      throw error;
+    };
+
+    expect(await validateProject('project dir')).toBeUndefined();
+  });
+
+  test('project dir exist', async () => {
+    fs.exist = true;
+
+    await expect(validateProject('project dir')).rejects.toThrow(
+      'process exit',
+    );
+  });
+
+  test('in git managed project', async () => {
+    fs.exist = false;
+    execa.mainFunction = emptyFunction;
+
+    await expect(validateProject('project dir')).rejects.toThrow(
+      'process exit',
+    );
+  });
+
+  test('unexpected error', async () => {
+    fs.exist = false;
+    execa.mainFunction = () => {
+      const error = new Error('error');
+
+      // eslint-disable-next-line flowtype/no-weak-types
+      (error: any).stderr = 'unexpected error';
+      throw error;
+    };
+
+    await expect(validateProject('project dir')).rejects.toThrow(
+      'process exit',
+    );
+  });
+});

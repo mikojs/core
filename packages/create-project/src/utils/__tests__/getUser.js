0f6c2248a61b634eb0ecacdbbaf829f817e2db2c
HsuTing <hsuting0106@gmail.com>
2019-03-28 01:17:20 +0800

    test(create-project): remove not needing testing

--- packages/create-project/src/utils/__tests__/getUser.js
+++ packages/create-project/src/utils/__tests__/getUser.js
@@ -5,19 +5,10 @@ import { execa } from 'execa';
 
 import { getUser } from '../getUser';
 
-describe('get user', () => {
-  test('work', async () => {
-    expect(await getUser()).toEqual([
-      'git config --get user.name',
-      'git config --get user.email',
-    ]);
-  });
+test('not set git user', async () => {
+  execa.mainFunction = () => {
+    throw new Error('not set git user');
+  };
 
-  test('not set git user', async () => {
-    execa.mainFunction = () => {
-      throw new Error('not set git user');
-    };
-
-    await expect(getUser()).rejects.toThrow('process exit');
-  });
+  await expect(getUser()).rejects.toThrow('process exit');
 });

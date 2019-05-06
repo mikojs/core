2659201a8ba5784814d6f8b00ed122af2e2b8f0d
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2019-05-06 13:04:59 +0800

    (@cat-org/server) Run babel building with running server (#185)
    * feat(server): run babel building with running server
    
    * fix(server): fix pacakges error
    
    * fix(server): add @babel/runtime
    
    * perf(server): upgrade babel version
    
    * feat(server): fix folder path in react-middleware
    
    * feat(server): should use outDir as folderPath
    
    * fix(server): fix cli error
    
    * fix(server): fix trigger webpack again
    
    * feat(server): add checking loaded modules
    
    * feat(server): add production mode

--- server/server/src/bin/index.js
+++ server/server/src/bin/index.js
@@ -10,25 +10,64 @@
 import path from 'path';
 
 import chokidar from 'chokidar';
-
-import defaultMiddleware from '@cat-org/default-middleware';
-import react from '@cat-org/react-middleware';
+import execa from 'execa';
+import parseArgv from '@babel/cli/lib/babel/options';
 
 import server from '../index';
 
+import loadMiddleware from 'utils/loadMiddleware';
+
 // TODO: add checking should use middleware
 (async () => {
-  chokidar
-    .watch(path.resolve('./src'), {
-      ignoreInitial: true,
-    })
-    .on('change', (filePath: string) => {
-      if (/\.jsx?/.test(filePath)) delete require.cache[filePath];
+  try {
+    const {
+      cliOptions: { outDir },
+    } = parseArgv(process.argv);
+
+    if (!outDir)
+      throw new Error('Must use `--out-dir` or `-d` to build the server');
+
+    const isProduction = process.env.NODE_ENV === 'production';
+    const babelOptions = process.argv
+      .slice(2)
+      .filter((argv: string) => !['-w', '--watch'].includes(argv))
+      .join(' ');
+
+    await execa.shell(`babel ${babelOptions}`, {
+      stdio: 'inherit',
     });
 
-  // eslint-disable-next-line flowtype/no-unused-expressions
-  server.init()
-    |> server.use(defaultMiddleware)
-    |> server.use(await react(path.resolve('./lib/pages')))
-    |> server.run(parseInt(process.env.PORT || 8000, 10));
+    // TODO: avoid to trigger webpack again
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // eslint-disable-next-line flowtype/no-unused-expressions
+    server.init()
+      |> server.use(loadMiddleware('@cat-org/default-middleware'))
+      |> server.use(
+        await loadMiddleware(
+          '@cat-org/react-middleware',
+          path.resolve(outDir, './pages'),
+          {
+            dev: !isProduction,
+          },
+        ),
+      )
+      |> server.run(parseInt(process.env.PORT || 8000, 10));
+
+    if (!isProduction) return;
+
+    chokidar
+      .watch(path.resolve(outDir), {
+        ignoreInitial: true,
+      })
+      .on('change', (filePath: string) => {
+        if (/\.jsx?/.test(filePath)) delete require.cache[filePath];
+      });
+
+    await execa.shell(`babel --skip-initial-build -w ${babelOptions}`, {
+      stdio: 'inherit',
+    });
+  } catch (e) {
+    throw e;
+  }
 })();

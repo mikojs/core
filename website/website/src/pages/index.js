199ae2d953abc51390cd28a5033475a15fa5b19e
HsuTing <hsuting0106@gmail.com>
2019-05-26 17:08:46 +0800

    feat(website): project init

---
+++ website/website/src/pages/index.js
@@ -0,0 +1,9 @@
+// @flow
+/* eslint-disable require-jsdoc */
+// TODO component should be ignored
+
+import React from 'react';
+
+const Home = () => <div>@cat-org/create-project</div>;
+
+export default Home;

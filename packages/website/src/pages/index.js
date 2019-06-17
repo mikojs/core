228d28a14e3fd6d1c50b10228e44d568c5bbb095
HsuTing <hsuting0106@gmail.com>
2019-06-18 01:26:38 +0800

    feat(website): add flow typescript

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -3,7 +3,7 @@
 import React, { type Node as NodeType } from 'react';
 import { graphql } from 'react-relay';
 
-export default class Home extends React.PureComponent<*> {
+export default class Home extends React.PureComponent<{| version: string |}> {
   static query = graphql`
     query pages_homeQuery {
       version

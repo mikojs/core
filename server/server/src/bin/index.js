36ed2d23aa6485cc9776d3be968e8f8b62f95c02
HsuTing <hsuting0106@gmail.com>
2019-06-29 02:59:25 +0800

    feat(server): add dev to koa-graphql

--- server/server/src/bin/index.js
+++ server/server/src/bin/index.js
@@ -137,6 +137,7 @@ const run = async (context: serverContextType) =>
     );
     graphql = new (loadModule('@cat-org/koa-graphql', DefaultGraphql))(
       path.resolve(context.dir, './graphql'),
+      { dev: context.dev },
     );
 
     if (process.env.NODE_ENV !== 'test') {

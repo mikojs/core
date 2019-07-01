b7b922c69d83abce296ac36aca9f015cd963e83d
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2019-07-02 02:11:45 +0800

    (@cat-org/configs) Fix server config (#303)
    * perf(jest): move babel-jest to jest
    
    * feat(configs): remove dev in server config
    
    * test(configs): add server testing

--- packages/configs/src/configs/server.js
+++ packages/configs/src/configs/server.js
@@ -2,7 +2,7 @@
 
 export default {
   install: (install: $ReadOnlyArray<string>) => [
-    ...install,
+    ...install.filter((text: string) => text !== '--dev'),
     'koa',
     '@cat-org/server',
     '@cat-org/koa-base',

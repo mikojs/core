51a47ff5d5ec149bac8cfab4906d7b65482ddb8d
HsuTing <hsuting0106@gmail.com>
2019-07-02 15:44:39 +0800

    perf(create-project): remove module.ignore_non_literal_requires

--- packages/create-project/src/stores/flow.js
+++ packages/create-project/src/stores/flow.js
@@ -30,7 +30,6 @@ const template = (
 module.file_ext=.js
 ${useStyles === 'less' ? 'module.file_ext=.less' : 'module.file_ext=.css'}`
 }
-module.ignore_non_literal_requires=true
 module.system.node.resolve_dirname=node_modules
 module.system.node.resolve_dirname=./src
 

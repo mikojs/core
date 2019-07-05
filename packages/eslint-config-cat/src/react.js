df0ff43ca4fef7e30d54ef262c0b5c87a847dd38
HsuTing <hsuting0106@gmail.com>
2019-07-05 12:35:29 +0800

    feat(eslint-config-cat): add new react config

---
+++ packages/eslint-config-cat/src/react.js
@@ -0,0 +1,8 @@
+// @flow
+
+export default {
+  rules: {
+    'jsdoc/require-example': ['error', { exemptedBy: ['react'] }],
+    'jsdoc/require-param': ['error', { exemptedBy: ['react'] }],
+  },
+};

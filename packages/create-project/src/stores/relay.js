7ffb981f02f298f14f68614aaa15fd09b648d43f
HsuTing <hsuting0106@gmail.com>
2019-07-08 02:17:44 +0800

    perf(core): use new node schema

--- packages/create-project/src/stores/relay.js
+++ packages/create-project/src/stores/relay.js
@@ -122,51 +122,9 @@ export default {
 
 const nodeTemplate = `// @flow
 
-export default {
-  typeDefs: \`
-  # An object with an ID.
-  interface Node {
-    # ID of the object.
-    id: ID!
-  }
+import node from '@cat-org/koa-graphql/lib/schemas/node';
 
-  # An edge in a connection.
-  interface Edge {
-    # A cursor for use in pagination.
-    cursor: String!
-
-    # The item at the end of the edge.
-    node: Node!
-  }
-
-  # Look up data listings.
-  interface Connection {
-    # A list of edges.
-    edges: [Edge]!
-
-    # Information to aid in pagination.
-    pageInfo: PageInfo!
-
-    # Identifies the total count of items in the connection.
-    total: Int!
-  }
-
-  # Information about pagination in a connection.
-  type PageInfo {
-    # When paginating forwards, the cursor to continue.
-    endCursor: String
-
-    # When paginating forwards, are there more items?
-    hasNextPage: Boolean!
-
-    # When paginating backwards, are there more items?
-    hasPreviousPage: Boolean!
-
-    # When paginating backwards, the cursor to continue.
-    startCursor: String
-  }
-\`,
-};`;
+export default node;`;
 
 const mainTemplate = `// @flow
 

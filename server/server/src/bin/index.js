dc426982c7c952ad1a59114e40498e0aa796fed1
HsuTing <hsuting0106@gmail.com>
2019-07-18 16:44:18 +0800

    perf(server): run server as child process

--- server/server/src/bin/index.js
+++ server/server/src/bin/index.js
@@ -6,13 +6,16 @@ import path from 'path';
 import parseArgv from '@babel/cli/lib/babel/options';
 import dirCommand from '@babel/cli/lib/babel/dir';
 import debug from 'debug';
+import execa from 'execa';
 
-import { requireModule } from '@cat-org/utils';
+import { handleUnhandledRejection } from '@cat-org/utils';
 
 import findOptionsPath from 'utils/findOptionsPath';
 import logger from 'utils/logger';
 
 const debugLog = debug('server:bin');
+
+handleUnhandledRejection();
 
 (async () => {
   const dev = process.env.NODE_ENV !== 'production';
@@ -56,19 +59,20 @@ const debugLog = debug('server:bin');
       },
     });
 
-  const context = {
-    src: opts.cliOptions.filenames[0],
-    dir: opts.cliOptions.outDir,
-    dev,
-    watch: opts.cliOptions.watch,
-    port: process.env.PORT ? parseInt(process.env.PORT, 10) : undefined,
-  };
-
-  debugLog(context);
-
-  await requireModule(
-    customFile
-      ? path.resolve(customFile)
-      : path.resolve(__dirname, '../defaults'),
-  )(context);
+  execa(
+    path.resolve(__dirname, './server.js'),
+    [
+      customFile
+        ? path.resolve(customFile)
+        : path.resolve(__dirname, '../defaults'),
+      '--src',
+      opts.cliOptions.filenames[0],
+      '--dir',
+      opts.cliOptions.outDir,
+      ...(opts.cliOptions.watch ? ['--watch'] : []),
+    ],
+    {
+      stdio: 'inherit',
+    },
+  );
 })();
---
+++ server/server/src/bin/server.js
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+// @flow
+
+import commander from 'commander';
+import chalk from 'chalk';
+import debug from 'debug';
+
+import { requireModule } from '@cat-org/utils';
+
+import { version } from '../../package.json';
+
+import logger from 'utils/logger';
+
+const debugLog = debug('server:bin:server');
+const program = new commander.Command('server')
+  .version(version, '-v, --version')
+  .arguments('<server file path>')
+  .usage(chalk`{green <server file path>}`)
+  .description(
+    chalk`Example:
+  server ./server.js {gray --src ./src --dir ./lib}`,
+  )
+  .option('--src <path>', 'src folder path')
+  .option('--dir <path>', 'dir folder path')
+  .option('--watch', 'use watch mode');
+
+const {
+  args: [serverFile],
+  src,
+  dir,
+  watch = false,
+} = program.parse([...process.argv]);
+
+debugLog(process.argv);
+
+if (!src || !dir || !serverFile)
+  throw logger.fail(
+    chalk`{red src, dir, serverFile} is required, but get {gray ${src}, ${dir}, ${serverFile}}`,
+  );
+
+const context = {
+  src,
+  dir,
+  dev: process.env.NODE_ENV !== 'production',
+  watch,
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : undefined,
+};
+
+debugLog(context);
+
+requireModule(serverFile)(context);

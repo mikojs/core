dd06ce457cb55a2a675bb40a0323ad66f6f52841
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2019-07-19 00:52:25 +0800

    (@cat-org/server) Can watch custom server (#344)
    * perf(server): run server as child process
    
    * feat(server): support to watch custom server
    
    * feat(server): can key in the word to restart and stop the server
    
    * style(server): modify code detail

--- server/server/src/bin/index.js
+++ server/server/src/bin/index.js
@@ -2,17 +2,22 @@
 // @flow
 
 import path from 'path';
+import readline from 'readline';
 
 import parseArgv from '@babel/cli/lib/babel/options';
 import dirCommand from '@babel/cli/lib/babel/dir';
 import debug from 'debug';
+import execa, { ThenableChildProcess as ThenableChildProcessType } from 'execa';
+import chokidar from 'chokidar';
 
-import { requireModule } from '@cat-org/utils';
+import { handleUnhandledRejection } from '@cat-org/utils';
 
 import findOptionsPath from 'utils/findOptionsPath';
 import logger from 'utils/logger';
 
 const debugLog = debug('server:bin');
+
+handleUnhandledRejection();
 
 (async () => {
   const dev = process.env.NODE_ENV !== 'production';
@@ -20,7 +25,7 @@ const debugLog = debug('server:bin');
   const customFile = opts.cliOptions.outFile;
 
   if (customFile) {
-    logger.info('Run with custom server');
+    logger.info('Run the custom server');
     debugLog(opts);
 
     const { src, dir } = findOptionsPath(
@@ -56,19 +61,56 @@ const debugLog = debug('server:bin');
       },
     });
 
-  const context = {
-    src: opts.cliOptions.filenames[0],
-    dir: opts.cliOptions.outDir,
-    dev,
-    watch: opts.cliOptions.watch,
-    port: process.env.PORT ? parseInt(process.env.PORT, 10) : undefined,
-  };
-
-  debugLog(context);
-
-  await requireModule(
+  const serverArgu = [
     customFile
       ? path.resolve(customFile)
       : path.resolve(__dirname, '../defaults'),
-  )(context);
+    '--src',
+    opts.cliOptions.filenames[0],
+    '--dir',
+    opts.cliOptions.outDir,
+    ...(opts.cliOptions.watch ? ['--watch'] : []),
+  ];
+
+  let subprocess: ThenableChildProcessType = execa(
+    path.resolve(__dirname, './server.js'),
+    serverArgu,
+    {
+      stdio: 'inherit',
+    },
+  );
+
+  if (!customFile || !opts.cliOptions.watch) return;
+
+  logger.log(
+    'Use `rs` to restart the custom server',
+    'Use `exit` or `ctrl + c` to stop the custom server',
+  );
+
+  /**
+   * @example
+   * restart()
+   */
+  const restart = () => {
+    logger.log(
+      'Restart the custom server',
+      'Use `rs` to restart the custom server',
+      'Use `exit` or `ctrl + c` to stop the custom server',
+    );
+
+    subprocess.cancel();
+    subprocess = execa(path.resolve(__dirname, './server.js'), serverArgu, {
+      stdio: 'inherit',
+    });
+  };
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  chokidar.watch(path.resolve(customFile)).on('change', restart);
+  rl.on('line', (line: string) => {
+    if (line === 'rs') restart();
+    else if (line === 'exit') process.exit(0);
+  });
 })();

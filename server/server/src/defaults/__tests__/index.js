66e70068866b6367602cc6dad10e185635bf1939
HsuTing <hsuting0106@gmail.com>
2019-07-18 13:52:32 +0800

    test(server, koa-graphql): close server after Each testing

--- server/server/src/defaults/__tests__/index.js
+++ server/server/src/defaults/__tests__/index.js
@@ -6,28 +6,36 @@ import fetch, { type Response as ResponseType } from 'node-fetch';
 
 import defaults from '../index';
 
+let runningServer: http$Server;
+
 jest.mock('../../utils/loadModule', () => <A, B>(a: A, b: B): B => b);
 
-test.each`
+describe.each`
   dev          | watch
   ${undefined} | ${undefined}
   ${true}      | ${true}
 `(
   'default server with dev = $dev, watch = $watch',
-  async ({ dev, watch }: {| dev: boolean, watch: boolean |}) => {
-    const runningServer = await defaults({
-      src: path.resolve(__dirname, './__ignore__/src'),
-      dir: path.resolve(__dirname, './__ignore__/src'),
-      dev,
-      watch,
+  ({ dev, watch }: {| dev: boolean, watch: boolean |}) => {
+    beforeEach(async () => {
+      runningServer = await defaults({
+        src: path.resolve(__dirname, './__ignore__/src'),
+        dir: path.resolve(__dirname, './__ignore__/src'),
+        dev,
+        watch,
+      });
     });
 
-    expect(
-      await fetch('http://localhost:8000').then((res: ResponseType) =>
-        res.text(),
-      ),
-    ).toBe('Not Found');
+    test('test', async () => {
+      expect(
+        await fetch('http://localhost:8000').then((res: ResponseType) =>
+          res.text(),
+        ),
+      ).toBe('Not Found');
+    });
 
-    runningServer.close();
+    afterAll(() => {
+      runningServer.close();
+    });
   },
 );

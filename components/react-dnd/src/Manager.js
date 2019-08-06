ec76915a4e810250feb28318050b24b9d49cad02
HsuTing <hsuting0106@gmail.com>
2019-08-06 11:38:37 +0800

    fix(react-dnd): fix Manager and Previewer ref error

--- components/react-dnd/src/Manager.js
+++ components/react-dnd/src/Manager.js
@@ -4,10 +4,23 @@ import React, { type Node as NodeType } from 'react';
 
 type propsType = {
   children: NodeType,
+  forwardedRef:
+    | { current: null | HTMLDivElement, ... }
+    | ((null | HTMLDivElement) => mixed),
 };
 
-const Manager = React.memo<propsType>(({ children }: propsType) => (
-  <div>{children}</div>
-));
+const Manager = React.memo<propsType>(
+  ({ children, forwardedRef }: propsType) => (
+    <div ref={forwardedRef}>{children}</div>
+  ),
+);
 
-export default Manager;
+export default React.forwardRef<
+  $Diff<propsType, { forwardedRef: mixed }>,
+  HTMLDivElement,
+>(
+  (
+    props: $Diff<propsType, { forwardedRef: mixed }>,
+    ref: $PropertyType<propsType, 'forwardedRef'>,
+  ) => <Manager {...props} forwardedRef={ref} />,
+);
--- components/react-dnd/src/Previewer.js
+++ components/react-dnd/src/Previewer.js
@@ -4,10 +4,23 @@ import React, { type Node as NodeType } from 'react';
 
 type propsType = {
   children: NodeType,
+  forwardedRef:
+    | { current: null | HTMLDivElement, ... }
+    | ((null | HTMLDivElement) => mixed),
 };
 
-const Previewer = React.memo<propsType>(({ children }: propsType) => (
-  <div>{children}</div>
-));
+const Previewer = React.memo<propsType>(
+  ({ children, forwardedRef }: propsType) => (
+    <div ref={forwardedRef}>{children}</div>
+  ),
+);
 
-export default Previewer;
+export default React.forwardRef<
+  $Diff<propsType, { forwardedRef: mixed }>,
+  HTMLDivElement,
+>(
+  (
+    props: $Diff<propsType, { forwardedRef: mixed }>,
+    ref: $PropertyType<propsType, 'forwardedRef'>,
+  ) => <Previewer {...props} forwardedRef={ref} />,
+);

f15f8205d558bc1d255cb3e0f19dffc2e861382e
HsuTing <hsuting0106@gmail.com>
2019-08-06 13:26:36 +0800

    feat(react-dnd): add react-frame-component

--- components/react-dnd/src/index.js
+++ components/react-dnd/src/index.js
@@ -3,10 +3,11 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+import Frame, { FrameContextConsumer } from 'react-frame-component';
 
 import Provider, { DataContext } from './Provider';
 import Renderer from './Renderer';
-import styles from './styles';
+import * as styles from './styles';
 import { type contextType } from './types';
 
 /*
@@ -24,10 +25,26 @@ const Dnd = React.memo<{||}>(() => (
     <DndProvider backend={HTML5Backend}>
       <DataContext.Consumer>
         {({ manager, previewer }: contextType) => (
-          <div style={styles}>
+          <div style={styles.root}>
             <Renderer source={manager} />
 
-            <Renderer source={previewer} />
+            <Frame
+              style={styles.iframe}
+              head={
+                <link
+                  rel="stylesheet"
+                  href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+                />
+              }
+            >
+              <FrameContextConsumer>
+                {({ window }: { window: mixed }) => (
+                  <DndProvider backend={HTML5Backend} context={window}>
+                    <Renderer source={previewer} />
+                  </DndProvider>
+                )}
+              </FrameContextConsumer>
+            </Frame>
           </div>
         )}
       </DataContext.Consumer>
--- components/react-dnd/src/styles/index.js
+++ components/react-dnd/src/styles/index.js
@@ -1,7 +1,13 @@
 // @flow
 
-export default {
+export const root = {
   display: 'grid',
   gridTemplateColumns: '250px auto',
   height: '100vh',
+};
+
+export const iframe = {
+  width: '100%',
+  height: '100%',
+  border: '0px',
 };

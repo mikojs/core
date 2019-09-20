459e2715e6714270739091e0beb1e8c3fcbbe008
HsuTing <hsuting0106@gmail.com>
2019-09-20 15:15:36 +0800

    test(logger): remove not needing mock

--- packages/logger/src/__tests__/index.js
+++ packages/logger/src/__tests__/index.js
@@ -2,6 +2,19 @@
 
 import logger from '../index';
 
+jest.mock(
+  'chalk',
+  () => (
+    texts: $ReadOnlyArray<string>,
+    ...keys: $ReadOnlyArray<string>
+  ): string =>
+    texts.reduce(
+      (result: string, text: string, index: number): string =>
+        `${result}${text}${keys[index] || ''}`,
+      '',
+    ),
+);
+
 describe('logger', () => {
   test.each`
     name         | prefix | color

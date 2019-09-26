5147ec8638635ad4d183e2a2edb983830f9960eb
HsuTing <hsuting0106@gmail.com>
2019-09-26 12:01:11 +0800

    feat(react-cms): add default EventsProvider

--- components/react-cms/src/index.js
+++ components/react-cms/src/index.js
@@ -1,6 +1,23 @@
 // @flow
 
-/** @react use to control the all event */
-const Cms = () => 'react-cms';
+import React from 'react';
+import { DndProvider } from 'react-dnd-cjs';
+import HTML5Backend from 'react-dnd-html5-backend-cjs';
 
-export default Cms;
+import EventsProvider from './EventsProvider';
+
+/* eslint-disable */
+const Example = () => <div>test</div>;
+/* eslint-enable */
+
+/** @react use to control the all main components */
+const Cms = () => (
+  <DndProvider backend={HTML5Backend}>
+    <EventsProvider>
+      <Example />
+      <Example />
+    </EventsProvider>
+  </DndProvider>
+);
+
+export default React.memo<{||}>(Cms);

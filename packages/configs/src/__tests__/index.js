bc1ebafd5beb6d7c2fb975f189362cfb34b267d3
HsuTing <hsuting0106@gmail.com>
2019-10-01 19:48:29 +0800

    test(configs): fix testing with the new config

--- packages/configs/src/__tests__/index.js
+++ packages/configs/src/__tests__/index.js
@@ -1,18 +1,30 @@
 // @flow
 
+import net from 'net';
 import path from 'path';
+import getPort from 'get-port';
 
 import getConfig from '../index';
 
 import configs from 'utils/configs';
 
-test('get config', () => {
+test('get config', async () => {
+  const port = await getPort();
+  const server = net.createServer((socket: net.Socket) => {
+    socket.on('data', () => {
+      server.close();
+    });
+  });
+
   configs.handleCustomConfigs({
     config: {
       getConfig: {},
     },
     filepath: path.resolve(process.cwd(), './.catrc.js'),
   });
+  server.listen(port);
 
-  expect(getConfig('getConfig', path.resolve('jest.config.js'))).toEqual({});
+  expect(getConfig('getConfig', port, path.resolve('jest.config.js'))).toEqual(
+    {},
+  );
 });

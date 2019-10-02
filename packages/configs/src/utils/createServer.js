81976b6596692f0cf1bdbf31e4fa0607398e589d
HsuTing <hsuting0106@gmail.com>
2019-10-02 22:20:30 +0800

    fix(configs): should check data before json.parse

--- packages/configs/src/utils/createServer.js
+++ packages/configs/src/utils/createServer.js
@@ -25,65 +25,71 @@ export default (port: number) => {
     socket.on('data', (data: string) => {
       debugLog(data);
 
-      const { pid, filePath } = JSON.parse(data);
+      try {
+        const { pid, filePath } = JSON.parse(data);
 
-      if (!cache[filePath]) cache[filePath] = [];
+        if (!pid || !filePath) return;
 
-      cache[filePath].push(pid);
-      debugLog(`Cache: ${JSON.stringify(cache, null, 2)}`);
-      clearInterval(timer);
+        if (!cache[filePath]) cache[filePath] = [];
 
-      let isRemoving: boolean = false;
+        cache[filePath].push(pid);
+        debugLog(`Cache: ${JSON.stringify(cache, null, 2)}`);
+        clearInterval(timer);
 
-      checkedTimes = 0;
-      timer = setInterval(() => {
-        const hasWorkingPids = Object.keys(cache).reduce(
-          (result: boolean, cacheFilePath: string): boolean => {
-            const newPids = cache[cacheFilePath].filter(isRunning);
+        let isRemoving: boolean = false;
 
-            if (newPids.length !== cache[cacheFilePath].length)
-              debugLog(
-                `Cache: ${JSON.stringify(
-                  { ...cache, [cacheFilePath]: newPids },
-                  null,
-                  2,
-                )}`,
-              );
+        checkedTimes = 0;
+        timer = setInterval(() => {
+          const hasWorkingPids = Object.keys(cache).reduce(
+            (result: boolean, cacheFilePath: string): boolean => {
+              const newPids = cache[cacheFilePath].filter(isRunning);
 
-            cache[cacheFilePath] = newPids;
+              if (newPids.length !== cache[cacheFilePath].length)
+                debugLog(
+                  `Cache: ${JSON.stringify(
+                    { ...cache, [cacheFilePath]: newPids },
+                    null,
+                    2,
+                  )}`,
+                );
 
-            return result || newPids.length !== 0;
-          },
-          false,
-        );
+              cache[cacheFilePath] = newPids;
 
-        if (!hasWorkingPids) {
-          if (checkedTimes >= 50)
-            server.close(() => {
-              clearInterval(timer);
-              debugLog('Close server');
-            });
-          else if (checkedTimes >= 5 && Object.keys(cache).length !== 0) {
-            if (!isRemoving) {
-              const [removeFilePath] = Object.keys(cache);
+              return result || newPids.length !== 0;
+            },
+            false,
+          );
 
-              if (!fs.existsSync(removeFilePath)) {
-                delete cache[removeFilePath];
-                return;
-              }
-
-              isRemoving = true;
-              rimraf(removeFilePath, () => {
-                isRemoving = false;
-
-                delete cache[removeFilePath];
-                debugLog(`Remove existing file: ${removeFilePath}`);
-                debugLog(`Cache: ${JSON.stringify(cache, null, 2)}`);
+          if (!hasWorkingPids) {
+            if (checkedTimes >= 50)
+              server.close(() => {
+                clearInterval(timer);
+                debugLog('Close server');
               });
-            }
-          } else checkedTimes += 1;
-        }
-      }, 100);
+            else if (checkedTimes >= 5 && Object.keys(cache).length !== 0) {
+              if (!isRemoving) {
+                const [removeFilePath] = Object.keys(cache);
+
+                if (!fs.existsSync(removeFilePath)) {
+                  delete cache[removeFilePath];
+                  return;
+                }
+
+                isRemoving = true;
+                rimraf(removeFilePath, () => {
+                  isRemoving = false;
+
+                  delete cache[removeFilePath];
+                  debugLog(`Remove existing file: ${removeFilePath}`);
+                  debugLog(`Cache: ${JSON.stringify(cache, null, 2)}`);
+                });
+              }
+            } else checkedTimes += 1;
+          }
+        }, 100);
+      } catch (err) {
+        debugLog(err);
+      }
     });
   });
 

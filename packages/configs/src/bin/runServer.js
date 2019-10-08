1475e8e0b526a3dc75923cc05a5dbfff8e4e94cf
HsuTing <hsuting0106@gmail.com>
2019-10-08 17:37:58 +0800

    refactor(configs): remove port to return from the callback
    [skip ci]

--- packages/configs/src/bin/runServer.js
+++ packages/configs/src/bin/runServer.js
@@ -3,14 +3,12 @@
 
 import fs from 'fs';
 
-import findProcess from 'find-process';
 import debug from 'debug';
 
 import { handleUnhandledRejection } from '@mikojs/utils';
 
 import createServer, { type serverArguType } from 'utils/createServer';
 
-const port = parseInt(process.argv[2], 10);
 const debugLog = !process.env.CONFIG_SERVER_DEBUG
   ? debug('configs:runServer')
   : ((): ((data: mixed) => void) & {
@@ -45,26 +43,12 @@ const debugLog = !process.env.CONFIG_SERVER_DEBUG
 
 handleUnhandledRejection();
 createServer(
-  port,
+  parseInt(process.argv[2], 10),
   debugLog,
-  async ({ type, cache }: serverArguType): Promise<number> => {
-    if (type === 'close') {
-      // $FlowFixMe TODO: Flow does not yet support method or property calls in optional chains.
-      debugLog.close?.(); // eslint-disable-line flowtype/no-unused-expressions
-      return port;
-    }
+  ({ type, cache }: serverArguType) => {
+    if (type !== 'close') return;
 
-    const [existServer] = (await findProcess('name', __filename)).slice(-1);
-
-    debugLog(existServer);
-    debugLog(process.pid);
-
-    if (existServer.pid === process.pid) return port;
-
-    const [existServerPort] = existServer.cmd.split(/ /).slice(-1);
-
-    debugLog(existServerPort);
-
-    return existServerPort;
+    // $FlowFixMe TODO: Flow does not yet support method or property calls in optional chains.
+    debugLog.close?.(); // eslint-disable-line flowtype/no-unused-expressions
   },
 );

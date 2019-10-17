6715f594ae99b336a5b63c95047dd9d0682f2b6c
HsuTing <hsuting0106@gmail.com>
2019-10-18 00:13:11 +0800

    fix(website): fix testing

--- packages/website/src/utils/createEnvironment/__tests__/index.js
+++ packages/website/src/utils/createEnvironment/__tests__/index.js
@@ -2,41 +2,57 @@
 
 import { fetchQuery, graphql } from 'react-relay';
 import { type SSRCache as SSRCacheType } from 'react-relay-network-modern-ssr/node8/server';
-import fetchMock from 'fetch-mock';
-
-import client from '../client';
-import * as server from '../index';
 
 import { version } from '../../../../package.json';
 
 const query = graphql`
-  query clientQuery {
+  query Tests_clientQuery {
     version
   }
 `;
 let relayData: SSRCacheType;
 
-fetchMock.mock('*', { data: { version } });
-
 describe('create environment', () => {
   describe('client', () => {
+    beforeAll(() => {
+      jest.resetModules();
+      process.env.BROWSER = 'true';
+    });
+
     test.each`
       info
       ${'first'}
       ${'second'}
     `('create environment in the $info time', async () => {
-      expect(await fetchQuery(client.createEnvironment(), query)).toEqual({
+      const { createEnvironment } = require('../index');
+      const fetchMock = require('fetch-mock');
+
+      fetchMock.reset();
+      fetchMock.mock('*', { data: { version } });
+
+      expect(await fetchQuery(createEnvironment(), query)).toEqual({
         version,
       });
     });
   });
 
   describe('server', () => {
+    beforeAll(() => {
+      jest.resetModules();
+      delete process.env.BROWSER;
+    });
+
     test('initialize environment before rendering component', async () => {
-      if (!server.initEnvironment)
+      const { initEnvironment } = require('../index');
+      const fetchMock = require('fetch-mock');
+
+      fetchMock.reset();
+      fetchMock.mock('*', { data: { version } });
+
+      if (!initEnvironment)
         throw new Error('Can not get initEnvironment from `createEnvironment`');
 
-      const { relaySSR, environment } = server.initEnvironment();
+      const { relaySSR, environment } = initEnvironment();
 
       expect(await fetchQuery(environment, query)).toEqual({ version });
 
@@ -44,9 +60,15 @@ describe('create environment', () => {
     });
 
     test('mock query when rendering component', async () => {
+      const { createEnvironment } = require('../index');
+      const fetchMock = require('fetch-mock');
+
+      fetchMock.reset();
+      fetchMock.mock('*', { data: { version } });
+
       expect(
         await fetchQuery(
-          server.createEnvironment(
+          createEnvironment(
             relayData,
             JSON.stringify({
               queryID: query?.()?.params.name,

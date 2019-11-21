8db0d2df621af11e045e359bd51ad90489f41b09
HsuTing <hsuting0106@gmail.com>
2019-11-21 13:47:50 +0800

    fix(koa-react): fix error to add react-hot-loader/root to the generated folder

--- server/koa-react/src/utils/getConfig.js
+++ server/koa-react/src/utils/getConfig.js
@@ -84,6 +84,8 @@ export default (
       {
         test: /\.jsx?$/,
         include: [CLIENT_PATH, ROOT_PATH, folderPath],
+        // TODO: add @mikojs/use-relay to handle exclude relay generated folder
+        exclude: /__generated__/,
         loader: 'babel-loader',
         options: {
           plugins: [

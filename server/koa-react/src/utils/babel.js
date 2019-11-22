e3da2d6f5771460e30e1563da2c181ab7fff61ce
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2019-11-22 11:08:56 +0800

    (@mikojs/koa-react) Use the babel version of react hot loader (#470)
    * perf(koa-react): not use loader to replace react-hot-loader, use react-hot-loader directly
    
    * feat(koa-react): add babel-loader to load react-hot-loader/babel
    
    * chore(koa-react): remove replaceLoader
    
    * perf(koa-react): use new babel plugin to add react-hot-loader
    
    * feat(koa-react): can replace tempalates path
    
    * feat(koa-react): support to auto add react-hot-loader/root
    
    * fix(koa-react): fix loader path error
    
    * fix(koa-react): fix error to add react-hot-loader/root to the generated folder
    
    * perf(koa-react): add new function to handle hot-loader
    
    * feat(koa-react): add hoist-non-react-statics for memo component
    
    * feat(koa-react): auto check if file is react file
    
    * fix(koa-react): upgrade and fix flow error
    
    * test(koa-react): add new testing for new babel
    
    * test(koa-react): add new testings for getStatic
    
    * test(koa-react): add more testing for testing react-hot-loader

---
+++ server/koa-react/src/utils/babel.js
@@ -0,0 +1,179 @@
+// @flow
+
+import nodePath from 'path';
+
+import { declare } from '@babel/helper-plugin-utils';
+import type nodePathType from '@babel/traverse';
+
+import type CacheType from './Cache';
+
+export default declare(
+  (
+    { assertVersion, types: t }: nodePathType,
+    { cacheDir }: { cacheDir: $PropertyType<CacheType, 'cacheDir'> },
+  ): {} => {
+    assertVersion(7);
+
+    /**
+     * @example
+     * isAddedHotLoader(node)
+     *
+     * @param {nodePathType} node - node from babel
+     *
+     * @return {boolean} - if hot loader is added or not
+     */
+    const isAddedHotLoader = (node: nodePathType) =>
+      t.isCallExpression(node) &&
+      t.isMemberExpression(node.callee) &&
+      t.isCallExpression(node.callee.object) &&
+      t.isIdentifier(node.callee.property, { name: 'hot' }) &&
+      t.isIdentifier(node.callee.object.callee, {
+        name: 'require',
+      }) &&
+      t.isStringLiteral(node.callee.object.arguments[0], {
+        value: 'react-hot-loader/root',
+      });
+
+    /**
+     * @example
+     * addHotLoader(node)
+     *
+     * @param {nodePathType} node - node from babel
+     *
+     * @return {nodePathType} - new node to add hot loader
+     */
+    const addHotLoader = (node: nodePathType) =>
+      t.callExpression(
+        t.memberExpression(
+          t.callExpression(t.identifier('require'), [
+            t.stringLiteral('react-hot-loader/root'),
+          ]),
+          t.identifier('hot'),
+        ),
+        [
+          t.callExpression(
+            t.memberExpression(
+              t.callExpression(t.identifier('require'), [
+                t.stringLiteral('@mikojs/koa-react/lib/utils/getStatic'),
+              ]),
+              t.identifier('hoistNonReactStaticsHotExported'),
+            ),
+            [
+              node,
+              t.binaryExpression(
+                '!==',
+                t.memberExpression(
+                  t.memberExpression(
+                    t.identifier('process'),
+                    t.identifier('env'),
+                  ),
+                  t.identifier('NODE_ENV'),
+                ),
+                t.stringLiteral('production'),
+              ),
+            ],
+          ),
+        ],
+      );
+    let isReact: boolean = false;
+
+    return {
+      pre: () => {
+        isReact = false;
+      },
+      visitor: {
+        ImportDeclaration: (path: nodePathType) => {
+          if (t.isStringLiteral(path.node.source, { value: 'react' }))
+            isReact = true;
+        },
+        Identifier: (
+          path: nodePathType,
+          {
+            file: {
+              opts: { filename },
+            },
+          }: {|
+            file: {|
+              opts: {| filename: string |},
+            |},
+          |},
+        ) => {
+          switch (path.node.name) {
+            case 'require': {
+              if (!t.isCallExpression(path.parentPath.node)) return;
+
+              if (
+                t.isStringLiteral(path.parentPath.node.arguments[0], {
+                  value: 'react',
+                })
+              ) {
+                isReact = true;
+                return;
+              }
+
+              if (!isReact) return;
+
+              const templateFolder = nodePath.resolve(
+                __dirname,
+                '../templates',
+              );
+              const currentTemplatePath = nodePath.resolve(
+                nodePath.dirname(filename),
+                path.parentPath.node.arguments[0].value,
+              );
+
+              if (!currentTemplatePath.match(templateFolder)) return;
+
+              const newTemplatePath = cacheDir(
+                nodePath.relative(templateFolder, currentTemplatePath),
+              );
+
+              if (currentTemplatePath === newTemplatePath) return;
+
+              path.parentPath.replaceWith(
+                t.callExpression(path.node, [t.stringLiteral(newTemplatePath)]),
+              );
+              return;
+            }
+
+            case 'module':
+              if (
+                !isReact ||
+                !t.isMemberExpression(path.parentPath.node) ||
+                !t.isIdentifier(path.parentPath.node.property, {
+                  name: 'exports',
+                }) ||
+                !t.isAssignmentExpression(path.parentPath.parentPath.node)
+              )
+                return;
+
+              const rootNode = path.parentPath.parentPath.node;
+
+              if (isAddedHotLoader(rootNode.right)) return;
+
+              path.parentPath.parentPath.replaceWith(
+                t.assignmentExpression(
+                  '=',
+                  rootNode.left,
+                  addHotLoader(rootNode.right),
+                ),
+              );
+              return;
+
+            default:
+              return;
+          }
+        },
+        ExportDefaultDeclaration: (path: nodePathType) => {
+          const rootNode = path.node.declaration;
+
+          if (!isReact || isAddedHotLoader(rootNode)) return;
+
+          path.replaceWith(
+            t.exportDefaultDeclaration(addHotLoader(path.node.declaration)),
+          );
+        },
+      },
+    };
+  },
+);

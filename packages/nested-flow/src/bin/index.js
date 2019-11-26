f7e42ba47beaf91f92e7561325b1a090eaae3dfd
HsuTing <hsuting0106@gmail.com>
2019-11-27 01:24:46 +0800

    feat(nested-flow): can parse flow error messages

--- packages/nested-flow/src/bin/index.js
+++ packages/nested-flow/src/bin/index.js
@@ -5,17 +5,46 @@ import path from 'path';
 
 import execa from 'execa';
 
+import { handleUnhandledRejection } from '@mikojs/utils';
+
 import cliOptions from 'utils/cliOptions';
-import findFlowConfigs from 'utils/findFlowConfigs';
+import findFlowDir from 'utils/findFlowDir';
+
+handleUnhandledRejection();
 
 (async () => {
+  const { log } = console;
   const argv = await cliOptions(process.argv);
+  let countError: number = 0;
 
-  await Promise.all(
-    findFlowConfigs().map((filePath: string) =>
-      execa(argv[0], argv.slice(1), {
-        cwd: path.dirname(filePath),
-      }),
-    ),
-  );
+  for (const folder of findFlowDir()) {
+    try {
+      await execa(argv[0], argv.slice(1), {
+        cwd: folder,
+      });
+    } catch (e) {
+      log(
+        e.stdout
+          .replace(
+            /Error ([-]+) /g,
+            `Error $1 ${path.relative(process.cwd(), folder)}`,
+          )
+          .replace(/\n\nFound [0-9]+ errors\n/g, '')
+          .replace(
+            /\n\n... [0-9]+ more errors \(only [0-9]+ out of [0-9]+ errors displayed\)\n/g,
+            '',
+          )
+          .replace(
+            /To see all errors, re-run Flow with --show-all-errors\n/g,
+            '',
+          ),
+      );
+
+      countError += (e.stdout.match(/Error ([-]+) /g) || []).length;
+    }
+  }
+
+  log(`
+Found ${countError} errors
+`);
 })();

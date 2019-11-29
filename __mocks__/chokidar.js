f097820ccd481627b79584ca0cc218440b8b4e97
HsuTing <hsuting0106@gmail.com>
2019-11-29 13:09:53 +0800

    test(server): add chokidar mock

--- __mocks__/chokidar.js
+++ __mocks__/chokidar.js
@@ -1,3 +1,14 @@
 // @flow
 
-export default jest.fn();
+export default ({
+  watch: jest.fn().mockReturnValue({
+    on: jest.fn(),
+  }),
+}: {|
+  watch: JestMockFn<
+    $ReadOnlyArray<void>,
+    {|
+      on: JestMockFn<$ReadOnlyArray<void>, void>,
+    |},
+  >,
+|});

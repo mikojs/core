52ade9082acde2d1e34f195c5de4b0ea49509b3f
HsuTing <hsuting0106@gmail.com>
2019-12-03 15:11:39 +0800

    feat(nested-flow): add flow-typed install command

---
+++ packages/nested-flow/src/commands/flowTyped/install.js
@@ -0,0 +1,65 @@
+// @flow
+
+import path from 'path';
+import { Writable } from 'stream';
+
+import execa from 'execa';
+import envinfo from 'envinfo';
+
+/**
+ * @example
+ * addFlowVersion()
+ *
+ * @return {Array} - if the version can be found, use to add the version arguments
+ */
+const addFlowVersion = async (): Promise<$ReadOnlyArray<string>> => {
+  const version = JSON.parse(
+    await envinfo.run(
+      {
+        npmPackages: ['flow-bin'],
+      },
+      { json: true },
+    ),
+  ).npmPackages?.['flow-bin']?.installed;
+
+  return !version ? [] : ['-f', version];
+};
+
+/**
+ * @example
+ * command(['flow-typed', 'install'], '/folder')
+ *
+ * @param {Array} argv - argv array
+ * @param {string} cwd - the folder where command runs
+ */
+export default async (argv: $ReadOnlyArray<string>, cwd: string) => {
+  const subprocess = execa(
+    argv[0],
+    [
+      ...argv.slice(1),
+      ...(argv.includes('-f') || argv.includes('--flowVersion')
+        ? []
+        : await addFlowVersion()),
+    ],
+    {
+      cwd,
+    },
+  );
+  const transform = new Writable({
+    write: (chunk: Buffer | string, encoding: string, callback: () => void) => {
+      const output = chunk.toString();
+
+      process.stdout.write(
+        output.replace(
+          /(flow-typed\/npm)/g,
+          `${path.relative(process.cwd(), cwd)}/flow-typed/npm`,
+        ),
+      );
+    },
+  });
+
+  subprocess.stdout.pipe(transform);
+  subprocess.stderr.pipe(transform);
+
+  await subprocess;
+};

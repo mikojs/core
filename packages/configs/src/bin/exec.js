b9ae0717ad95b5de267b27168f867fc5722b3ccc
HsuTing <hsuting0106@gmail.com>
2020-01-16 17:37:15 +0800

    improvement(configs): not throw exec error

--- packages/configs/src/bin/exec.js
+++ packages/configs/src/bin/exec.js
@@ -5,12 +5,14 @@ import { cosmiconfigSync } from 'cosmiconfig';
 import npmWhich from 'npm-which';
 import execa from 'execa';
 import chalk from 'chalk';
+import debug from 'debug';
 
 import { handleUnhandledRejection, createLogger } from '@mikojs/utils';
 
 import getExecCommands from 'utils/getExecCommands';
 
 const logger = createLogger('@mikojs/configs (exec)');
+const debugLog = debug('configs:bin:exec');
 
 handleUnhandledRejection();
 
@@ -25,8 +27,15 @@ handleUnhandledRejection();
   ];
 
   logger.log(chalk`Run command: {gray ${commands.join(' ')}}`);
+  debugLog({ type, argv, config });
 
-  await execa(commands[0], commands.slice(1), {
-    stdio: 'inherit',
-  });
+  try {
+    await execa(commands[0], commands.slice(1), {
+      stdio: 'inherit',
+    });
+  } catch (e) {
+    logger.log('Run command fail');
+    debugLog(e);
+    process.exit(e.exitCode || 1);
+  }
 })();

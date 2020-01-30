e77d97da5bfb15d3e716399a532846ffb66e94b4
HsuTing <hsuting0106@gmail.com>
2020-01-31 00:47:15 +0800

    feat(react-ssr): add getStatic function

---
+++ components/react-ssr/src/utils/getStatic.js
@@ -0,0 +1,33 @@
+// @flow
+
+import { type ComponentType } from 'react';
+import { Memo } from 'react-is';
+
+type memoComponentType = ComponentType<*> & {|
+  type: ComponentType<*>,
+  $$typeof: string,
+|};
+
+/**
+ * After `react-is@>=16.12`, `isMemo` is used to check the component which is created by react.createElement.
+ *
+ * @example
+ * isMemo(Component)
+ *
+ * @param {memoComponentType} Component - component type
+ *
+ * @return {boolean} - is the memo component
+ */
+const isMemo = (Component: memoComponentType) => Component?.$$typeof === Memo;
+
+/**
+ * @example
+ * getStatic(Component)
+ *
+ * @param {ComponentType} Component - component type
+ *
+ * @return {object} - the properties of the component
+ */
+export default (Component: ComponentType<*>) =>
+  // $FlowFixMe FIXME: https://github.com/facebook/flow/issues/8145
+  !isMemo(Component) ? Component : Component.type || Component;

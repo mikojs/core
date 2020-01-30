b69298534154f673b4137536ee0609bdbf2d6076
HsuTing <hsuting0106@gmail.com>
2020-01-30 23:51:49 +0800

    feat(react-ssr): add useCtx hook

---
+++ components/react-ssr/src/hooks/useCtx.js
@@ -0,0 +1,56 @@
+// @flow
+
+import { useRef, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { ExecutionEnvironment } from 'fbjs';
+
+type ctxType = {|
+  ctx:
+    | {}
+    | {|
+        path: string,
+        querystring: string,
+        originalUrl: string,
+        origin: string,
+        href: string,
+        host: string,
+        hostname: string,
+        protocol: string,
+      |},
+  isLoading: boolean,
+|};
+
+/**
+ * @example
+ * useCtx()
+ *
+ * @return {ctxType} - ctx object
+ */
+export default (): ctxType => {
+  const isServer = !ExecutionEnvironment.canUseEventListeners;
+  const { pathname, search } = useLocation();
+  const ctx = isServer
+    ? {}
+    : {
+        path: pathname,
+        querystring: search.replace(/\?/, ''),
+        originalUrl: `${pathname}${search}`,
+        origin: window.location.origin,
+        href: window.location.href,
+        host: window.location.host,
+        hostname: window.location.hostname,
+        protocol: window.location.protocol,
+      };
+  const prevCtxRef = useRef(ctx);
+
+  useEffect(() => {
+    prevCtxRef.current = ctx;
+  }, [ctx.originalUrl]);
+
+  const { current: prevCtx } = prevCtxRef;
+
+  return {
+    ctx,
+    isLoading: prevCtx.originalUrl !== ctx.originalUrl,
+  };
+};

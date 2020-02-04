d2bd69a60d083ea203fe950bf16a11dce9baf544
HsuTing <hsuting0106@gmail.com>
2020-02-04 11:54:46 +0800

    feat(koa-react): use new @mikojs/react-ssr

--- server/koa-react/src/utils/client.js
+++ server/koa-react/src/utils/client.js
@@ -1,70 +1,22 @@
 // @flow
 
-import React from 'react';
-import { hydrate } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { setConfig } from 'react-hot-loader';
-import { emptyFunction } from 'fbjs';
 
-import { mockChoice, handleUnhandledRejection } from '@mikojs/utils';
-
-import Root, { type propsType as rootPropsType } from 'components/Root';
+import { handleUnhandledRejection } from '@mikojs/utils';
+import client from '@mikojs/react-ssr/lib/client';
 
 import Main from 'templates/Main';
 import Loading from 'templates/Loading';
-import ErrorComponent from 'templates/Error';
+import Error from 'templates/Error';
 import routesData from 'templates/routesData';
 
 handleUnhandledRejection();
 setConfig({
-  errorReporter: ErrorComponent,
+  errorReporter: Error,
 });
-
-/**
- * @example
- * run()
- */
-const run = async () => {
-  const {
-    mainInitialProps,
-    pageInitialProps,
-    chunkName,
-  } = window.__MIKOJS_DATA__;
-  // preload page
-  const {
-    component: { loader },
-  } =
-    routesData.find(
-      ({
-        component: { chunkName: componentChunkName },
-      }: $ElementType<$PropertyType<rootPropsType<>, 'routesData'>, number>) =>
-        chunkName === componentChunkName,
-    ) ||
-    (() => {
-      throw new Error('Can not find page component');
-    })();
-  const { default: InitialPage } = await loader();
-
-  // render
-  hydrate(
-    <Router>
-      <Root
-        Main={Main}
-        Loading={Loading}
-        Error={ErrorComponent}
-        routesData={routesData}
-        InitialPage={InitialPage}
-        mainInitialProps={mainInitialProps}
-        pageInitialProps={pageInitialProps}
-      />
-    </Router>,
-    document.getElementById('__MIKOJS__') ||
-      (() => {
-        throw new Error('Can not find main HTMLElement');
-      })(),
-  );
-};
-
-mockChoice(process.env.NODE_ENV === 'test', emptyFunction, run);
-
-export default run;
+client({
+  Main,
+  Loading,
+  Error,
+  routesData,
+});

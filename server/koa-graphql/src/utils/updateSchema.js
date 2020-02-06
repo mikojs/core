509be35a714287029ce1344f71bdf4f17ffe3d8a
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2020-02-07 02:41:07 +0800

    (@mikojs/koa-graphql) Add more options to filter the files (#530)
    * feat(koa-graphql): add more options to filter the files
    
    * fix(koa-graphql): fix detail
    
    * improvement(koa-graphql): rewrite options
    
    * feat(koa-graphql): merge options
    
    * style(koa-graphql): fix code style
    
    * style(koa-graphql): fix code style

--- server/koa-graphql/src/utils/updateSchema.js
+++ server/koa-graphql/src/utils/updateSchema.js
@@ -3,32 +3,40 @@
 import path from 'path';
 
 import { type GraphQLSchema as GraphQLSchemaType } from 'graphql';
-import {
-  addResolveFunctionsToSchema,
-  type makeExecutableSchemaOptionsType,
-} from 'graphql-tools';
+import { addResolveFunctionsToSchema } from 'graphql-tools';
 import debug from 'debug';
 
 import { requireModule } from '@mikojs/utils';
+
+import { type optionsType } from '../index';
 
 const debugLog = debug('graphql:updateSchema');
 
 /**
  * @example
- * updateSchema('/folderPath', options, schema, '/filePath')
+ * updateSchema('/folderPath', {}, schema, '/filePath')
  *
  * @param {string} folderPath - the folder path
- * @param {makeExecutableSchemaOptionsType} options - build schema options
+ * @param {optionsType} options - koa graphql options
  * @param {GraphQLSchemaType} schema - built schema
  * @param {string} filePath - the file path
  */
 export default (
   folderPath: string,
-  { resolverValidationOptions }: makeExecutableSchemaOptionsType = {},
+  {
+    extensions = /\.js$/,
+    exclude,
+    makeExecutableSchemaOptions: { resolverValidationOptions } = {},
+  }: optionsType,
   schema: GraphQLSchemaType,
   filePath: string,
 ) => {
-  if (!new RegExp(path.resolve(folderPath)).test(filePath)) return;
+  if (
+    !extensions.test(filePath) ||
+    exclude?.test(filePath) ||
+    !new RegExp(path.resolve(folderPath)).test(filePath)
+  )
+    return;
 
   const newResolvers = requireModule(filePath);
 

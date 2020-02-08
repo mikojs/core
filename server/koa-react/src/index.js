1baca9efd376b63e123177d2bc1acc8e1dbe404f
HsuTing <hsuting0106@gmail.com>
2020-02-09 02:36:58 +0800

    feat(koa-react): add root functions

--- server/koa-react/src/index.js
+++ server/koa-react/src/index.js
@@ -1,7 +1,61 @@
 // @flow
 
+import path from 'path';
+
+import { d3DirTree } from '@mikojs/utils';
+import { type d3DirTreeNodeType } from '@mikojs/utils/lib/d3DirTree';
+
+import getCache from './utils/getCache';
+import writeClient from './utils/writeClient';
+
 export type optionsType = {|
   basename?: string,
   extensions?: RegExp,
   exclude?: RegExp,
 |};
+
+type returnType = {|
+  update: (filePath: string) => void,
+|};
+
+/**
+ * @example
+ * react('/')
+ *
+ * @param {string} folderPath - the folder path
+ * @param {optionsType} options - koa react options
+ *
+ * @return {returnType} - koa react functions
+ */
+export default (
+  folderPath: string,
+  // $FlowFixMe FIXME https://github.com/facebook/flow/issues/2977
+  options?: optionsType = {},
+): returnType => {
+  const cache = getCache(folderPath, options);
+  const { extensions = /\.js$/, exclude } = options;
+
+  d3DirTree(folderPath, {
+    extensions,
+    exclude,
+  })
+    .leaves()
+    .forEach(({ data: { path: filePath } }: d3DirTreeNodeType) => {
+      cache.addPage(filePath);
+    });
+  writeClient(cache);
+
+  return {
+    update: (filePath: string) => {
+      if (
+        !extensions.test(filePath) ||
+        exclude?.test(filePath) ||
+        !new RegExp(path.resolve(folderPath)).test(filePath)
+      )
+        return;
+
+      cache.addPage(filePath);
+      writeClient(cache);
+    },
+  };
+};

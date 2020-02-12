91a08970d571d22edcc48ee24e6873e1618b4687
HsuTing <hsuting0106@gmail.com>
2020-02-12 14:03:32 +0800

    test(koa-react): add Error rendering in server testing

---
+++ server/koa-react/src/utils/__tests__/__ignore__/ErrorComponent.js
@@ -0,0 +1,6 @@
+// @flow
+
+/** @react error component */
+export default () => {
+  throw new Error('error component');
+};
--- server/koa-react/src/utils/__tests__/buildServer.js
+++ server/koa-react/src/utils/__tests__/buildServer.js
@@ -4,6 +4,8 @@
  * @flow
  */
 
+import path from 'path';
+
 import Koa from 'koa';
 import webpack from 'koa-webpack';
 import getPort from 'get-port';
@@ -50,6 +52,27 @@ describe('build server', () => {
         },
       }).then((res: ResponseType) => res.text()),
     ).toBe('Not Found');
+
+    server.close();
+  });
+
+  test('render error', async () => {
+    const app = new Koa();
+    const port = await getPort();
+    const cache = getCache(__dirname, {});
+
+    cache.addPage(path.resolve(__dirname, './__ignore__/ErrorComponent.js'));
+    app.use(await webpack());
+    // $FlowFixMe TODO: can not extend koa context type
+    app.use(buildServer({}, cache));
+
+    const server = app.listen(port);
+
+    expect(
+      await fetch(
+        `http://localhost:${port}/__ignore__/ErrorComponent`,
+      ).then((res: ResponseType) => res.text()),
+    ).toMatch(/error component/);
 
     server.close();
   });

87a67c9e75117a707c2719b7eb59c941e0811b53
HsuTing <hsuting0106@gmail.com>
2020-02-16 21:53:32 +0800

    improvement(configs): add pritnInfo functions

--- packages/configs/src/cliOptions/index.js
+++ packages/configs/src/cliOptions/index.js
@@ -10,6 +10,8 @@ import { createLogger } from '@mikojs/utils';
 
 import { version } from '../../package.json';
 
+import printInfo from './printInfo';
+
 import configs from 'utils/configs';
 
 type optionType =
@@ -21,107 +23,8 @@ type optionType =
       cliName: string,
     |};
 
-type infoType = {|
-  alias?: string,
-  install?: $ReadOnlyArray<string>,
-  config?: {},
-  ignore?: {|
-    name?: string,
-    ignore?: $ReadOnlyArray<string>,
-  |},
-  run?: $ReadOnlyArray<string>,
-  env?: {},
-  configsFiles?: {},
-|};
-
 const debugLog = debug('configs:cliOptions');
 const logger = createLogger('@mikojs/configs');
-
-/**
- * @example
- * printInfo('cliName')
- *
- * @param {string} cliName - cli name
- *
- * @return {boolean} - test result
- */
-const printInfo = (cliName: ?string): boolean => {
-  const { info } = console;
-
-  if (cliName) {
-    const config = configs.get(cliName);
-
-    if (!config) {
-      logger
-        .fail(chalk`Can not find {cyan \`${cliName}\`} in configs`)
-        .fail(chalk`Use {green \`info\`} to get the more information`);
-      return false;
-    }
-
-    logger.info(
-      chalk`Here is thie information of the {cyan ${cliName}} config:`,
-    );
-    info();
-    info(
-      JSON.stringify(
-        (Object.keys(config): $ReadOnlyArray<string>).reduce(
-          (result: infoType, key: string): infoType => {
-            switch (key) {
-              case 'install':
-                return {
-                  ...result,
-                  install: config.install?.([]),
-                };
-
-              case 'run':
-                return {
-                  ...result,
-                  run: config.run?.([]),
-                };
-
-              case 'config':
-                return {
-                  ...result,
-                  // $FlowFixMe TODO: Flow does not yet support method or property calls in optional chains.
-                  config: config.config?.({}),
-                };
-
-              case 'ignore':
-                return {
-                  ...result,
-                  // $FlowFixMe TODO: Flow does not yet support method or property calls in optional chains.
-                  ignore: config.ignore?.(),
-                };
-
-              default:
-                return {
-                  ...result,
-                  [key]: config[key],
-                };
-            }
-          },
-          // $FlowFixMe TODO: https://github.com/facebook/flow/issues/2977
-          ({}: infoType),
-        ),
-        null,
-        2,
-      )
-        .split(/\n/)
-        .map((text: string) => `  ${text}`)
-        .join('\n'),
-    );
-    info();
-  } else {
-    logger.info('Here are the all config names which you can use:');
-    info();
-    configs.all().forEach((key: string) => {
-      info(`  - ${key}`);
-    });
-    info();
-  }
-
-  return true;
-};
 
 /**
  * @example
@@ -287,7 +190,7 @@ export default async (argv: $ReadOnlyArray<string>): Promise<optionType> =>
       .usage(chalk`{green [command-type]}`)
       .description('print more info about configs')
       .action((cliName: ?string) => {
-        resolve(printInfo(cliName));
+        resolve(printInfo(cliName, logger));
       });
 
     program

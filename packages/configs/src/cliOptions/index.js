15a90c9b9e51abb56c9b07190c271f291e9a68e3
HsuTing <hsuting0106@gmail.com>
2020-02-17 00:41:23 +0800

    feat(configs): add getOptions function

--- packages/configs/src/cliOptions/index.js
+++ packages/configs/src/cliOptions/index.js
@@ -2,8 +2,6 @@
 
 import commander from 'commander';
 import chalk from 'chalk';
-import debug from 'debug';
-import npmWhich from 'npm-which';
 import { emptyFunction } from 'fbjs';
 
 import { createLogger } from '@mikojs/utils';
@@ -12,105 +10,11 @@ import { version } from '../../package.json';
 
 import printInfo from './printInfo';
 import validateCliName from './validateCliName';
+import getOptions, { type optionType } from './getOptions';
 
 import configs from 'utils/configs';
 
-type optionType =
-  | boolean
-  | {|
-      cli: string,
-      argv: $ReadOnlyArray<string>,
-      env: {},
-      cliName: string,
-    |};
-
-const debugLog = debug('configs:cliOptions');
 const logger = createLogger('@mikojs/configs');
-
-/**
- * @example
- * fileOptions('--key', 'key', '--key')
- *
- * @param {string} optionKey - option key to test
- * @param {string} arg - current argument
- * @param {string} prevArg - prev argument
- *
- * @return {boolean} - test result
- */
-const filterOptions = (optionKey: ?string, arg: string, prevArg: string) =>
-  !optionKey
-    ? false
-    : optionKey === arg ||
-      optionKey === prevArg ||
-      new RegExp(`^${optionKey}=`).test(arg);
-
-/**
- * @example
- * getOptions({ cliName: 'cliName', ... })
- *
- * @param {object} options - the options from the commander
- *
- * @return {optionType} - cli options
- */
-const getOptions = ({
-  cliName,
-  configsFiles,
-  rawArgs,
-  options,
-}: {|
-  cliName: string,
-  configsFiles?: $ReadOnlyArray<string>,
-  rawArgs: $ReadOnlyArray<string>,
-  options: $ReadOnlyArray<{|
-    short?: string,
-    long: string,
-  |}>,
-|}): optionType => {
-  if (!validateCliName(logger, cliName)) return false;
-
-  configs.addConfigsFilesToConfig(cliName, configsFiles || []);
-
-  const {
-    alias: cli = cliName,
-    run = emptyFunction.thatReturnsArgument,
-    env = {},
-  } = configs.get(cliName);
-  const rawArgsFiltered = rawArgs
-    .slice(2)
-    .filter(
-      (arg: string, index: number, allArgs: $ReadOnlyArray<string>) =>
-        arg !== cliName &&
-        !options.some(
-          ({ short, long }: {| short?: string, long: string |}) =>
-            filterOptions(short, arg, allArgs[index - 1]) ||
-            filterOptions(long, arg, allArgs[index - 1]),
-        ),
-    );
-
-  debugLog({ rawArgsFiltered });
-
-  try {
-    const result = {
-      cli:
-        typeof cli !== 'function'
-          ? npmWhich(process.cwd()).sync(cli)
-          : cli([cliName, ...rawArgsFiltered]),
-      argv: run(rawArgsFiltered),
-      env,
-      cliName,
-    };
-
-    debugLog(result);
-
-    return result;
-  } catch (e) {
-    if (!/not found/.test(e.message)) throw e;
-
-    logger.fail(e.message.replace(/not found/, 'Not found cli'));
-
-    return false;
-  }
-};
 
 /**
  * @example
@@ -155,7 +59,11 @@ export default async (argv: $ReadOnlyArray<string>): Promise<optionType> =>
             |}>,
           |},
         ) => {
-          resolve(getOptions({ ...options, cliName }));
+          resolve(
+            !validateCliName(logger, cliName)
+              ? false
+              : getOptions(logger, { ...options, cliName }),
+          );
         },
       );
 

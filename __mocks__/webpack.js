2265ca60262788b08dee96832de5fae1eaa35766
HsuTing <hsuting0106@gmail.com>
2020-02-25 10:28:19 +0800

    test(koa-react): add more testing

--- __mocks__/webpack.js
+++ __mocks__/webpack.js
@@ -1,32 +1,9 @@
 // @flow
 
-const mockCallbackArguments = jest.fn().mockReturnValue([
-  null,
-  {
-    hasErrors: () => false,
-    toJson: () => ({
-      assetsByChunkName: {
-        commons: '/commons',
-        client: '/client',
-      },
-    }),
-  },
-]);
+const webpack = jest.fn().mockReturnValue({
+  run: jest.fn(),
+});
 
-const webpack: JestMockFn<
-  [{||}, (err: ?Error, stats: {}) => void],
-  void,
-> = jest
-  .fn()
-  .mockImplementation(
-    (config: {||}, callback: (err: ?Error, stats: {}) => void) => {
-      callback(...mockCallbackArguments());
-    },
-  );
-
-// $FlowFixMe TODO: flow not support
 webpack.EnvironmentPlugin = class EnvironmentPlugin {};
-// $FlowFixMe TODO: flow not support
-webpack.mockCallbackArguments = mockCallbackArguments;
 
 export default webpack;

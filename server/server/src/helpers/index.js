9293d4a94bfa3225c7c6e2a2a8d717bca88f9b42
HsuTing <hsuting0106@gmail.com>
2020-03-03 17:31:47 +0800

    fix(server): auto mock helper.run in testing mode

--- server/server/src/helpers/index.js
+++ server/server/src/helpers/index.js
@@ -2,7 +2,9 @@
 
 import path from 'path';
 
-import { requireModule } from '@mikojs/utils';
+import { emptyFunction } from 'fbjs';
+
+import { requireModule, mockChoice } from '@mikojs/utils';
 
 /**
  * @example
@@ -14,13 +16,23 @@ export default (): (<C>(helperName: string) => C) => {
   const cache = {};
 
   return <+C>(helperName: string): C => {
-    if (!cache[helperName])
-      cache[helperName] = requireModule<() => C>(
+    if (!cache[helperName]) {
+      const helper = requireModule<() => C & { run: () => void }>(
         path.resolve(
           __dirname,
           `build${helperName[0].toUpperCase()}${helperName.slice(1)}`,
         ),
       )();
+
+      cache[helperName] = {
+        ...helper,
+        run: mockChoice(
+          process.env.NODE_ENV === 'test',
+          emptyFunction,
+          helper.run,
+        ),
+      };
+    }
 
     return cache[helperName];
   };

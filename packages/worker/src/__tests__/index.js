d22947514f3a75275c47a2a32a13fb4a21cba5ab
HsuTing <hsuting0106@gmail.com>
2020-03-17 10:28:40 +0800

    test(worker): simplify testing

--- packages/worker/src/__tests__/index.js
+++ packages/worker/src/__tests__/index.js
@@ -13,78 +13,98 @@ import buildServer from '../utils/buildServer';
 import { start, func, end } from './__ignore__/worker';
 
 describe('worker', () => {
-  test('main server', async () => {
-    const expecteds = [
-      { key: 'value' },
-      ['test'],
-      'test',
-      '-1',
-      '0',
-      '1',
-      -1,
-      0,
-      1,
-      true,
-      false,
-      undefined,
-      null,
-    ];
+  describe('main server', () => {
     let port: number;
+    let worker: {| start: typeof start, func: typeof func, end: typeof end |};
 
-    expecteds.forEach((expected: mixed) => {
-      func.mockReturnValueOnce(expected);
+    beforeAll(async () => {
+      findProcess.mockReturnValue([]);
+      execa.mockImplementation((filePath: string, [serverPort]: [number]): {|
+        unref: JestMockFn<$ReadOnlyArray<void>, void>,
+      |} => {
+        buildServer(serverPort).unref();
+        port = serverPort;
+
+        return {
+          unref: jest.fn(),
+        };
+      });
+
+      worker = await buildWorker(
+        path.resolve(__dirname, './__ignore__/worker.js'),
+      );
     });
-    func
-      .mockImplementationOnce((stdout: stream$Writable) => {
+
+    test('start', async () => {
+      expect(port !== (await getPort({ port }))).toBeTruthy();
+    });
+
+    test.each`
+      expected
+      ${{ key: 'value' }}
+      ${['test']}
+      ${'test'}
+      ${'-1'}
+      ${'0'}
+      ${'1'}
+      ${-1}
+      ${0}
+      ${1}
+      ${true}
+      ${false}
+      ${undefined}
+      ${null}
+    `(
+      'func with expected = $expected',
+      async ({ expected }: {| expected: mixed |}) => {
+        func.mockReturnValueOnce(expected);
+
+        expect(await worker.func()).toEqual(expected);
+      },
+    );
+
+    test('func with stdout', async () => {
+      const output = [];
+
+      func.mockImplementationOnce((stdout: stream$Writable) => {
         stdout.write('test');
-      })
-      .mockImplementation(() => {
+      });
+
+      expect(
+        await worker.func(
+          new stream.Writable({
+            write: (
+              chunk: Buffer | string,
+              encoding: string,
+              callback: () => void,
+            ) => {
+              if (Buffer.isBuffer(chunk)) output.push(chunk.toString());
+
+              callback();
+            },
+          }),
+        ),
+      ).toBeUndefined();
+      expect(output).toEqual(['t', 'e', 's', 't']);
+    });
+
+    test('func with error', async () => {
+      func.mockImplementation(() => {
         throw new Error('error');
       });
-    findProcess.mockReturnValue([]);
-    execa.mockImplementation((filePath: string, [serverPort]: [number]): {|
-      unref: JestMockFn<$ReadOnlyArray<void>, void>,
-    |} => {
-      buildServer(serverPort).unref();
-      port = serverPort;
 
-      return {
-        unref: jest.fn(),
-      };
+      await expect(worker.func()).rejects.toThrow('error');
     });
 
-    const worker = await buildWorker(
-      path.resolve(__dirname, './__ignore__/worker.js'),
-    );
-    const stdout = [];
+    test('end', async () => {
+      expect(await worker.end()).toBeUndefined();
+    });
 
-    expect(port !== (await getPort({ port }))).toBeTruthy();
-
-    await expecteds.reduce(
-      (result: Promise<void>, expected: mixed) =>
-        result
-          .then(() => worker.func())
-          .then((data: mixed) => expect(data).toEqual(expected)),
-      Promise.resolve(),
-    );
-    expect(
-      await worker.func(
-        new stream.Writable({
-          write: (chunk: Buffer, encoding: string, callback: () => void) => {
-            if (Buffer.isBuffer(chunk)) stdout.push(chunk.toString());
-
-            callback();
-          },
-        }),
-      ),
-    ).toBeUndefined();
-    await expect(worker.func()).rejects.toThrow('error');
-    expect(await worker.end()).toBeUndefined();
-
-    expect(start).not.toHaveBeenCalled();
-    expect(func).toHaveBeenCalledTimes(expecteds.length + 2);
-    expect(end).not.toHaveBeenCalled();
-    expect(stdout).toEqual(['t', 'e', 's', 't']);
+    test('final count', () => {
+      expect(start).not.toHaveBeenCalled();
+      expect(func).toHaveBeenCalledTimes(13 + 2);
+      expect(end).not.toHaveBeenCalled();
+    });
   });
 
   test('not main server', async () => {

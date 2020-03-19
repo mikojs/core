236c395c1ef1735345e903eed51f3bad624700b8
HsuTing <hsuting0106@gmail.com>
2020-03-19 11:28:37 +0800

    improvement(miko): can install dependencies and devDependencies manually

--- packages/miko/src/configs/index.js
+++ packages/miko/src/configs/index.js
@@ -51,16 +51,24 @@ export default (
       ? configsArrayOrOne
       : [configsArrayOrOne]),
     {
-      install: (): $ReadOnlyArray<string> => {
+      install: () => ['install:dependencies', '&&', 'install:devDependencies'],
+      'install:dependencies': (): $ReadOnlyArray<string> => {
         const dependencies = ['yarn', 'install'];
-        const devDependencies = ['yarn', 'install', '--dev'];
 
         Object.keys(configs).forEach((key: string) => {
           dependencies.push(...configs[key][1].dependencies);
+        });
+
+        return dependencies.length === 2 ? [] : dependencies;
+      },
+      'install:devDependencies': (): $ReadOnlyArray<string> => {
+        const devDependencies = ['yarn', 'install'];
+
+        Object.keys(configs).forEach((key: string) => {
           devDependencies.push(...configs[key][1].devDependencies);
         });
 
-        return [...dependencies, '&&', ...devDependencies];
+        return devDependencies.length === 2 ? [] : devDependencies;
       },
     },
   ].reduce(

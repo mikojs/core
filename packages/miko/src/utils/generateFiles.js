8b97cda9f96fc83dfb9ad142b3ca8b7b58e04858
HsuTing <hsuting0106@gmail.com>
2020-03-25 22:26:23 +0800

    feat(miko): add gitignore warning

--- packages/miko/src/utils/generateFiles.js
+++ packages/miko/src/utils/generateFiles.js
@@ -1,12 +1,17 @@
 // @flow
 
+import fs from 'fs';
 import path from 'path';
 
-import outputFileSync from 'output-file-sync';
+import chalk from 'chalk';
 import debug from 'debug';
+import outputFileSync from 'output-file-sync';
+
+import { createLogger } from '@mikojs/utils';
 
 import { type cacheType } from './buildCache';
 
+const logger = createLogger('@mikojs/miko');
 const debugLog = debug('miko:generateFiles');
 
 /**
@@ -17,8 +22,13 @@ const debugLog = debug('miko:generateFiles');
  * @param {cacheType} cache - configs cache
  */
 export default (cwd: string, cache: cacheType) => {
-  debugLog(cwd);
+  const gitFilePath = path.resolve(cwd, './.gitignore');
+  const gitignore = (!fs.existsSync(gitFilePath)
+    ? ''
+    : fs.readFileSync(gitFilePath, 'utf-8')
+  ).replace(/^#.*$/gm, '');
 
+  debugLog(cwd);
   cache.keys().forEach((key: string) => {
     const { filenames, config, ignore } = cache.get(key);
     const configFilename = filenames?.config;
@@ -37,6 +47,13 @@ export default (cwd: string, cache: cacheType) => {
     ]
       .filter(Boolean)
       .forEach((argu: [string, string]) => {
+        const filename = path.basename(argu[0]);
+
+        if (!new RegExp(filename).test(gitignore))
+          logger.warn(
+            chalk`{red ${filename}} should be added in {bold {gray .gitignore}}`,
+          );
+
         debugLog(argu);
         outputFileSync(...argu);
       });

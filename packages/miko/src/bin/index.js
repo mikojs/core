e129cf0bbcf975e6290abbbbc89479c44dc0a167
HsuTing <hsuting0106@gmail.com>
2020-03-26 14:19:07 +0800

    feat(miko): add events to cli

--- packages/miko/src/bin/index.js
+++ packages/miko/src/bin/index.js
@@ -1,23 +1,35 @@
 #! /usr/bin/env node
 // @flow
 
+import path from 'path';
+
 import { handleUnhandledRejection } from '@mikojs/utils';
+import buildWorker from '@mikojs/worker';
 
 import getOptions from 'utils/getOptions';
+import generateFiles from 'utils/generateFiles';
+import removeFiles from 'utils/removeFiles';
+import typeof * as workerType from 'utils/worker';
 
 handleUnhandledRejection();
 
 (async () => {
   const { type } = await getOptions(process.argv);
+  const worker = await buildWorker<workerType>(
+    path.resolve(__dirname, '../utils/worker'),
+  );
 
   switch (type) {
     case 'end':
+      if (await worker.removeTracking()) await removeFiles();
       break;
 
     case 'init':
+      // TODO: initialize commands in package.json
       break;
 
     default:
+      generateFiles();
       break;
   }
 })();

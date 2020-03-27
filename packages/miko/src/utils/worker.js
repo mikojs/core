a15a58faf8565145c85c824cf9b813c14adb43ba
HsuTing <hsuting0106@gmail.com>
2020-03-27 11:48:33 +0800

    feat(miko): can close server auto

--- packages/miko/src/utils/worker.js
+++ packages/miko/src/utils/worker.js
@@ -1,13 +1,44 @@
 // @flow
 
-// TODO import isRunning from 'is-running';
+import fs from 'fs';
+
+import isRunning from 'is-running';
 import rimraf from 'rimraf';
 import debug from 'debug';
 
 import { end } from '@mikojs/worker';
 
+export const TIME_TO_CHECK = 100;
+export const TIME_TO_REMOVE_FILES = 500;
+export const TIME_TO_CLOSE_SERVER = 5000;
+
 const debugLog = debug('miko:worker');
 const cache = {};
+let timer: TimeoutID;
+
+/**
+ * @example
+ * removeFilePath('/filePath', () => {})
+ *
+ * @param {string} filePath - file path
+ * @param {Function} callback - callback function
+ */
+const removeFilePath = (filePath: string, callback: () => void) => {
+  if (!fs.existsSync(filePath)) {
+    delete cache[filePath];
+    debugLog(`File does not exist: ${filePath}`);
+    debugLog(cache);
+    callback();
+    return;
+  }
+
+  rimraf(filePath, () => {
+    delete cache[filePath];
+    debugLog(`Remove existing file: ${filePath}`);
+    debugLog(cache);
+    callback();
+  });
+};
 
 /**
  * @example
@@ -22,8 +53,64 @@ export const addTracking = (pid: number, filePath: string) => {
   cache[filePath].push(pid);
   debugLog({ pid, filePath });
   debugLog(cache);
+  clearTimeout(timer);
 
-  // TODO: auto close server
+  /**
+   * @example
+   * checking(0)
+   *
+   * @param {number} checkedTimes - checked times
+   */
+  const checking = (checkedTimes: number) => {
+    const hasWorkingPids = Object.keys(cache).reduce(
+      (result: boolean, cacheFilePath: string): boolean => {
+        const newPids = cache[cacheFilePath].filter(isRunning);
+
+        if (newPids.length !== cache[cacheFilePath].length)
+          debugLog(
+            `Cache: ${JSON.stringify(
+              { ...cache, [cacheFilePath]: newPids },
+              null,
+              2,
+            )}`,
+          );
+
+        cache[cacheFilePath] = newPids;
+
+        return result || newPids.length !== 0;
+      },
+      false,
+    );
+
+    if (hasWorkingPids) {
+      timer = setTimeout(checking, TIME_TO_CHECK, 0);
+      return;
+    }
+
+    if (
+      checkedTimes >= TIME_TO_REMOVE_FILES / TIME_TO_CHECK &&
+      Object.keys(cache).length !== 0
+    ) {
+      removeFilePath(Object.keys(cache)[0], () => {
+        timer = setTimeout(
+          checking,
+          TIME_TO_CHECK,
+          Object.keys(cache).length === 0 ? 0 : checkedTimes,
+        );
+      });
+      return;
+    }
+
+    if (checkedTimes >= TIME_TO_CLOSE_SERVER / TIME_TO_CHECK) {
+      clearTimeout(timer);
+      end(__filename);
+      return;
+    }
+
+    timer = setTimeout(checking, TIME_TO_CHECK, checkedTimes + 1);
+  };
+
+  checking(0);
 };
 
 /**
@@ -42,10 +129,7 @@ export const killAllEvents = async () => {
           cache[filePath].forEach((pid: number) => {
             process.kill(pid, 0);
           });
-          rimraf(filePath, () => {
-            delete cache[filePath];
-            resolve();
-          });
+          removeFilePath(filePath, resolve);
         }),
     ),
   );

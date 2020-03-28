77e344224451a7bf70d15ec1b13dbf29a3087135
HsuTing <hsuting0106@gmail.com>
2020-03-28 11:28:41 +0800

    style(badges): modify parsing way

--- packages/badges/src/utils/__tests__/getOptions.js
+++ packages/badges/src/utils/__tests__/getOptions.js
@@ -5,18 +5,20 @@ import getOptions from '../getOptions';
 const defaultArgv = ['node', 'badges'];
 
 describe('get options', () => {
-  test('work', () => {
-    expect(getOptions([...defaultArgv, 'readme-path'])).toEqual([
-      'readme-path',
-    ]);
+  test.each`
+    argv
+    ${['readmePath']}
+    ${['readmePath1', 'readmePath2']}
+  `('run with $argv', async ({ argv }: {| argv: $ReadOnlyArray<string> |}) => {
+    expect(await getOptions([...defaultArgv, ...argv])).toEqual(argv);
   });
 
-  test('not give readme path', () => {
+  test('not give readme path', async () => {
     const mockLog = jest.fn();
 
     global.console.error = mockLog;
 
-    expect(getOptions(defaultArgv)).toEqual([]);
+    expect(await getOptions(defaultArgv)).toEqual([]);
     expect(mockLog).toHaveBeenCalled();
   });
 });

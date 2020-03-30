7d26222d74aef9f29dbe495c2a8b3ad727248c53
HsuTing <hsuting0106@gmail.com>
2020-03-30 13:38:39 +0800

    feat(core): migrate old config to new config

--- .mikorc.js
+++ .mikorc.js
@@ -1,3 +1,75 @@
 // @flow
 
-module.exports = [];
+/* eslint-disable flowtype/require-return-type */
+/* eslint-disable flowtype/require-parameter-type */
+/* eslint-disable jsdoc/require-jsdoc */
+
+/* eslint-disable import/no-extraneous-dependencies */
+const defaultConfigs = require('@mikojs/configs/lib/configs');
+/* eslint-enable import/no-extraneous-dependencies */
+
+const extendConfigs = require('./.catrc');
+
+const migrateConfigs = configs =>
+  Object.keys(configs).reduce((result, key) => {
+    switch (key) {
+      case 'exec':
+      case 'lint:watch':
+      case 'test':
+      case 'server':
+        return result;
+
+      default: {
+        const config = configs[key];
+        const output = typeof config === 'function' ? config : {};
+
+        if (config.config) output.config = config.config;
+
+        if (config.ignore)
+          output.ignore = ignore => config.ignore({ ignore }).ignore;
+
+        if (config.filenames) output.filenames = config.filenames;
+
+        return {
+          ...result,
+          [key]: output,
+        };
+      }
+    }
+  }, {});
+
+module.exports = [
+  {
+    babel: {
+      filenames: {
+        config: 'babel.config.js',
+      },
+    },
+    jest: {
+      filenames: {
+        config: 'jest.config.js',
+      },
+    },
+    lint: {
+      filenames: {
+        config: '.eslintrc.js',
+        ignore: '.eslintignore',
+      },
+    },
+    prettier: {
+      filenames: {
+        config: '.prettierrc.js',
+        ignore: '.prettierignore',
+      },
+    },
+    lintStaged: {
+      filenames: {
+        config: '.lintstagedrc.js',
+      },
+    },
+  },
+  defaultConfigs,
+  ...extendConfigs,
+].map(config =>
+  config instanceof Array ? config.map(migrateConfigs) : migrateConfigs(config),
+);

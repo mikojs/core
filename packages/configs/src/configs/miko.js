b79c18c63447d5e6bab2b702c3ee26962ec29ee3
HsuTing <hsuting0106@gmail.com>
2020-04-12 23:14:16 +0800

    improvement(configs): modify miko command descriptions

--- packages/configs/src/configs/miko.js
+++ packages/configs/src/configs/miko.js
@@ -9,35 +9,35 @@
 export default () => ({
   build: {
     command: 'babel src -d lib --verbose',
-    description: 'run babel in the build mode',
+    description: 'run `babel` in the build mode',
   },
   dev: {
     command: 'miko build -w',
-    description: 'run babel in the dev mode',
+    description: 'run `babel` in the dev mode',
   },
   prod: {
     command: 'NODE_ENV=production miko build',
-    description: 'run babel in the production mode',
+    description: 'run `babel` in the production mode',
   },
   prettier: {
     command: 'prettier --write',
-    description: 'run prettier with `--write` option',
+    description: 'run `prettier` with `--write` option',
   },
   lint: {
     command: 'esw --cache --color',
-    description: 'run eslint with generating the cache',
+    description: 'run `eslint` with generating the cache',
   },
   'lint:watch': {
     command: 'miko lint -w --quiet --rule "prettier/prettier: off"',
-    description: 'run eslint in the watch mode',
+    description: 'run `eslint` in the watch mode',
   },
   jest: {
     command: 'jest --coverage=false',
-    description: 'run jest without generating the coverage',
+    description: 'run `jest` without generating the coverage',
   },
   test: {
     command: 'jest --silent',
-    description: 'run jest in the silent mode',
+    description: 'run `jest` in the silent mode',
   },
   'flow-typed': {
     command: 'flow-typed install --verbose',
@@ -45,15 +45,15 @@ export default () => ({
   },
   'husky:pre-commit': {
     command: 'miko build && miko lint-staged && flow',
-    description: 'checking the code style before git commit',
+    description: 'checking the code style before running `git commit`',
   },
   'husky:post-merge': {
     command: 'miko build',
-    description: 'rebuild package after git merge',
+    description: 'rebuild the package after running `git merge`',
   },
   'husky:post-checkout': {
     command: 'miko build',
-    description: 'rebuild package after git checkout',
+    description: 'rebuild the package after running `git checkout`',
   },
   release: {
     command: 'standard-version',

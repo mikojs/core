5020f4f07db711ad4d69ec7e13c399dd572548f0
HsuTing <hsuting0106@gmail.com>
2020-04-19 22:30:45 +0800

    test(configs): add more testings

--- packages/configs/src/normalize/__tests__/babel.js
+++ packages/configs/src/normalize/__tests__/babel.js
@@ -2,9 +2,13 @@
 
 import babel, { type presetOrPluginType } from '../babel';
 
-test('babel', () => {
+test.each`
+  presets
+  ${['env']}
+  ${[['env']]}
+`('babel', ({ presets }: {| presets: $ReadOnlyArray<string> |}) => {
   expect(
-    babel.presetOrPlugin('preset', ['env'], {
+    babel.presetOrPlugin('preset', presets, {
       'babel-preset-env': ([preset]: presetOrPluginType) => [preset],
     }),
   ).toEqual(['env']);
--- packages/configs/src/normalize/__tests__/lint.js
+++ packages/configs/src/normalize/__tests__/lint.js
@@ -2,16 +2,17 @@
 
 import lint, { type rulesType } from '../lint';
 
-test('lint', () => {
+test.each`
+  rules
+  ${{ 'jsdoc/check-tag-names': 'error' }}
+  ${{ 'jsdoc/check-tag-names': ['error'] }}
+`('lint', ({ rules }: {| rules: $ReadOnlyArray<string> |}) => {
   expect(
-    lint.rules(
-      { 'jsdoc/check-tag-names': 'error' },
-      {
-        'jsdoc/check-tag-names': ([rule]: $NonMaybeType<
-          $PropertyType<rulesType, 'jsdoc/check-tag-names'>,
-        >) => [rule || 'error'],
-      },
-    ),
+    lint.rules(rules, {
+      'jsdoc/check-tag-names': ([rule]: $NonMaybeType<
+        $PropertyType<rulesType, 'jsdoc/check-tag-names'>,
+      >) => [rule || 'error'],
+    }),
   ).toEqual({
     'jsdoc/check-tag-names': 'error',
   });

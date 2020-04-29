e2c44173ee6c3d4a166af15119a04100177b45e5
HsuTing <hsuting0106@gmail.com>
2020-04-30 01:12:10 +0800

    test(miko): add more testing

--- packages/miko/src/utils/__tests__/getOptions.js
+++ packages/miko/src/utils/__tests__/getOptions.js
@@ -8,11 +8,11 @@ import getOptions, { type optionsType } from '../getOptions';
 import cache from '../cache';
 
 const command =
-  'yarn install && lerna exec \'echo "test" && echo "test test"\' --stream && echo "test test \'test\'test" && echo "test"';
+  'yarn install && lerna exec \'echo "test" && echo "test test"\' --stream && echo "test test \'"test"test\'test" && echo "test"';
 const expectedCommand = [
   ['yarn', 'install'],
   ['lerna', 'exec', '\'echo "test" && echo "test test"\'', '--stream'],
-  ['echo', '"test test \'test\'test"'],
+  ['echo', '"test test \'"test"test\'test"'],
   ['echo', '"test"'],
 ];
 

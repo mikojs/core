605d96dc14f799350706059fe0b4a8f50c69c834
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2020-04-30 01:48:51 +0800

    (@mikojs/core) Add more type checking (#594)
    * feat(core): add more type checking
    
    * feat(configs): add flow command
    
    * fix(core): fix flow type error
    
    * test(configs): add more testing
    
    * test(miko): add more testing
    
    * fix(configs): fix prettier and flow type error

--- __mocks__/fbjs/lib/getElementPosition.js
+++ __mocks__/fbjs/lib/getElementPosition.js
@@ -1,3 +1,6 @@
 // @flow
 
-export default jest.fn().mockReturnValue({ width: 0, height: 0 });
+export default (jest.fn().mockReturnValue({ width: 0, height: 0 }): JestMockFn<
+  $ReadOnlyArray<void>,
+  {| width: number, height: number |},
+>);
--- __mocks__/react-dnd-cjs.js
+++ __mocks__/react-dnd-cjs.js
@@ -3,16 +3,19 @@
 import { emptyFunction } from 'fbjs';
 
 // TODO: https://github.com/react-dnd/react-dnd/issues/1540
-export const useDrag = jest
-  .fn()
-  .mockReturnValue([{ isDragging: false }, emptyFunction]);
+export const useDrag: JestMockFn<
+  $ReadOnlyArray<void>,
+  [{| isDragging: boolean |}, () => void],
+> = jest.fn().mockReturnValue([{ isDragging: false }, emptyFunction]);
 
-export const useDrop = jest
-  .fn()
-  .mockReturnValue([{ isOver: false }, emptyFunction]);
+export const useDrop: JestMockFn<
+  $ReadOnlyArray<void>,
+  [{| isOver: boolean |}, () => void],
+> = jest.fn().mockReturnValue([{ isOver: false }, emptyFunction]);
 
-export const useDragLayer = jest
-  .fn()
-  .mockReturnValue({ isOneOfItemDragging: false });
+export const useDragLayer: JestMockFn<
+  $ReadOnlyArray<void>,
+  {| isOneOfItemDragging: boolean |},
+> = jest.fn().mockReturnValue({ isOneOfItemDragging: false });
 
 export const { DndContext, DndProvider } = jest.requireActual('react-dnd-cjs');

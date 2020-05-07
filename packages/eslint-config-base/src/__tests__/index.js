9a81ca6db678f0fdde2100a60eb6f2d73c66a512
HsuTing <hsuting0106@gmail.com>
2020-05-08 00:11:35 +0800

    fix(utils): fix flow type error

--- packages/eslint-config-base/src/__tests__/index.js
+++ packages/eslint-config-base/src/__tests__/index.js
@@ -6,7 +6,8 @@ import path from 'path';
 import { CLIEngine } from 'eslint';
 import { hyphenate } from 'fbjs';
 
-import { d3DirTree, type d3DirTreeNodeType } from '@mikojs/utils';
+import { d3DirTree } from '@mikojs/utils';
+import { type d3DirTreeNodeType } from '@mikojs/utils/lib/d3DirTree';
 
 /**
  * FIXME:

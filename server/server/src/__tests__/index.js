fe3d5d496cecb1ed0ae85f926b79281422c0a6fe
HsuTing <hsuting0106@gmail.com>
2020-05-11 16:01:08 +0800

    test(server): add more testing

--- server/server/src/__tests__/index.js
+++ server/server/src/__tests__/index.js
@@ -6,25 +6,53 @@ import http from 'http';
 import getPort from 'get-port';
 import fetch, { type Body as BodyType } from 'node-fetch';
 
+import { mockUpdate, type mergeDirEventType } from '@mikojs/utils/lib/mergeDir';
+
 import buildApi from '../index';
 
 describe('server', () => {
+  beforeEach(() => {
+    mockUpdate.clear();
+  });
+
   test.each`
-    pathname             | canFind
-    ${'/api'}            | ${true}
-    ${'/id'}             | ${true}
-    ${'/test/not-found'} | ${false}
-    ${'/test/api'}       | ${true}
+    pathname             | canFind  | updateEvent
+    ${'/api'}            | ${true}  | ${'init'}
+    ${'/id'}             | ${true}  | ${'init'}
+    ${'/test/not-found'} | ${false} | ${'init'}
+    ${'/test/api'}       | ${true}  | ${'init'}
+    ${'/api'}            | ${false} | ${'unlink'}
+    ${'/api'}            | ${true}  | ${'error'}
   `(
     'fetch $pathname',
-    async ({ pathname, canFind }: {| pathname: string, canFind: boolean |}) => {
+    async ({
+      pathname,
+      canFind,
+      updateEvent,
+    }: {|
+      pathname: string,
+      canFind: boolean,
+      updateEvent: mergeDirEventType,
+    |}) => {
+      const folderPath = path.resolve(__dirname, './__ignore__');
       const server = http.createServer(
-        buildApi(path.resolve(__dirname, './__ignore__')),
+        buildApi(
+          folderPath,
+          updateEvent === 'init' ? undefined : { dev: true },
+        ),
       );
       const port = await getPort();
       const url = `http://localhost:${port}${pathname}`;
 
       server.listen(port);
+
+      if (updateEvent !== 'init') {
+        expect(mockUpdate.cache).toHaveLength(1);
+        mockUpdate.cache[0](
+          updateEvent,
+          path.resolve(folderPath, `.${pathname}`),
+        );
+      }
 
       expect(
         await fetch(url).then((res: BodyType) =>

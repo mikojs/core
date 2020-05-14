7db25ede1abbe8873bb930eceb884c8208b454d9
HsuTing <hsuting0106@gmail.com>
2020-05-14 09:42:14 +0800

    feat(server): add error message

--- server/server/src/bin/index.js
+++ server/server/src/bin/index.js
@@ -4,15 +4,22 @@
 import path from 'path';
 
 import ora from 'ora';
+import debug from 'debug';
 
 import { createLogger } from '@mikojs/utils';
 
 import buildApi from '../index';
 import buildCli from '../buildCli';
 
-buildCli(
-  process.argv,
-  path.resolve('./api'),
-  createLogger('@mikojs/server', ora({ discardStdin: false })),
-  buildApi,
-);
+const logger = createLogger('@mikojs/server', ora({ discardStdin: false }));
+const debugLog = debug('server:bin');
+
+(async () => {
+  try {
+    await buildCli(process.argv, path.resolve('./api'), logger, buildApi);
+  } catch (e) {
+    debugLog(e);
+    logger.fail(e.message);
+    process.exit(1);
+  }
+})();

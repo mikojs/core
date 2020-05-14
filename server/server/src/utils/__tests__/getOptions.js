6ddc1b0b74311245ed5b421277d826048f078523
HsuTing <hsuting0106@gmail.com>
2020-05-14 14:51:06 +0800

    test(server): fix testing error

--- server/server/src/utils/__tests__/getOptions.js
+++ server/server/src/utils/__tests__/getOptions.js
@@ -5,8 +5,8 @@ import getOptions, { type optionsType } from '../getOptions';
 describe('get options', () => {
   test.each`
     argv                      | expected
-    ${[]}                     | ${{ port: 8000, folderPath: './' }}
-    ${['-p', '8080']}         | ${{ port: 8080, folderPath: './' }}
+    ${[]}                     | ${{ port: 8000, folderPath: __filename }}
+    ${['-p', '8080']}         | ${{ port: 8080, folderPath: __filename }}
     ${['-f', './folderPath']} | ${{ port: 8000, folderPath: './folderPath' }}
   `(
     'run $argv',

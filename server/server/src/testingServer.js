97bc60e089c7431b80ce77a8804f4e88c5064764
HsuTing <hsuting0106@gmail.com>
2020-05-19 21:43:15 +0800

    feat(server): add testing server

---
+++ server/server/src/testingServer.js
@@ -0,0 +1,41 @@
+// @flow
+
+import http from 'http';
+
+import getPort from 'get-port';
+import fetch, { type Body as BodyType } from 'node-fetch';
+
+import { type middlewareType } from './index';
+
+type cacheType = {|
+  port: number,
+  server?: http.Server,
+  run: (middleware: middlewareType<>) => Promise<void>,
+  close: () => ?http.Server,
+  fetch: (pathname: string, options: *) => Promise<BodyType>,
+|};
+
+/**
+ * @return {cacheType} - testing server object
+ */
+export default (): cacheType => {
+  const cache: cacheType = {
+    port: -1,
+    run: async (middleware: middlewareType<>) => {
+      const server = http.createServer(
+        (req: http.IncomingMessage, res: http.ServerResponse) => {
+          middleware(req, res);
+        },
+      );
+
+      cache.close();
+      cache.port = await getPort();
+      cache.server = server.listen(cache.port);
+    },
+    close: () => cache.server?.close(),
+    fetch: (pathname: string, options?: *) =>
+      fetch(`http://localhost:${cache.port}${pathname}`, options),
+  };
+
+  return cache;
+};

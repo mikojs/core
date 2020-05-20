66a0ba1e071d05188be5bfc8f23f276040a1c3a7
HsuTing <hsuting0106@gmail.com>
2020-05-20 15:06:33 +0800

    improvement(server): remove Promise

--- server/server/src/buildCli.js
+++ server/server/src/buildCli.js
@@ -10,7 +10,7 @@ import { handleUnhandledRejection } from '@mikojs/utils';
 import { type mergeDirEventType } from '@mikojs/utils/lib/mergeDir';
 import typeof createLoggerType from '@mikojs/utils/lib/createLogger';
 
-import getOptions, { type optionsType } from './utils/getOptions';
+import getOptions from './utils/getOptions';
 
 import {
   type middlewareType,
@@ -24,8 +24,6 @@ const debugLog = debug('server:buildCli');
 handleUnhandledRejection();
 
 /**
- * TODO: should use async/await after flow fix error
- *
  * @param {Array} argv - command line
  * @param {string} defaultFolder - default folder
  * @param {createLoggerType} logger - logger function
@@ -33,7 +31,7 @@ handleUnhandledRejection();
  *
  * @return {any} - http server
  */
-export default (
+export default async (
   argv: $ReadOnlyArray<string>,
   defaultFolder: string,
   logger: $Call<createLoggerType, string>,
@@ -44,42 +42,41 @@ export default (
 ): Promise<http.Server> => {
   logger.start('Server start');
 
-  return getOptions(argv, defaultFolder)
-    .then(({ port, folderPath }: optionsType): http.Server => {
-      const middleware = buildMiddleware(
-        folderPath,
-        (type: 'start' | 'end', event: mergeDirEventType, filePath: string) => {
-          const relativePath = path.relative(folderPath, filePath);
+  try {
+    const { port, folderPath } = await getOptions(argv, defaultFolder);
+    const middleware = buildMiddleware(
+      folderPath,
+      (type: 'start' | 'end', event: mergeDirEventType, filePath: string) => {
+        const relativePath = path.relative(folderPath, filePath);
 
-          if (type === 'start') {
-            logger.start(
-              chalk`{gray [${event}]} Server updating (${relativePath})`,
-            );
-            return;
-          }
-
-          logger.succeed(
-            chalk`{gray [${event}]} Server updated (${relativePath})`,
+        if (type === 'start') {
+          logger.start(
+            chalk`{gray [${event}]} Server updating (${relativePath})`,
           );
-        },
-      );
-      const server = http.createServer(
-        (req: http.IncomingMessage, res: http.ServerResponse) => {
-          middleware(req, res);
-        },
-      );
+          return;
+        }
 
-      debugLog({ port, folderPath, middleware, server });
-      server.listen(port);
-      logger.succeed(
-        chalk`Running server at port: {gray {bold ${port.toString()}}}`,
-      );
+        logger.succeed(
+          chalk`{gray [${event}]} Server updated (${relativePath})`,
+        );
+      },
+    );
+    const server = http.createServer(
+      (req: http.IncomingMessage, res: http.ServerResponse) => {
+        middleware(req, res);
+      },
+    );
 
-      return server;
-    })
-    .catch((e: Error) => {
-      debugLog(e);
-      logger.fail(e.message);
-      throw e;
-    });
+    debugLog({ port, folderPath, middleware, server });
+    server.listen(port);
+    logger.succeed(
+      chalk`Running server at port: {gray {bold ${port.toString()}}}`,
+    );
+
+    return server;
+  } catch (e) {
+    debugLog(e);
+    logger.fail(e.message);
+    throw e;
+  }
 };

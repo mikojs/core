2945b1690ff3057190abd5b3f6e5b213be9468d4
HsuTing <hsuting0106@gmail.com>
2020-05-30 15:20:01 +0800

    test(pages): add default testings

---
+++ server/pages/src/__tests__/index.js
@@ -0,0 +1,29 @@
+// @flow
+
+import path from 'path';
+
+import { mockUpdate } from '@mikojs/utils/lib/mergeDir';
+import testingServer from '@mikojs/server/lib/testingServer';
+
+import buildPages from '../index';
+
+import testings from './__ignore__/testings';
+
+const server = testingServer();
+
+describe('pages', () => {
+  beforeEach(() => {
+    mockUpdate.clear();
+  });
+
+  test.each(testings)('%s', async (pathname: string) => {
+    const folderPath = path.resolve(__dirname, './__ignore__/pages');
+    const pages = buildPages(folderPath);
+
+    await server.run(pages.middleware);
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+});

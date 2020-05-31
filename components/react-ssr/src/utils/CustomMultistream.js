0e9e5282b403f4f246bf32c4918185fb91a22fcd
HsuTing <hsuting0106@gmail.com>
2020-05-31 16:39:25 +0800

    improvement(react-ssr): remove not used

--- components/react-ssr/src/utils/CustomMultistream.js
+++ components/react-ssr/src/utils/CustomMultistream.js
@@ -1,7 +1,5 @@
 // @flow
 
-import { type Readable as ReadableType } from 'stream';
-
 import Multistream from 'multistream';
 
 /**
@@ -10,14 +8,7 @@ export default class CustomMultistream extends Multistream {
   /**
    */
   destroy() {
-    if (this._current) {
-      this._next();
-      return;
-    }
-
-    this._queue.forEach((stream: ReadableType) => {
-      if (stream.destroy) stream.destroy();
-    });
-    this.emit('close');
+    if (this._current) this._next();
+    else this.emit('close');
   }
 }

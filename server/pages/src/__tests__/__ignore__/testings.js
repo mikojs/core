a4929ec18937529738b6c78d647ef228c953b3c2
HsuTing <hsuting0106@gmail.com>
2020-06-02 02:16:53 +0800

    test(pages): add default templates testing

--- server/pages/src/__tests__/__ignore__/testings.js
+++ server/pages/src/__tests__/__ignore__/testings.js
@@ -1,9 +1,5 @@
 // @flow
 
-// const upperDocument = '<html lang="en"><head><meta data-react-helmet="true" charSet="utf-8"/><meta data-react-helmet="true" name="viewport" content="width=device-width, initial-scale=1"/><title data-react-helmet="true">mikojs</title><link data-react-helmet="true" rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"/></head><body>';
-
-// const lowerDocument = '</body></html>';
-
 /**
  * @param {string} name - page name
  * @param {object} pageProps - page props
--- server/pages/src/__tests__/index.js
+++ server/pages/src/__tests__/index.js
@@ -15,6 +15,7 @@ import buildPages from '../index';
 
 import { getPage, getNotFound, getValue } from './__ignore__/testings';
 
+const folderPath = path.resolve(__dirname, './__ignore__/pages');
 const server = testingServer();
 
 describe('pages', () => {
@@ -54,7 +55,6 @@ describe('pages', () => {
           chunkName: string,
         |}) => {
           const newPathname = `${!useBasename ? '' : '/basename'}${pathname}`;
-          const folderPath = path.resolve(__dirname, './__ignore__/pages');
           const pages = buildPages(
             folderPath,
             !useBasename ? undefined : { basename: 'basename' },
@@ -80,6 +80,44 @@ describe('pages', () => {
       );
     },
   );
+
+  test('default templates', async () => {
+    const pages = buildPages(folderPath);
+
+    await server.run(pages.middleware);
+
+    [
+      'Document.js',
+      'Error.js',
+      'Loading.js',
+      'Main.js',
+      'NotFound.js',
+      'Others.js',
+    ].forEach((filename: string) => {
+      mockUpdate.cache[0](
+        'unlink',
+        path.resolve(folderPath, '.templates', filename),
+      );
+    });
+
+    expect(
+      await server.fetch('/').then((res: fetchResultType) => res.text()),
+    ).toEqual(
+      [
+        '<!DOCTYPE html>',
+        '<html lang="en"><head>',
+        '<meta data-react-helmet="true" charSet="utf-8"/>',
+        '<meta data-react-helmet="true" name="viewport" content="width=device-width, initial-scale=1"/>',
+        '<title data-react-helmet="true">mikojs</title>',
+        '<link data-react-helmet="true" rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"/>',
+        '</head><body>',
+        '<main id="__MIKOJS__"><div>/</div></main>',
+        '<script data-react-helmet="true">',
+        'var __MIKOJS_DATA__ = {"mainInitialProps":{},"pageInitialProps":{"pathname":"/"},"chunkName":"pages/index"};',
+        '</script></body></html>',
+      ].join(''),
+    );
+  });
 
   afterAll(() => {
     server.close();

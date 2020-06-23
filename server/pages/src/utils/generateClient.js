d2807c8f1eabe6e3aff473c968fec8ff2c00d7ef
HsuTing <hsuting0106@gmail.com>
2020-06-23 12:54:48 +0800

    style(pages): modify code style

--- server/pages/src/utils/generateClient.js
+++ server/pages/src/utils/generateClient.js
@@ -14,41 +14,41 @@ export default (routes: routesType, clientPath: string) => {
       clientPath,
       `import { setConfig } from 'react-hot-loader';
 
-  import handleUnhandledRejection from '@mikojs/utils/lib/handleUnhandledRejection';
-  import client from '@mikojs/react-ssr/lib/client';
+import handleUnhandledRejection from '@mikojs/utils/lib/handleUnhandledRejection';
+import client from '@mikojs/react-ssr/lib/client';
 
-  import Main from '${routes.getTamplate('main')}';
-  import Loading from '${routes.getTamplate('loading')}';
-  import Error from '${routes.getTamplate('error')}';
+import Main from '${routes.getTamplate('main')}';
+import Loading from '${routes.getTamplate('loading')}';
+import Error from '${routes.getTamplate('error')}';
 
-  handleUnhandledRejection();
-  setConfig({
-    errorReporter: Error,
-  });
-  client({
-    Main,
-    Loading,
-    Error,
-    routesData: [${routes
-      .get()
-      .map(
-        ({
-          path,
-          component: { chunkName },
-        }: $ElementType<$PropertyType<routesType, 'cache'>, number>) =>
-          `{ ${[
-            'exact: true',
-            `path: '${path}'`,
-            `component: { ${[
-              `chunkName: '${chunkName}'`,
-              `loader: () => import(/* webpackChunkName: "${chunkName}" */ '${routes.getFilePath(
-                path,
-              )}')`,
-            ].join(',')} }`,
+handleUnhandledRejection();
+setConfig({
+  errorReporter: Error,
+});
+client({
+  Main,
+  Loading,
+  Error,
+  routesData: [${routes
+    .get()
+    .map(
+      ({
+        path,
+        component: { chunkName },
+      }: $ElementType<$PropertyType<routesType, 'cache'>, number>) =>
+        `{ ${[
+          'exact: true',
+          `path: '${path}'`,
+          `component: { ${[
+            `chunkName: '${chunkName}'`,
+            `loader: () => import(/* webpackChunkName: "${chunkName}" */ '${routes.getFilePath(
+              path,
+            )}')`,
           ].join(',')} }`,
-      )
-      .join(',')}],
-  });`,
+        ].join(',')} }`,
+    )
+    .join(',')}],
+});`,
     );
   });
 };

cc841d43d099e6ed8f3319c7a5bb71b2de05109b
HsuTing <hsuting0106@gmail.com>
2020-06-27 21:42:08 +0800

    feat(server): add dir events

---
+++ server/server/src/utils/dirEvents.js
@@ -0,0 +1,84 @@
+// @flow
+
+import EventEmitter from 'events';
+
+import fs from 'fs';
+import path from 'path';
+
+import { invariant, emptyFunction } from 'fbjs';
+
+import { d3DirTree, mockChoice } from '@mikojs/utils';
+import {
+  type d3DirTreeOptionsType,
+  type d3DirTreeNodeType,
+} from '@mikojs/utils/lib/d3DirTree';
+
+export type dirEventsType =
+  | 'init'
+  | 'add'
+  | 'addDir'
+  | 'change'
+  | 'unlink'
+  | 'unlinkDir'
+  | 'ready'
+  | 'raw'
+  | 'error';
+
+export type dirDataType = {|
+  filePath: $PropertyType<$PropertyType<d3DirTreeNodeType, 'data'>, 'filePath'>,
+  name: $PropertyType<$PropertyType<d3DirTreeNodeType, 'data'>, 'name'>,
+  extension: $PropertyType<
+    $PropertyType<d3DirTreeNodeType, 'data'>,
+    'extension',
+  >,
+|};
+
+/**
+ * @param {string} folderPath - folder path
+ * @param {boolean} watch - watch mode or not
+ * @param {d3DirTreeOptionsType} options - d3 dir tree options
+ *
+ * @return {EventEmitter} - events
+ */
+export default (
+  folderPath: string,
+  watch: boolean,
+  options: d3DirTreeOptionsType,
+): EventEmitter => {
+  const events = new EventEmitter();
+  const { extensions, exclude } = options;
+
+  invariant(
+    fs.existsSync(folderPath),
+    `Can not find the folder: ${path.relative(process.cwd(), folderPath)}`,
+  );
+  d3DirTree(folderPath, options)
+    .leaves()
+    .filter(({ data: { type } }: d3DirTreeNodeType) => type === 'file')
+    .forEach(
+      ({ data: { path: filePath, name, extension } }: d3DirTreeNodeType) =>
+        events.emit('init', {
+          filePath,
+          name,
+          extension,
+        }),
+    );
+
+  if (watch)
+    mockChoice(process.env.NODE_ENV !== 'test', require('chokidar'), {
+      watch: emptyFunction.thatReturns(events),
+    })
+      .watch(folderPath, { ignored: exclude, ignoreInitial: true })
+      .on('all', (event: dirEventsType, filePath: string) => {
+        if (!extensions?.test(filePath)) return;
+
+        delete require.cache[filePath];
+        events.emit(event, {
+          filePath,
+          name: path.basename(filePath),
+          extension: path.extname(filePath),
+        });
+      });
+
+  return events;
+};

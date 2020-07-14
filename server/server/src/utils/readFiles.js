294098480395634745632a26636f1db8a91c1798
HsuTing <hsuting0106@gmail.com>
2020-07-14 13:23:02 +0800

    feat(server): add read files

---
+++ server/server/src/utils/readFiles.js
@@ -0,0 +1,73 @@
+// @flow
+
+import path from 'path';
+
+import chokidar from 'chokidar';
+
+type optionsType = {|
+  folderPath: string,
+  basename?: string,
+  ignored?: RegExp,
+  extensions?: RegExp,
+|};
+
+type eventsType =
+  | 'add'
+  | 'addDir'
+  | 'change'
+  | 'unlink'
+  | 'unlinkDir'
+  | 'ready'
+  | 'raw'
+  | 'error';
+
+type callbackType = (
+  event: eventsType,
+  data: {|
+    name: string,
+    extension: string,
+    filePath: string,
+    pathname: string,
+  |},
+) => void;
+
+/**
+ * @param {optionsType} options - read files options
+ * @param {callbackType} callback - callback
+ *
+ * @return {chokidar} - chokidar watcher
+ */
+export default (
+  { folderPath, basename, ignored, extensions }: optionsType,
+  callback: callbackType,
+): typeof chokidar => {
+  const watcher = chokidar.watch(folderPath, {
+    ignored,
+  });
+
+  watcher.on('all', (event: eventsType, filePath: string) => {
+    if (!extensions?.test(filePath)) return;
+
+    const name = path.basename(filePath);
+    const extension = path.extname(filePath);
+
+    delete require.cache[filePath];
+    callback(event, {
+      name,
+      extension,
+      filePath,
+      pathname: `/${[
+        basename,
+        path.dirname(path.relative(folderPath, filePath)).replace(/^\./, ''),
+        name
+          .replace(extension, '')
+          .replace(/^index$/, '')
+          .replace(/\[([^[\]]*)\]/g, ':$1'),
+      ]
+        .filter(Boolean)
+        .join('/')}`,
+    });
+  });
+
+  return watcher;
+};

033c4a61e89a6990e0312ca8766fd897280eaea9
HsuTing <hsuting0106@gmail.com>
2020-07-20 10:46:14 +0800

    chore(server): add readFiles

---
+++ server/server/src/utils/readFiles.js
@@ -0,0 +1,60 @@
+// @flow
+
+import path from 'path';
+import EventEmitter from 'events';
+
+import chokidar from 'chokidar';
+
+type optionsType = {|
+  folderPath: string,
+  basename?: string,
+  ignored?: RegExp,
+  extensions?: RegExp,
+|};
+
+type eventsType =
+  | 'add'
+  | 'addDir'
+  | 'change'
+  | 'unlink'
+  | 'unlinkDir'
+  | 'ready'
+  | 'raw'
+  | 'error';
+
+/**
+ * @param {EventEmitter} events - events
+ * @param {optionsType} options - build read files options
+ */
+export default (
+  events: EventEmitter,
+  { folderPath, basename, ignored, extensions }: optionsType,
+) => {
+  const watcher = chokidar.watch(folderPath, {
+    ignored,
+  });
+
+  watcher.on('all', (event: eventsType, filePath: string) => {
+    if (!extensions?.test(filePath)) return;
+
+    const name = path.basename(filePath);
+    const extension = path.extname(filePath);
+
+    delete require.cache[filePath];
+    events.on(event, {
+      name,
+      extension,
+      filePath,
+      pathname: `/${[
+        basename,
+        path.dirname(path.relative(folderPath, filePath)).replace(/^\./, ''),
+        name
+          .replace(extension, '')
+          .replace(/^index$/, '')
+          .replace(/\[([^[\]]*)\]/g, ':$1'),
+      ]
+        .filter(Boolean)
+        .join('/')}`,
+    });
+  });
+};

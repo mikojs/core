a56ef37ee89c026bdbade34d9187fe8985c5be9e
HsuTing <hsuting0106@gmail.com>
2020-08-14 17:54:12 +0800

    feat(babel-plugin-merge-dir): can merge dir

--- babel/babel-plugin-merge-dir/src/index.js
+++ babel/babel-plugin-merge-dir/src/index.js
@@ -1,15 +1,40 @@
 // @flow
 
+import path from 'path';
+
 import { declare } from '@babel/helper-plugin-utils';
 import type nodePathType from '@babel/traverse';
+import outputFileSync from 'output-file-sync';
 
 export default declare(
   (
     { assertVersion, types: t }: nodePathType,
-    { dir }: {| dir: string |},
+    {
+      dir = '.',
+      callback,
+    }: {|
+      dir?: string,
+      callback: (filenames: $ReadOnlyArray<string>) => string,
+    |},
   ): {} => {
+    const cacheFilePath = path.resolve(dir, './.mergeDir');
+    const cache = {};
+
     assertVersion(7);
 
-    return {};
+    return {
+      post: ({
+        opts: { filename },
+      }: {|
+        opts: {|
+          filename: string,
+        |},
+      |}) => {
+        if (!filename.includes(dir) || !callback) return;
+
+        cache[filename] = `./${path.relative(dir, filename)}`;
+        outputFileSync(cacheFilePath, callback(Object.values(cache[filename])));
+      },
+    };
   },
 );

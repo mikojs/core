ffc90458a5c74b4a41f8936ce23a4edf94b39956
HsuTing <hsuting0106@gmail.com>
2020-09-17 00:52:02 +0800

    feat(babel): can subsribe event

--- babel/babel/src/bin/index.js
+++ babel/babel/src/bin/index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 // @flow
 
+import crypto from 'crypto';
+
 import ora from 'ora';
 import watchman from 'fb-watchman';
 import parseArgv from '@babel/cli/lib/babel/options';
@@ -13,18 +15,67 @@ const logger = createLogger('@mikojs/miko', ora({ discardStdin: false }));
 const client = new watchman.Client();
 const opts = parseArgv(process.argv);
 const fn = opts.cliOptions.outDir ? dirCommand : fileCommand;
+const hash = crypto.createHash('md5').update('@mikojs/babel').digest('hex');
+
+/**
+ * @param {any} watch - watch-project response
+ * @param {string} relativePath - relative path
+ */
+const subscribe = (watch: mixed, relativePath: ?string) => {
+  client.command(
+    [
+      'subscribe',
+      watch,
+      hash,
+      {
+        ...(!relativePath
+          ? {}
+          : {
+              relative_root: relativePath,
+            }),
+        expression: ['allof', ['match', '*.js']],
+        fields: ['name', 'size', 'mtime_ms', 'exists', 'type'],
+      },
+    ],
+    (err: Error) => {
+      if (err) {
+        logger.fail(err.message);
+        return;
+      }
+    },
+  );
+
+  client.on('subscription', ({ subscription }: {| subscription: string |}) => {
+    if (subscription !== hash) return;
+
+    /*
+    files.forEach(file => {
+      console.log(file);
+    });
+    */
+  });
+};
 
 /** */
 const watching = () => {
   client.command(
     ['watch-project', opts.cliOptions.outFile || opts.cliOptions.outDir],
-    (err: Error, { warning }: {| warning?: string |}) => {
+    (
+      err: Error,
+      {
+        warning,
+        watch,
+        relative_path: relativePath,
+      }: {| warning?: string, watch: mixed, relative_path?: string |},
+    ) => {
       if (err) {
         logger.fail(err.message);
         return;
       }
 
       if (warning) logger.warn(warning).start('Watching files');
+
+      subscribe(watch, relativePath);
     },
   );
 };

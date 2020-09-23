fcaef69fe823c50fbe46f096106cbcfdeaa77aaa
HsuTing <hsuting0106@gmail.com>
2020-09-23 11:34:47 +0800

    feat(server): can build middleware

--- server/server/src/index.js
+++ server/server/src/index.js
@@ -1,1 +1,40 @@
 // @flow
+
+import {
+  type IncomingMessage as IncomingMessageType,
+  type ServerResponse as ServerResponseType,
+} from 'http';
+
+import { emptyFunction, invariant } from 'fbjs';
+import findCacheDir from 'find-cache-dir';
+import cryptoRandomString from 'crypto-random-string';
+
+import { requireModule, mockChoice } from '@mikojs/utils';
+
+import { type middlewareType } from './types';
+
+import buildMiddleware from './utils/buildMiddleware';
+
+const cacheDir = findCacheDir({ name: '@mikojs/server', thunk: true });
+
+/**
+ * @param {string} event - event type
+ *
+ * @return {middlewareType} - server middleware
+ */
+export default async (
+  event: 'dev' | 'build' | 'start',
+): Promise<middlewareType> => {
+  const hash = cryptoRandomString({ length: 10, type: 'base64' });
+  const cacheFilePath = cacheDir(`${hash}.js`);
+
+  mockChoice(event !== 'start', buildMiddleware, emptyFunction)(cacheFilePath);
+
+  return (req: IncomingMessageType, res: ServerResponseType) => {
+    invariant(
+      event !== 'build',
+      'Should not use the middleware in the build mode',
+    );
+    requireModule<middlewareType>(cacheFilePath)(req, res);
+  };
+};

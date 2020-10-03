40a2fbff952d31f07b96ae8f434127bf1332f919
HsuTing <hsuting0106@gmail.com>
2020-10-04 01:40:46 +0800

    fix(circleci): fix watchman cache

--- .circleci/watchman.sh
+++ .circleci/watchman.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if type watchman > /dev/null; then
-  exit 0;
+if [ ! -d ~/watchman ]; then
+  git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1 ~/watchman
+  cd ~/watchman
+  ./autogen.sh
+  ./configure --without-python
+  make
 fi
 
-git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1 ~/watchman
-cd ~/watchman
-./autogen.sh
-./configure --without-python
-make
 make install

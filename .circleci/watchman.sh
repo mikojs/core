5e9c01425e715a94ce89a5904e4c006f46eab734
HsuTing <hsuting0106@gmail.com>
2020-10-04 02:08:56 +0800

    ci(circleci): use environment variables

--- .circleci/watchman.sh
+++ .circleci/watchman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d ~/watchman ]; then
-  git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1 ~/watchman
+  git clone https://github.com/facebook/watchman.git -b $WATCHMAN_VERSION --depth 1 ~/watchman
   cd ~/watchman
   ./autogen.sh
   ./configure --without-python

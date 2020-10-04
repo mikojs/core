5e7d7a0183b560e7273438f03c8aca4588c81977
HsuTing <hsuting0106@gmail.com>
2020-10-05 00:57:18 +0800

    test(server): add build testing

--- server/server/src/__tests__/__ignore__/build.js
+++ server/server/src/__tests__/__ignore__/build.js
@@ -1,8 +1,27 @@
 // @flow
 
+import { type buildDataType } from '../../index';
+
+const cache = {};
+
 /**
+ * @param {buildDataType} buildData - the data of the build function
+ *
  * @return {string} - middleware cache
  */
-export default (): string => `module.exports = (req, res) => {
-  res.end(req.url);
+export default ({ exists, filePath, pathname }: buildDataType): string => {
+  cache[pathname] = filePath;
+
+  return `const requireModule = require('@mikojs/utils/lib/requireModule');
+
+const cache = ${JSON.stringify(cache)};
+
+module.exports = (req, res) => {
+  const cacheKey = Object.keys(cache).find(key => key === req.url);
+
+  if (!cacheKey)
+    res.end();
+  else
+    requireModule(cache[cacheKey])(req, res);
 };`;
+};

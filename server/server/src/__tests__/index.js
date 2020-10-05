0d1d84d3b1e8638268d4f45c73395001c60419ef
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2020-10-06 01:04:19 +0800

    (@mikojs/server) Add more testing (#665)
    * test(server): add server and testingServer
    
    * style(utils, server): modify code style
    
    * fix(server): use watchman to query
    
    * fix(server): fix coverage
    
    * test(server): fix coverage
    
    * test(server): add more route testing
    
    * test(utils): fix testing case

--- server/server/src/__tests__/index.js
+++ server/server/src/__tests__/index.js
@@ -1,35 +1,10 @@
 // @flow
 
 import server from '../index';
-import testingServer, { type fetchResultType } from '../testingServer';
-
-import foo from './__ignore__/foo';
+import testings from './__ignore__/testings';
 
 import watcher from 'utils/watcher';
 
-describe.each`
-  mockWatcher
-  ${true}
-  ${false}
-`(
-  'server with mockWatcher = $mockWatcher',
-  ({ mockWatcher }: {| mockWatcher: boolean |}) => {
-    beforeAll(async () => {
-      if (!mockWatcher) server.set({ watcher });
+server.set({ watcher });
 
-      await testingServer.run(foo);
-    });
-
-    test('fetch', async () => {
-      expect(
-        await testingServer
-          .fetch('/')
-          .then((res: fetchResultType) => res.text()),
-      ).toBe('/');
-    });
-
-    afterAll(() => {
-      testingServer.close();
-    });
-  },
-);
+describe('server', testings);

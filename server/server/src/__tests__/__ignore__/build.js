a63d4ce17511c4436905cb4fcde7bfbc3e4c2ce6
HsuTing <hsuting0106@gmail.com>
2020-10-05 10:00:57 +0800

    test(server): add type

--- server/server/src/__tests__/__ignore__/build.js
+++ server/server/src/__tests__/__ignore__/build.js
@@ -2,7 +2,7 @@
 
 import { type buildDataType } from '../../index';
 
-const cache = {};
+const cache: {| [string]: string |} = {};
 
 /**
  * @param {buildDataType} buildData - the data of the build function

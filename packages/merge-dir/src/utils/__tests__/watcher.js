737283e8b5f3281e029004f238836265346ab472
HsuTing <hsuting0106@gmail.com>
2020-10-07 00:50:07 +0800

    test(merge-dir): add testing case

---
+++ packages/merge-dir/src/utils/__tests__/watcher.js
@@ -0,0 +1,27 @@
+// @flow
+
+import { emptyFunction } from 'fbjs';
+
+import watcher, { handler } from '../watcher';
+
+describe('watcher', () => {
+  test('watcher close', async () => {
+    expect((await watcher(__dirname, emptyFunction))()).toBeUndefined();
+  });
+
+  test('handler reject', async () => {
+    const mockLog = jest.fn();
+
+    global.console.warn = mockLog;
+
+    await expect(
+      new Promise((resolve, reject) =>
+        handler(resolve, reject)(new Error('error'), {
+          warning: 'warning',
+          files: [],
+        }),
+      ),
+    ).rejects.toThrow('error');
+    expect(mockLog).toHaveBeenCalledWith('warning');
+  });
+});

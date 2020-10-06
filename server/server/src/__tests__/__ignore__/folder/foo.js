d423d65279981c73e14a7bc9a91eaa46b1d0cca7
HsuTing <hsuting0106@gmail.com>
2020-10-07 01:40:05 +0800

    style(server): modify code style

--- server/server/src/__tests__/__ignore__/folder/foo.js
+++ server/server/src/__tests__/__ignore__/folder/foo.js
@@ -1,3 +1,3 @@
 // @flow
 
-export { default } from '../middleware';
+export { default } from './index';
--- server/server/src/__tests__/__ignore__/folder/index.js
+++ server/server/src/__tests__/__ignore__/folder/index.js
@@ -1,3 +1,11 @@
 // @flow
 
-export { default } from '../middleware';
+import {
+  type IncomingMessage as IncomingMessageType,
+  type ServerResponse as ServerResponseType,
+} from 'http';
+
+/** @middleware testing middleware */
+export default (req: IncomingMessageType, res: ServerResponseType) => {
+  res.end(req.url);
+};

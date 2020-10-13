28be7a30d2ae0943980a460ff09ef3cb6fa55664
HsuTing <hsuting0106@gmail.com>
2020-10-14 00:27:02 +0800

    test(merge-dir): simplify testing

--- packages/merge-dir/src/__tests__/__ignore__/testings.js
+++ packages/merge-dir/src/__tests__/__ignore__/testings.js
@@ -1,27 +1,33 @@
 // @flow
 
+import path from 'path';
+
 import testing from '../../testing';
 
-import func from './foo';
+import build from './build';
 
 /** */
 export default () => {
-  beforeAll(async () => {
-    (await testing.ready())();
-  });
+  describe('dev mode', () => {
+    const func = build(path.resolve(__dirname, './folder/foo'));
 
-  test.each`
-    pathname
-    ${'/'}
-    ${'/foo'}
-    ${'/:id'}
-    ${'/bar'}
-    ${'/bar/foo'}
-    ${'/bar/bar'}
-    ${'/baz'}
-    ${'/baz/foo'}
-    ${'/baz/bar'}
-  `('get $pathname', ({ pathname }: {| pathname: string |}) => {
-    expect(func(pathname)).toBe(pathname);
+    beforeAll(async () => {
+      (await testing.ready())();
+    });
+
+    test.each`
+      pathname
+      ${'/'}
+      ${'/foo'}
+      ${'/:id'}
+      ${'/bar'}
+      ${'/bar/foo'}
+      ${'/bar/bar'}
+      ${'/baz'}
+      ${'/baz/foo'}
+      ${'/baz/bar'}
+    `('get $pathname', ({ pathname }: {| pathname: string |}) => {
+      expect(func(pathname)).toBe(pathname);
+    });
   });
 };

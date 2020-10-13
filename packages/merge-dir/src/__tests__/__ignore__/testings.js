e6ab5b7ea8233bd2d2372f2776a9bb783cedac9d
HsuTing <hsuting0106@gmail.com>
2020-10-14 00:43:59 +0800

    test(merge-dir): add more testing with build and run

--- packages/merge-dir/src/__tests__/__ignore__/testings.js
+++ packages/merge-dir/src/__tests__/__ignore__/testings.js
@@ -2,14 +2,18 @@
 
 import path from 'path';
 
+import { emptyFunction } from 'fbjs';
+
 import testing from '../../testing';
 
 import build from './build';
 
+const folderPath = path.resolve(__dirname, './folder/foo');
+
 /** */
 export default () => {
   describe('dev mode', () => {
-    const func = build(path.resolve(__dirname, './folder/foo'));
+    const func = build(folderPath);
 
     beforeAll(async () => {
       (await testing.ready())();
@@ -29,5 +33,20 @@ export default () => {
     `('get $pathname', ({ pathname }: {| pathname: string |}) => {
       expect(func(pathname)).toBe(pathname);
     });
+  });
+
+  test('build and run mode', async () => {
+    testing.updateTools({ type: 'build' });
+    testing.set(
+      folderPath,
+      () =>
+        `module.exports = require('fbjs/lib/emptyFunction').thatReturnsArgument;`,
+    );
+    (await testing.ready())();
+    testing.updateTools({ type: 'run' });
+
+    expect(testing.get(testing.set(folderPath, emptyFunction))('test')).toBe(
+      'test',
+    );
   });
 };

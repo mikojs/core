54493c0e479e088286b0280ba5a6636c9c7adc8e
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2020-10-17 19:27:30 +0800

    (@mikojs/server) Add new server (#679)
    * feat(merge-dir): add server.run
    
    * feat(server): add build event
    
    * feat(server): add testing server
    
    * test(server): add normal testing
    
    * test(server): add build testing

---
+++ server/server/src/__tests__/index.js
@@ -0,0 +1,33 @@
+// @flow
+
+import mergeDir from '@mikojs/merge-dir';
+
+import server from '../index';
+import testingServer, { type fetchResultType } from '../testingServer';
+
+describe('server', () => {
+  beforeAll(async () => {
+    await testingServer.run(
+      mergeDir.use(
+        __dirname,
+        () => `module.exports = (req, res) => {
+  res.end(req.url);
+};`,
+      ),
+    );
+  });
+
+  test('fetch', async () => {
+    expect(
+      await testingServer.fetch('/').then((res: fetchResultType) => res.text()),
+    ).toBe('/');
+  });
+
+  test('build', async () => {
+    expect(await server.build()).toBeUndefined();
+  });
+
+  afterAll(() => {
+    testingServer.close();
+  });
+});

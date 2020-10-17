7b869e04296fd82360266b553a1886f47be1bd7e
HsuTing <hsuting0106@gmail.com>
2020-10-17 19:14:40 +0800

    test(server): add normal testing

---
+++ server/server/src/__tests__/index.js
@@ -0,0 +1,28 @@
+// @flow
+
+import mergeDir from '@mikojs/merge-dir';
+
+import testingServer, { type fetchResultType } from '../testingServer';
+
+describe('server', () => {
+  beforeAll(async () => {
+    await testingServer.run(
+      mergeDir.use(
+        __dirname,
+        () => `module.exports = (req, res) => {
+  res.end(req.url);
+};`,
+      ),
+    );
+  });
+
+  test('fetch', async () => {
+    expect(
+      await testingServer.fetch('/').then((res: fetchResultType) => res.text()),
+    ).toBe('/');
+  });
+
+  afterAll(() => {
+    testingServer.close();
+  });
+});

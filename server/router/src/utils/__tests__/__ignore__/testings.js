b3040273022c2e3a6dabeb5fbd26c59a1bdfbd5f
HsuTing <hsuting0106@gmail.com>
2020-10-18 10:41:35 +0800

    test(router): add more testing

--- server/router/src/utils/__tests__/__ignore__/testings.js
+++ server/router/src/utils/__tests__/__ignore__/testings.js
@@ -1,5 +1,21 @@
 // @flow
 
+/**
+ * @param {string} filePath - file path
+ *
+ * @return {string} - cache data
+ */
+const getCacheData = (filePath: string) => `{
+  filePath: path.resolve(__filename, '${filePath.replace(
+    /:([\w]+)/,
+    '[$1]',
+  )}.js'),
+  regExp: pathToRegexp('/${filePath}', []),
+  getUrlQuery: pathname => match('/${filePath}', { decode: decodeURIComponent })(
+    pathname,
+  ).params,
+}`;
+
 export default [
   [
     'add a.js',
@@ -8,12 +24,35 @@ export default [
       filePath: 'a.js',
       pathname: '/a',
     },
-    `const cache = [{
-  filePath: path.resolve(__filename, 'a.js'),
-  regExp: pathToRegexp('/a', []),
-  getUrlQuery: pathname => match('/a', { decode: decodeURIComponent })(
-    pathname,
-  ).params,
-}];`,
+    `const cache = [${getCacheData('a')}];`,
+  ],
+  [
+    'add a/a.js',
+    {
+      exists: true,
+      filePath: 'a/a.js',
+      pathname: '/a/a',
+    },
+    `const cache = [${getCacheData('a/a')}, ${getCacheData('a')}];`,
+  ],
+  [
+    'add [:id].js',
+    {
+      exists: true,
+      filePath: '[id].js',
+      pathname: '/:id',
+    },
+    `const cache = [${getCacheData('a/a')}, ${getCacheData(
+      'a',
+    )}, ${getCacheData(':id')}];`,
+  ],
+  [
+    'remove [:id].js',
+    {
+      exists: false,
+      filePath: '[id].js',
+      pathname: '/:id',
+    },
+    `const cache = [${getCacheData('a/a')}, ${getCacheData('a')}];`,
   ],
 ];

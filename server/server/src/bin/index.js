50583190d4637a59cf0cd516152017befdb8f5e1
HsuTing <hsuting0106@gmail.com>
2020-10-19 01:18:54 +0800

    feat(server): add more information

--- server/server/src/bin/index.js
+++ server/server/src/bin/index.js
@@ -1,17 +1,32 @@
 #! /usr/bin/env node
 // @flow
 
-import { handleUnhandledRejection } from '@mikojs/utils';
+import ora from 'ora';
+import chalk from 'chalk';
+
+import { handleUnhandledRejection, createLogger } from '@mikojs/utils';
 
 import server from '../index';
 
 import getServerOptions from 'utils/getServerOptions';
 
+const logger = createLogger('@mikojs/server', ora({ discardStdin: false }));
+
 handleUnhandledRejection();
 
 (async () => {
-  const type = await getServerOptions(process.argv);
+  const { event } = await getServerOptions(process.argv);
 
-  if (type === 'error') process.exit(1);
-  else server.set(type);
+  switch (event) {
+    case 'error':
+      logger
+        .fail(chalk`Missing required argument {red <event>}`)
+        .fail(chalk`Use {gray -h} to get the more information`);
+      process.exit(1);
+      break;
+
+    default:
+      server.set(event);
+      break;
+  }
 })();

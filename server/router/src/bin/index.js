e1a00a7722644848b5f109a1791c007563740e50
HsuTing <hsuting0106@gmail.com>
2020-10-21 23:50:07 +0800

    feat(router): add handleUnhandledRejection

--- server/router/src/bin/index.js
+++ server/router/src/bin/index.js
@@ -1,9 +1,11 @@
 // @flow
 
+import { handleUnhandledRejection } from '@mikojs/utils';
 import parseArgv from '@mikojs/server/lib/parseArgv';
 
 import router from '../index';
 
 import { version } from '../../package.json';
 
+handleUnhandledRejection();
 parseArgv('router', version, router, process.argv);

49c56cb8d709cd83c834957442efafcc206601d9
HsuTing <hsuting0106@gmail.com>
2020-10-26 10:31:32 +0800

    feat(router): add middleware function

--- server/router/src/index.js
+++ server/router/src/index.js
@@ -1,8 +1,24 @@
 // @flow
 
+import url from 'url';
+import {
+  type IncomingMessage as IncomingMessageType,
+  type ServerResponse as ServerResponseType,
+} from 'http';
+
+import {
+  parse,
+  type QueryParameters as QueryParametersType,
+} from 'query-string';
+
+import { requireModule } from '@mikojs/utils';
 import server, { type middlewareType } from '@mikojs/server';
 
-import buildRouter from './utils/buildRouter';
+import buildRouter, { type cacheType } from './utils/buildRouter';
+
+type reqType = IncomingMessageType & {|
+  query: QueryParametersType,
+|};
 
 /**
  * @param {string} folderPath - folder path
@@ -11,4 +27,31 @@ import buildRouter from './utils/buildRouter';
  * @return {middlewareType} - router middleware
  */
 export default (folderPath: string, prefix?: string): middlewareType<> =>
-  server.mergeDir(folderPath, prefix, buildRouter);
+  server.mergeDir(
+    folderPath,
+    prefix,
+    buildRouter,
+  )((cache: cacheType): middlewareType<
+    reqType,
+    ServerResponseType,
+  > => (req: reqType, res: ServerResponseType) => {
+    const { pathname, query } = url.parse(req.url);
+    const router = cache.find(
+      ({ regExp }: $ElementType<cacheType, number>) =>
+        pathname && regExp.exec(pathname),
+    );
+
+    if (!router) {
+      res.statusCode = 404;
+      res.end();
+      return;
+    }
+
+    const { filePath, getUrlQuery } = router;
+
+    req.query = {
+      ...parse(query || ''),
+      ...getUrlQuery(pathname),
+    };
+    requireModule(filePath)(req, res);
+  });

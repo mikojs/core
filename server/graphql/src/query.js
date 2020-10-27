0c3efde4f9d0338e844859bf7aeca4b9a490bd07
HsuTing <hsuting0106@gmail.com>
2020-10-27 23:06:53 +0800

    feat(graphql): add query

---
+++ server/graphql/src/query.js
@@ -0,0 +1,39 @@
+// @flow
+
+import {
+  graphql,
+  type GraphQLArgs as GraphQLArgsType,
+  type ExecutionResult as ExecutionResultType,
+} from 'graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
+import server from '@mikojs/server';
+
+import buildCache, { type cacheType } from './utils/buildCache';
+
+type optionsType = $Diff<GraphQLArgsType, { schema: mixed }>;
+type queryType = (
+  folderPath: string,
+) => (options: optionsType) => Promise<ExecutionResultType>;
+
+export default {
+  set: server.set,
+  ready: server.ready,
+
+  /**
+   * @param {string} folderPath - folder path
+   *
+   * @return {queryType} - query function
+   */
+  query: (folderPath: string): queryType =>
+    server.mergeDir(
+      folderPath,
+      undefined,
+      buildCache,
+    )((cache: cacheType) => (options: optionsType) =>
+      graphql({
+        ...options,
+        schema: makeExecutableSchema(cache),
+      }),
+    ),
+};

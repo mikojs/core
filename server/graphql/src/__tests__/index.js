f4270636ff30d1d3e04947a90156adca9b02fd35
HsuTing <hsuting0106@gmail.com>
2020-10-31 14:02:23 +0800

    test(graphql): add graphql testing

--- server/graphql/src/__tests__/index.js
+++ server/graphql/src/__tests__/index.js
@@ -4,6 +4,17 @@ import path from 'path';
 
 import testingServer, { type fetchResultType } from '../testingServer';
 
+const query = `
+  {
+    version
+  }
+`;
+const expected = {
+  data: {
+    version: '1.0.0',
+  },
+};
+
 describe('router', () => {
   beforeAll(async () => {
     await testingServer.run(path.resolve(__dirname, './__ignore__'));
@@ -18,19 +29,19 @@ describe('router', () => {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            query: `
-              {
-                version
-              }
-            `,
+            query,
           }),
         })
         .then((res: fetchResultType) => res.json()),
-    ).toEqual({
-      data: {
-        version: '1.0.0',
-      },
-    });
+    ).toEqual(expected);
+  });
+
+  test('graphql', async () => {
+    expect(
+      await testingServer.graphql({
+        source: query,
+      }),
+    ).toEqual(expected);
   });
 
   afterAll(() => {

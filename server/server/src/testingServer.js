2df72cac4624630fd3e72e411be00f72dc2b45f5
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2020-11-01 23:43:55 +0800

    (@mikojs/server) Modify middleware type (#695)
    * feat(server): add default type
    
    * perf(router): modify flow type

--- server/server/src/testingServer.js
+++ server/server/src/testingServer.js
@@ -41,7 +41,7 @@ export default {
   /**
    * @param {middlewareType} middleware - middleware function
    */
-  run: async (middleware: middlewareType<>) => {
+  run: async <Req = {}, Res = {}>(middleware: middlewareType<Req, Res>) => {
     cache.close();
     cache.port = await getPort();
     cache.server = await server.run(middleware, cache.port);

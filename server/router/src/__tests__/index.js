b924edb46cbd6dcd954ed6a60aa4d9ab657c8c13
HsuTing <hsuting0106@gmail.com>
2020-11-04 13:16:30 +0800

    test(router): add folderPathOrMiddleware testing

--- server/router/src/__tests__/index.js
+++ server/router/src/__tests__/index.js
@@ -2,37 +2,51 @@
 
 import path from 'path';
 
+import { type middlewareType } from '@mikojs/server';
+
 import testingServer, { type fetchResultType } from '../testingServer';
+import router from './__ignore__/router';
 
-describe('router', () => {
-  beforeAll(async () => {
-    await testingServer.run(path.resolve(__dirname, './__ignore__'));
-  });
+describe.each`
+  folderPathOrMiddleware
+  ${path.resolve(__dirname, './__ignore__')}
+  ${router}
+`(
+  'router with folderPathOrMiddleware = $folderPathOrMiddleware',
+  ({
+    folderPathOrMiddleware,
+  }: {|
+    folderPathOrMiddleware: string | middlewareType<>,
+  |}) => {
+    beforeAll(async () => {
+      await testingServer.run(folderPathOrMiddleware);
+    });
 
-  test.each`
-    url      | expected
-    ${'/'}   | ${'/ {}'}
-    ${'/id'} | ${'/id {"id":"id"}'}
-  `(
-    'fetch $url',
-    async ({ url, expected }: {| url: string, expected: string |}) => {
+    test.each`
+      url      | expected
+      ${'/'}   | ${'/ {}'}
+      ${'/id'} | ${'/id {"id":"id"}'}
+    `(
+      'fetch $url',
+      async ({ url, expected }: {| url: string, expected: string |}) => {
+        expect(
+          await testingServer
+            .fetch(url)
+            .then((res: fetchResultType) => res.text()),
+        ).toBe(expected);
+      },
+    );
+
+    test('not found', async () => {
       expect(
         await testingServer
-          .fetch(url)
-          .then((res: fetchResultType) => res.text()),
-      ).toBe(expected);
-    },
-  );
+          .fetch('/a/notFound')
+          .then((res: fetchResultType) => res.status),
+      ).toBe(404);
+    });
 
-  test('not found', async () => {
-    expect(
-      await testingServer
-        .fetch('/a/notFound')
-        .then((res: fetchResultType) => res.status),
-    ).toBe(404);
-  });
-
-  afterAll(() => {
-    testingServer.close();
-  });
-});
+    afterAll(() => {
+      testingServer.close();
+    });
+  },
+);

04165005c635453410cf3afa8b112f11ae4fe4f4
HsuTing <hsuting0106@gmail.com>
2020-11-05 13:28:57 +0800

    feat(graphql): add prefix checking

--- server/graphql/src/index.js
+++ server/graphql/src/index.js
@@ -1,5 +1,11 @@
 // @flow
 
+import url from 'url';
+import {
+  type IncomingMessage as IncomingMessageType,
+  type ServerResponse as ServerResponseType,
+} from 'http';
+
 import {
   graphqlHTTP,
   type OptionsData as OptionsDataType,
@@ -15,11 +21,30 @@ export type graphqlType = middlewareType<{}, resType>;
 
 /**
  * @param {string} folderPath - folder path
+ * @param {string} prefix - pathname prefix
  * @param {OptionsDataType} options - graphql middleware options
  *
  * @return {graphqlType} - graphqlmiddleware
  */
 export default (
   folderPath: string,
+  prefix?: string,
   options?: $Diff<OptionsDataType, {| schema: mixed |}>,
-): graphqlType => graphqlHTTP({ ...options, schema: schema(folderPath) });
+): graphqlType => (
+  req: IncomingMessageType,
+  res: ServerResponseType & resType,
+) => {
+  const { pathname } = url.parse(req.url);
+
+  if (
+    !pathname ||
+    !prefix ||
+    !new RegExp(`^${prefix.replace(/^([^/])/, '/$1')}`).test(pathname)
+  ) {
+    res.statusCode = 404;
+    res.end();
+    return;
+  }
+
+  graphqlHTTP({ ...options, schema: schema(folderPath) })(req, res);
+};

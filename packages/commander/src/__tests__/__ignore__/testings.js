7f8f2603c198bc5327298241367ec8ef89b17bb1
HsuTing <hsuting0106@gmail.com>
2020-11-09 13:28:41 +0800

    test(commander): add more testings

--- packages/commander/src/__tests__/__ignore__/testings.js
+++ packages/commander/src/__tests__/__ignore__/testings.js
@@ -16,6 +16,64 @@ export default ([
       description: 'description',
     },
     [],
-    ['_command'],
+    [{}],
+  ],
+  [
+    {
+      name: 'commander <args>',
+      version: '1.0.0',
+      description: 'description',
+    },
+    ['args'],
+    ['args', {}],
+  ],
+  [
+    {
+      name: 'commander',
+      version: '1.0.0',
+      description: 'description',
+      options: [
+        {
+          flags: '-o <option>',
+          description: 'option',
+        },
+      ],
+    },
+    ['-o', 'option'],
+    [{ o: 'option' }],
+  ],
+  [
+    {
+      name: 'commander',
+      version: '1.0.0',
+      description: 'description',
+      commands: {
+        command: {
+          description: 'description',
+        },
+      },
+    },
+    ['command'],
+    ['command'],
+  ],
+  [
+    {
+      name: 'commander',
+      version: '1.0.0',
+      description: 'description',
+      commands: {
+        command: {
+          description: 'description',
+          options: [
+            {
+              flags: '-o <option>',
+              description: 'option',
+            },
+          ],
+        },
+      },
+    },
+    ['command', '-o', 'option'],
+    ['command', { o: 'option' }],
   ],
 ]: $ReadOnlyArray<testingType>);

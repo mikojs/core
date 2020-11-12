68c97868c8f6622452adf2a1eef54b79d9f419ae
HsuTing <hsuting0106@gmail.com>
2020-11-12 14:12:49 +0800

    chore(miko): use new cli

--- packages/miko/src/bin/index.js
+++ packages/miko/src/bin/index.js
@@ -4,19 +4,17 @@
 import path from 'path';
 
 import ora from 'ora';
-import chalk from 'chalk';
 import debug from 'debug';
-import execa from 'execa';
+import chalk from 'chalk';
 
 import { handleUnhandledRejection, createLogger } from '@mikojs/utils';
 import buildWorker from '@mikojs/worker';
 
-import getMikoOptions from 'utils/getMikoOptions';
+import parseArgv from 'utils/parseArgv';
 import generateFiles from 'utils/generateFiles';
-import { type commandsType } from 'utils/getCommands';
-import getExecaArguments from 'utils/getExecaArguments';
 
 import typeof * as workerType from 'worker';
+import commands from 'commands';
 
 const logger = createLogger('@mikojs/miko', ora({ discardStdin: false }));
 const debugLog = debug('miko:bin');
@@ -24,14 +22,12 @@ const debugLog = debug('miko:bin');
 handleUnhandledRejection();
 
 (async () => {
-  const { type, keep = false, getCommands } = await getMikoOptions(
-    process.argv,
-  );
+  const [type, { keep = false }, rawArgs = []] = await parseArgv(process.argv);
   const worker = await buildWorker<workerType>(
     path.resolve(__dirname, '../worker/index.js'),
   );
 
-  debugLog({ type, keep, getCommands });
+  debugLog({ type, keep, rawArgs });
   logger.start('Running');
 
   switch (type) {
@@ -40,36 +36,11 @@ handleUnhandledRejection();
       logger.succeed('Done.');
       break;
 
-    case 'command':
+    case 'generate':
       await worker.addTracking(process.pid, generateFiles());
 
-      const commands = getCommands?.() || [[]];
-
-      debugLog(commands);
-      logger.info(
-        chalk`{gray Run command: ${commands
-          .map((command: $ElementType<commandsType, number>) =>
-            command.join(' '),
-          )
-          .join(' && ')}}`,
-      );
-
-      try {
-        await commands.reduce(
-          (
-            result: Promise<void>,
-            command: $ElementType<commandsType, number>,
-          ) => result.then(() => execa(...getExecaArguments(command))),
-          Promise.resolve(),
-        );
-      } catch (e) {
-        debugLog(e);
-        process.exit(e.code || 1);
-      }
-      break;
-
-    default:
-      if (keep) {
+      if (!keep) logger.succeed('Done.');
+      else {
         let count: number = 0;
 
         logger.info(chalk`{gray Use ctrl + c to stop.}`);
@@ -77,17 +48,30 @@ handleUnhandledRejection();
           logger.start(
             `Running${[].constructor
               .apply({}, new Array(count))
-              .map(() => '.')
+              .fill('.')
               .join('')}`,
           );
           count = count + 1 > 3 ? 0 : count + 1;
         }, 500);
       }
+      break;
+
+    default:
+      const { info, run } = commands(
+        typeof type === 'string' ? type : [type],
+        rawArgs,
+      );
 
       await worker.addTracking(process.pid, generateFiles());
+      debugLog(info);
+      logger.info(chalk`{gray Run command: ${info}}`);
 
-      if (!keep) logger.succeed('Done.');
-
+      try {
+        await run();
+      } catch (e) {
+        debugLog(e);
+        process.exit(1);
+      }
       break;
   }
 })();

323729dc247466c1604419c1828e8e2c1f395aa9
HsuTing <hsuting0106@gmail.com>
2020-11-12 13:31:24 +0800

    feat(miko): add commands

--- packages/miko/src/bin/new.js
+++ packages/miko/src/bin/new.js
@@ -1,12 +1,19 @@
 #! /usr/bin/env node
 // @flow
 
+import path from 'path';
+
 import ora from 'ora';
 import debug from 'debug';
+import chalk from 'chalk';
 
 import { handleUnhandledRejection, createLogger } from '@mikojs/utils';
+import buildWorker from '@mikojs/worker';
 
 import parseArgv from 'utils/parseArgv';
+import generateFiles from 'utils/generateFiles';
+
+import typeof * as workerType from 'worker';
 
 const logger = createLogger('@mikojs/miko', ora({ discardStdin: false }));
 const debugLog = debug('miko:bin');
@@ -14,8 +21,41 @@ const debugLog = debug('miko:bin');
 handleUnhandledRejection();
 
 (async () => {
-  const result = await parseArgv(process.argv);
+  const [type, { keep = false }] = await parseArgv(process.argv);
+  const worker = await buildWorker<workerType>(
+    path.resolve(__dirname, '../worker/index.js'),
+  );
 
-  debugLog(result);
+  debugLog({ type });
   logger.start('Running');
+
+  switch (type) {
+    case 'kill':
+      await worker.killAllEvents();
+      logger.succeed('Done.');
+      break;
+
+    case 'generate':
+      await worker.addTracking(process.pid, generateFiles());
+
+      if (!keep) logger.succeed('Done.');
+      else {
+        let count: number = 0;
+
+        logger.info(chalk`{gray Use ctrl + c to stop.}`);
+        setInterval(() => {
+          logger.start(
+            `Running${[].constructor
+              .apply({}, new Array(count))
+              .fill('.')
+              .join('')}`,
+          );
+          count = count + 1 > 3 ? 0 : count + 1;
+        }, 500);
+      }
+      break;
+
+    default:
+      break;
+  }
 })();

e8384302b819953da61e940349afd9d83722bb8a
HsuTing <hsuting0106@gmail.com>
2020-11-13 09:58:35 +0800

    fix(graphql): use new parseArgv

--- server/graphql/src/bin/index.js
+++ server/graphql/src/bin/index.js
@@ -1,10 +1,20 @@
 // @flow
 
-import parseArgv from '@mikojs/server/lib/parseArgv';
+import parseArgv, {
+  type defaultOptionsType,
+} from '@mikojs/server/lib/parseArgv';
 
 import graphql from '../index';
 import { version } from '../../package.json';
 
-parseArgv('graphql', version, graphql, process.argv).catch(() => {
+parseArgv(
+  'graphql',
+  (defaultOptions: defaultOptionsType) => ({
+    ...defaultOptions,
+    version,
+  }),
+  graphql,
+  process.argv,
+).catch(() => {
   process.exit(1);
 });

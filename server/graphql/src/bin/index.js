579e217b24815e24ffec62216fd33c8ac1ea4d5b
HsuTing <hsuting0106@gmail.com>
2020-11-21 19:19:08 +0800

    fix(graphql): fix cli error

--- server/graphql/src/bin/index.js
+++ server/graphql/src/bin/index.js
@@ -7,6 +7,7 @@ import { printSchema } from 'graphql';
 import { relayCompiler } from 'relay-compiler';
 import { type Config as ConfigType } from 'relay-compiler/bin/RelayCompilerMain.js.flow';
 
+import { handleUnhandledRejection } from '@mikojs/utils';
 import server from '@mikojs/server';
 import parseArgv, {
   type defaultOptionsType,
@@ -18,36 +19,38 @@ import { version } from '../../package.json';
 
 import relayCompilerCommand from './relayCompiler';
 
+handleUnhandledRejection();
+
 (async () => {
-  try {
-    const result = await parseArgv<
-      {},
-      resType,
-      ['relay-compiler', string, ConfigType],
-    >(
-      'graphql',
-      (defaultOptions: defaultOptionsType) => ({
-        ...defaultOptions,
-        version,
-        commands: {
-          ...defaultOptions.commands,
-          'relay-compiler': relayCompilerCommand,
-        },
-      }),
-      graphql,
-      process.argv,
-    );
-
-    if (!result || result instanceof http.Server) return;
-
-    const [, sourcePath, config] = result;
-    const getCache = server.mergeDir(sourcePath, undefined, buildCache);
-
-    relayCompiler({
-      ...config,
-      schema: printSchema(getCache()),
-    });
-  } catch (e) {
+  const result = await parseArgv<
+    {},
+    resType,
+    ['relay-compiler', string, ConfigType],
+  >(
+    'graphql',
+    (defaultOptions: defaultOptionsType) => ({
+      ...defaultOptions,
+      version,
+      commands: {
+        ...defaultOptions.commands,
+        'relay-compiler': relayCompilerCommand,
+      },
+    }),
+    graphql,
+    process.argv,
+  ).catch(() => {
     process.exit(1);
-  }
+  });
+
+  if (!result || result instanceof http.Server) return;
+
+  const [, sourcePath, config] = result;
+  const getCache = server.mergeDir(sourcePath, undefined, buildCache);
+  const close = await server.ready();
+
+  relayCompiler({
+    ...config,
+    schema: printSchema(getCache()),
+  });
+  close();
 })();

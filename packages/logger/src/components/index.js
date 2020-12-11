1c805da136b41c63c8217a1437977dbaba985cb2
HsuTing <hsuting0106@gmail.com>
2020-12-11 13:39:26 +0800

    feat(logger): add divider

--- packages/logger/src/components/index.js
+++ packages/logger/src/components/index.js
@@ -2,6 +2,7 @@
 
 import React, { type AbstractComponent as AbstractComponentType } from 'react';
 import { Text } from 'ink';
+import Divider from 'ink-divider';
 
 export type eventType = 'success' | 'fail';
 export type propsType = {|
@@ -16,7 +17,11 @@ export type propsType = {|
 /** @react logger */
 const Logger = ({ logs }: propsType) =>
   Object.keys(logs).map((name: string) => (
-    <Text key={name}>{JSON.stringify(logs[name])}</Text>
+    <React.Fragment key={name}>
+      <Divider title={name} width={100} />
+
+      <Text>{JSON.stringify(logs[name])}</Text>
+    </React.Fragment>
   ));
 
 export default (React.memo<propsType>(

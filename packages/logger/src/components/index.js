b30b0846101041cd765033580ff77778cdc7c8c1
HsuTing <hsuting0106@gmail.com>
2020-12-16 13:14:13 +0800

    fix(logger): should show first logger name when logger are not only one

--- packages/logger/src/components/index.js
+++ packages/logger/src/components/index.js
@@ -17,19 +17,23 @@ export type propsType = {|
 
 /** @react logger */
 const Logger = ({ logs }: propsType) =>
-  Object.keys(logs).map((name: string, index: number) => (
-    <React.Fragment key={name}>
-      {index === 0 ? null : <Divider title={name} width={100} />}
+  Object.keys(logs).map(
+    (name: string, index: number, names: $ReadOnlyArray<string>) => (
+      <React.Fragment key={name}>
+        {index === 0 && names.length <= 1 ? null : (
+          <Divider title={name} width={100} />
+        )}
 
-      {logs[name].messages.map(
-        (messageProps: messagePropsType, messageIndex: number) => (
-          <Message {...messageProps} key={messageIndex} />
-        ),
-      )}
+        {logs[name].messages.map(
+          (messageProps: messagePropsType, messageIndex: number) => (
+            <Message {...messageProps} key={messageIndex} />
+          ),
+        )}
 
-      {!logs[name].loading ? null : <Loading message={logs[name].loading} />}
-    </React.Fragment>
-  ));
+        {!logs[name].loading ? null : <Loading message={logs[name].loading} />}
+      </React.Fragment>
+    ),
+  );
 
 export default (React.memo<propsType>(
   Logger,

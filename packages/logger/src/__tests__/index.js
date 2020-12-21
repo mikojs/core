d4f77b5dbb4823ae845572a091d6f360c7d3062e
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2020-12-21 23:18:45 +0800

    (@mikojs/logger) Add debug logger (#716)
    * feat(logger): add debug log
    
    * test(logger): add debug testing
    
    * test(logger): fix debug testing

--- packages/logger/src/__tests__/index.js
+++ packages/logger/src/__tests__/index.js
@@ -6,25 +6,42 @@ import chalk from 'chalk';
 import createLogger, { cache } from '../index';
 
 describe('logger', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     cache.render = render;
     cache.messages = [];
+    delete process.env.DEBUG;
   });
 
-  test('could log message', () => {
-    const logger = createLogger('logger');
+  test.each`
+    debug
+    ${undefined}
+    ${'logger:*'}
+  `(
+    'could log message with process.env.DEBUG = $debug',
+    ({ debug }: {| debug?: string |}) => {
+      const logger = createLogger('logger:debug');
 
-    logger.start('start');
-    logger.info('info');
-    logger.warn('warn');
-    logger.log('log');
-    logger.success('success');
-    logger.error('error');
+      if (debug) process.env.DEBUG = debug;
+      else delete process.env.DEBUG;
 
-    expect(cache.instance?.lastFrame()).toBe(chalk`{blue ⅰ }logger info
-{yellow ⅰ }logger warn
+      logger.start('start');
+      logger.info('info');
+      logger.warn('warn');
+      logger.debug('debug');
+      logger.log('log');
+      logger.success('success');
+      logger.error('error');
+
+      expect(cache.instance?.lastFrame()).toBe(chalk`{blue ⅰ }logger info
+{yellow ⅰ }logger warn${
+        !debug
+          ? ''
+          : `
+  logger:debug debug`
+      }
   logger log
 {green ✓ }logger success
 {red ✘ }logger error`);
-  });
+    },
+  );
 });

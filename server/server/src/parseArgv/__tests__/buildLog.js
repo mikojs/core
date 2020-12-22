e2ea6eb47db519a030bebfb90aee038eafdd4448
HsuTing <hsuting0106@gmail.com>
2020-12-23 01:26:02 +0800

    test(@mikojs/server): add testing error

--- server/server/src/parseArgv/__tests__/buildLog.js
+++ server/server/src/parseArgv/__tests__/buildLog.js
@@ -1,28 +1,27 @@
 // @flow
 
 import createLogger from '@mikojs/logger';
+import testing from '@mikojs/logger/lib/testing';
 
 import buildLog from '../buildLog';
 
 describe('build log', () => {
+  beforeEach(() => {
+    testing.reset();
+  });
+
   test.each`
     data
     ${'done'}
     ${true}
     ${false}
   `('run with data = $data', ({ data }: {| data: 'done' | boolean |}) => {
-    const mockLog = jest.fn();
-
-    global.console = {
-      log: mockLog,
-      info: mockLog,
-    };
     buildLog(
       data === 'done' ? data : 'update',
       'server',
       createLogger('server'),
     )({ exists: Boolean(data), filePath: './', pathname: '/' });
 
-    expect(mockLog).toHaveBeenCalled();
+    expect(testing.getInstance()?.lastFrame()).not.toBe('');
   });
 });

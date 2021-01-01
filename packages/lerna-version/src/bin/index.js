e987268fabcbc5c8af31bb389ed0a4c56c76b507
HsuTing <hsuting0106@gmail.com>
2021-01-01 23:23:57 +0800

    feat(@mikojs/lerna-version): could add changelog

--- packages/lerna-version/src/bin/index.js
+++ packages/lerna-version/src/bin/index.js
@@ -1,2 +1,46 @@
 #! /usr/bin/env node
 // @flow
+
+import fs from 'fs';
+import path from 'path';
+
+import chalk from 'chalk';
+import Changelog from 'lerna-changelog/lib/changelog';
+import { load as loadConfig } from 'lerna-changelog/lib/configuration';
+// TODO import execa from 'execa';
+
+import { handleUnhandledRejection } from '@mikojs/utils';
+import commander from '@mikojs/commander';
+
+import { version } from '../../package.json';
+
+handleUnhandledRejection();
+
+const parseArgv = commander<[string]>({
+  name: 'lerna-version',
+  version,
+  description: chalk`generate {green CHANGELOG.md} with {green lerna version}`,
+  args: '<version>',
+});
+
+(async () => {
+  const [nextVersion] = await parseArgv(process.argv);
+  const config = loadConfig({
+    nextVersionFromMetadata: version,
+  });
+  const changelog = await new Changelog({
+    ...config,
+    nextVersion,
+  }).createMarkdown();
+  const changelogFilePath = path.resolve('CHANGELOG.md');
+
+  fs.writeFileSync(
+    changelogFilePath,
+    fs.readFileSync(changelogFilePath, 'utf-8').replace(
+      /# CHANGELOG/,
+      `$1
+
+${changelog}`,
+    ),
+  );
+})();

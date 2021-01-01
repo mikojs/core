6e7bf4752af2bfcf3142a6f75a59d038c172c4b4
HsuTing <hsuting0106@gmail.com>
2021-01-01 13:40:22 +0800

    chore(@mikojs/configs): use main branch

--- packages/configs/src/__tests__/withLerna.js
+++ packages/configs/src/__tests__/withLerna.js
@@ -23,8 +23,8 @@ describe('with lerna', () => {
     ${'flow-typed:restore-cache'} | ${'true'}  | ${'echo "no cache"'}
     ${'flow-typed:save-cache'}    | ${'true'}  | ${`rm -rf ${targetFolder} && cp -r ${sourceFolder} ${targetFolder}`}
     ${'flow-typed:restore-cache'} | ${'true'}  | ${`rm -rf ${sourceFolder} && cp -r ${targetFolder} ${sourceFolder}`}
-    ${'dev'}                      | ${'true'}  | ${'lerna exec "miko babel -w" --parallel --stream --since master'}
-    ${'husky:pre-commit'}         | ${'true'}  | ${'miko build --since master && miko flow --since master && lint-staged'}
+    ${'dev'}                      | ${'true'}  | ${'lerna exec "miko babel -w" --parallel --stream --since main'}
+    ${'husky:pre-commit'}         | ${'true'}  | ${'miko build --since main && miko flow --since main && lint-staged'}
     ${'clean'}                    | ${'true'}  | ${`lerna exec 'rm -rf lib flow-typed/npm' --parallel && lerna exec 'rm -rf .flowconfig' --parallel --ignore @mikojs/core && lerna clean && rm -rf ./.changelog`}
   `(
     'run miko with cliName = $cliName',

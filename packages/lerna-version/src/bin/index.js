563248159fa9b55d81b4a4ca4f9a0c4789cc65a7
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2021-01-01 23:43:44 +0800

    (@mikojs/lerna-version) Add new package (#726)
    * feat(@mikojs/lerna-version): add new package
    
    * feat(@mikojs/lerna-version): use importError to avoid importing
    
    * feat(@mikojs/lerna-version): add cli file
    
    * feat(@mikojs/lerna-version): could add changelog
    
    * feat(@mikojs/lerna-version): add lerna version command

---
+++ packages/lerna-version/src/bin/index.js
@@ -0,0 +1,57 @@
+#! /usr/bin/env node
+// @flow
+
+import fs from 'fs';
+import path from 'path';
+
+import chalk from 'chalk';
+import Changelog from 'lerna-changelog/lib/changelog';
+import { load as loadConfig } from 'lerna-changelog/lib/configuration';
+import execa from 'execa';
+
+import { handleUnhandledRejection } from '@mikojs/utils';
+import commander from '@mikojs/commander';
+
+import { version } from '../../package.json';
+
+handleUnhandledRejection();
+
+const parseArgv = commander<[string]>({
+  name: 'lerna-version',
+  version,
+  description: chalk`generate {green CHANGELOG.md} with {green lerna version}`,
+  args: '<version>',
+});
+
+(async () => {
+  const [nextVersion] = await parseArgv(process.argv);
+  const config = loadConfig({
+    nextVersionFromMetadata: version,
+  });
+  const changelog = await new Changelog({
+    ...config,
+    nextVersion,
+  }).createMarkdown();
+  const changelogFilePath = path.resolve('CHANGELOG.md');
+
+  fs.writeFileSync(
+    changelogFilePath,
+    fs.readFileSync(changelogFilePath, 'utf-8').replace(
+      /# CHANGELOG/,
+      `$1
+
+${changelog}`,
+    ),
+  );
+
+  await [
+    ['git', 'add', changelogFilePath],
+    ['git', 'commit', '-m', 'chore(root): add CHANGELOG.md'],
+    ['lerna', 'version', nextVersion],
+  ].reduce(async (result: Promise<void>, commands: $ReadOnlyArray<string>) => {
+    await result;
+    await execa(commands[0], commands.slice(1), {
+      stdio: 'inherit',
+    });
+  }, Promise.resolve());
+})();

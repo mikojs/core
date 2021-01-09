2e044aeeed2106bf846db01b5c01a00599a89f41
HsuTing <hsuting0106@gmail.com>
2021-01-09 23:48:32 +0800

    feat(@mikojs/lerna-link-bin): add commander

--- lerna/link-bin/src/bin/index.js
+++ lerna/link-bin/src/bin/index.js
@@ -4,7 +4,18 @@
 import { getPackagesSync } from '@lerna/project';
 import symlinkBinary from '@lerna/symlink-binary';
 
-(() => {
+import commander from '@mikojs/commander';
+
+import { version } from '../../package.json';
+
+const parseArgv = commander<[]>({
+  name: 'lerna-link-bin',
+  version,
+  description: 'Link bin files in the monorepo.',
+});
+
+(async () => {
+  await parseArgv(process.argv);
   getPackagesSync().forEach((pkg: {| rootPath: string |}) => {
     symlinkBinary(pkg, pkg.rootPath);
   });

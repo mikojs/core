9eed3e82d336479044affeb238954e4ae8b8c27b
HsuTing <hsuting0106@gmail.com>
2021-01-10 16:25:24 +0800

    feat(@mikojs/lerna-flow-typed): add link command

--- lerna/flow-typed/src/bin/index.js
+++ lerna/flow-typed/src/bin/index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 // @flow
 
+import path from 'path';
+
 import chalk from 'chalk';
 
 import { handleUnhandledRejection } from '@mikojs/utils';
@@ -8,41 +10,57 @@ import commander from '@mikojs/commander';
 
 import { version } from '../../package.json';
 
-import store from 'utils/store';
+import link from 'utils/link';
 import remove from 'utils/remove';
+import store from 'utils/store';
 
 handleUnhandledRejection();
 
 const parseArgv = commander<
-  [
-    | {
-        help: () => void,
-      }
-    | 'store'
-    | 'restore'
-    | 'remove',
-  ],
+  | [
+      | {
+          help: () => void,
+        }
+      | 'store'
+      | 'restore'
+      | 'remove',
+    ]
+  | ['link', ?string],
 >({
   name: 'lerna-flow-typed',
   version,
   description: chalk`Handle the all {green flow-typed} folders in the monorepo.`,
   commands: {
+    link: {
+      description: chalk`Link {green .flowconfig} in the each package.`,
+      args: '[flowconfig]',
+    },
+    remove: {
+      description: chalk`Remove linked {green .flowconfig} in the each pacakge`,
+    },
     store: {
       description: chalk`Store the all {green flow-typed} folders to the cache directory.`,
     },
     restore: {
       description: chalk`Restore the all {green flow-typed} folders from the cache directory.`,
     },
-    remove: {
-      description: chalk`Remove linked {green .flowconfig} in the each pacakge`,
-    },
   },
 });
 
 (async () => {
-  const [type] = await parseArgv(process.argv);
+  const result = await parseArgv(process.argv);
 
-  switch (type) {
+  switch (result[0]) {
+    case 'link':
+      if (result.length === 1) return;
+
+      await link(path.resolve(result[1] || '.flowconfig'));
+      break;
+
+    case 'remove':
+      await remove();
+      break;
+
     case 'store':
       await store();
       break;
@@ -51,12 +69,8 @@ const parseArgv = commander<
       await store(true);
       break;
 
-    case 'remove':
-      await remove();
-      break;
-
     default:
-      type.help();
+      result[0].help();
       break;
   }
 })();

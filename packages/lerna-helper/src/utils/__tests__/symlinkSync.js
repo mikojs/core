c20b0bdd64dce8b5f9fcdd6e9f91d927d9ec2fb5
HsuTing <hsuting0106@gmail.com>
2021-02-08 14:30:30 +0800

    test(@mikojs/lerna-helper): fix testing error

--- packages/lerna-helper/src/utils/__tests__/symlinkSync.js
+++ packages/lerna-helper/src/utils/__tests__/symlinkSync.js
@@ -6,10 +6,15 @@ import rimraf from 'rimraf';
 
 import symlinkSync from '../symlinkSync';
 
+import init from './__ignore__/init';
+
+jest.mock('fs');
+
 test('symlink sync', async () => {
-  const filePath = path.resolve(__dirname, '../../../.flowconfig');
+  const filePath = path.resolve('./.flowconfig');
   const errorMessage = 'Remove folder error.';
 
+  init();
   rimraf.mockImplementation((_: string, callback: (err: mixed) => void) =>
     callback(new Error(errorMessage)),
   );

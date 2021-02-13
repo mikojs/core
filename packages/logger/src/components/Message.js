8718f45ccef834e796eebb2941909ad8fa0d3e0f
HsuTing <hsuting0106@gmail.com>
2021-02-13 15:09:18 +0800

    perf(@mikojs/logger): modify logger code style

--- packages/logger/src/components/Message.js
+++ packages/logger/src/components/Message.js
@@ -6,31 +6,27 @@ import Spinner from 'ink-spinner';
 
 export type propsType = {|
   name: string,
-  color: string,
   event: 'loading' | 'success' | 'error' | 'info' | 'warn' | 'log',
+  color: string,
   message: string,
 |};
 
 const symbols = {
-  loading: (
-    <>
-      <Spinner type="dots" />{' '}
-    </>
-  ),
-  success: <Text color="green">&#10003; </Text>,
-  error: <Text color="red">&#10008; </Text>,
-  info: <Text color="blue">&#8560; </Text>,
-  warn: <Text color="yellow">&#8560; </Text>,
-  log: <Text>{'  '}</Text>,
+  loading: <Spinner type="dots" />,
+  success: <Text color="green">&#10003;</Text>,
+  error: <Text color="red">&#10008;</Text>,
+  info: <Text color="blue">&#8560;</Text>,
+  warn: <Text color="yellow">&#8560;</Text>,
+  log: <Text> </Text>,
 };
 
 /** @react show message */
-const Message = ({ name, color, event, message }: propsType) => (
+const Message = ({ name, event, color, message }: propsType) => (
   <Box>
     <Text>
       {symbols[event]}
 
-      <Text color={color}>{name} </Text>
+      <Text color={color}> {name} </Text>
 
       {message}
     </Text>

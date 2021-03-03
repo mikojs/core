f71f91702ac8c18f675a34064fea0615a9eda0ad
HsuTing <hsuting0106@gmail.com>
2021-03-03 13:25:39 +0800

    perf(@mikojs/commander): support miko config in parsing arguments

--- packages/commander/src/__tests__/index.js
+++ packages/commander/src/__tests__/index.js
@@ -17,7 +17,20 @@ describe('commander', () => {
         ...argv,
       ]);
 
-      expect(result.slice(-1)[0]).toMatchObject(expected.slice(-1)[0]);
+      expect(result.slice(-1)[0]).toMatchObject({
+        ...expected.slice(-1)[0],
+        miko: Object.keys(configs).reduce(
+          (subResult: $ElementType<testingType, 0>, key: string) =>
+            ['name', 'version', 'commands'].includes(key)
+              ? subResult
+              : {
+                  ...subResult,
+                  [key]: configs[key],
+                },
+          // $FlowFixMe FIXME: https://github.com/facebook/flow/issues/5332
+          ({}: $ElementType<testingType, 0>),
+        ),
+      });
       expect(result.slice(0, -1)).toEqual(expected.slice(0, -1));
     },
   );

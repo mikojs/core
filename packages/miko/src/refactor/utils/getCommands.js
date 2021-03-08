eff76702a02f88ccacbbb9101329e9c44b6d96cb
HsuTing <hsuting0106@gmail.com>
2021-03-08 13:19:25 +0800

    feat(@mikojs/miko): could parse subCommand

--- packages/miko/src/refactor/utils/getCommands.js
+++ packages/miko/src/refactor/utils/getCommands.js
@@ -5,16 +5,38 @@ import crypto from 'crypto';
 export type commandsType = $ReadOnlyArray<$ReadOnlyArray<string>>;
 
 /**
+ * @param {commandsType} commands - commands array
+ *
+ * @return {string} - command string
+ */
+const getCommandStr = (commands: commandsType) =>
+  commands.map((str: $ReadOnlyArray<string>) => str.join(' ')).join(' && ');
+
+/**
  * @param {string} commandStr - command string
  *
  * @return {commandsType} - commands array
  */
-const getCommands = (commandStr: string): commandsType =>
-  commandStr
-    .replace(/["'].+['"]/, (str: string, p1: string) =>
-      str.replace(p1, crypto.createHash('md5').update(p1).digest('hex')),
-    )
+const getCommands = (commandStr: string): commandsType => {
+  const [patternStr, patternCommand] = commandStr.match(/["'](.+)['"]/) || [];
+  const hash = !patternStr
+    ? ''
+    : crypto.createHash('md5').update(patternStr).digest('hex');
+
+  return (!patternStr ? commandStr : commandStr.replace(patternStr, hash))
     .split('[ ]*&&[ ]*')
-    .map((str: string) => str.split(/[ ]+/));
+    .map((str: string) =>
+      str
+        .split(/[ ]+/)
+        .map((subStr: string) =>
+          subStr !== hash
+            ? subStr
+            : patternStr.replace(
+                patternCommand,
+                getCommandStr(getCommands(patternCommand)),
+              ),
+        ),
+    );
+};
 
 export default getCommands;

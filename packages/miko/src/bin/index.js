561c0195ed0e07e36ab475750f65f43fcb2ee52b
HsuTing <hsuting0106@gmail.com>
2021-03-27 18:57:26 +0800

    refactor(@mikojs/miko): remove not used commands

--- packages/miko/src/bin/index.js
+++ packages/miko/src/bin/index.js
@@ -31,42 +31,21 @@ handleUnhandledRejection();
   const worker = await buildWorker<workerType>(
     path.resolve(__dirname, '../worker/index.js'),
   );
+  const { info, run } = commands(
+    configs,
+    typeof type === 'string' ? type : [type],
+    rawArgs,
+  );
 
   logger.debug({ type, keep, rawArgs });
   refactorGenerateFiles();
+  await worker.addTracking(process.pid, generateFiles());
+  logger.info(chalk`Run command: {gray ${info}}.`);
 
-  switch (type) {
-    case 'kill':
-      await worker.killAllEvents();
-      logger.success('Successfully killed the server.');
-      break;
-
-    case 'generate':
-      await worker.addTracking(process.pid, generateFiles());
-
-      if (!keep) logger.success('Successfully generated the files.');
-      else {
-        logger.info(chalk`Use {green ctrl + c} to stop.`);
-        logger.start('Server is running.');
-      }
-      break;
-
-    default:
-      const { info, run } = commands(
-        configs,
-        typeof type === 'string' ? type : [type],
-        rawArgs,
-      );
-
-      await worker.addTracking(process.pid, generateFiles());
-      logger.info(chalk`Run command: {gray ${info}}.`);
-
-      try {
-        await run();
-      } catch (e) {
-        logger.debug(e);
-        process.exit(1);
-      }
-      break;
+  try {
+    await run();
+  } catch (e) {
+    logger.debug(e);
+    process.exit(1);
   }
 })();

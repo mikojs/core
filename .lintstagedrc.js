543328af2427b4e7e0d92bd3121ff2854367ee3f
HsuTing <hsuting0106@gmail.com>
2021-04-01 12:48:16 +0800

    feat(@mikojs/core): add prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  '*.js': ['prettier --write'],
   '*.md': ['prettier --write --parser markdown'],
   '**/package.json': [
     'prettier-package-json --write',
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: [
-    '@babel/env',
-    '@babel/flow',
-  ],
+  presets: ['@babel/env', '@babel/flow'],
 };

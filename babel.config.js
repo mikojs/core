4fe2b7231f87db999fbab1f2ff612b872f555e8a
HsuTing <hsuting0106@gmail.com>
2021-04-05 15:36:26 +0800

    feat(@mikojs/core): add core-js and babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  presets: ['@babel/env'],
+  presets: [
+    [
+      '@babel/env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
+  ],
   plugins: ['add-module-exports'],
+  ignore:
+    process.env.NODE_ENV === 'test'
+      ? []
+      : ['**/__tests__/**', '**/__mocks__/**'],
 };

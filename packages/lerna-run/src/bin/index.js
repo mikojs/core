e807033f8158589a536785f8d64dc4a6dfe2c289
HsuTing <hsuting0106@gmail.com>
2021-04-07 00:26:20 +0800

    feat(@mikojs/lerna-run): add commander

--- packages/lerna-run/src/bin/index.js
+++ packages/lerna-run/src/bin/index.js
@@ -1,1 +1,24 @@
 #! /usr/bin/env node
+
+import chalk from 'chalk';
+
+import commander from '@mikojs/commander';
+
+import { version } from '../../package.json';
+
+commander({
+  name: '@mikojs/lerna-run',
+  version,
+  description: chalk`Here are some useful commands for {green monorepo}.`,
+  commands: {
+    'link-flow': {
+      description: chalk`Link {green .flowconfig}, {green dependencies} and {green devDependencies} in each package.`,
+      options: [
+        {
+          flags: '--remove',
+          description: 'Remove linked files in each package.',
+        },
+      ],
+    },
+  },
+}).parse();

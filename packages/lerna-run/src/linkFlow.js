8e41375ee2c24aefe9fcee6e637be22d9fcb2ba0
HsuTing <hsuting0106@gmail.com>
2021-04-07 00:42:53 +0800

    feat(@mikojs/lerna-run): add link-flow function

---
+++ packages/lerna-run/src/linkFlow.js
@@ -0,0 +1,45 @@
+import path from 'path';
+
+import { getPackagesSync } from '@lerna/project';
+
+import symlinkSync from './utils/symlinkSync';
+
+export default async remove => {
+  const packages = getPackagesSync();
+
+  await Promise.all(
+    packages.map(
+      async ({
+        rootPath,
+        location,
+        dependencies,
+        devDependencies,
+        peerDependencies,
+      }) => {
+        await symlinkSync(
+          path.resolve(rootPath, './.flowconfig'),
+          path.resolve(location, './.flowconfig'),
+          remove,
+        );
+        await Promise.all(
+          [dependencies, devDependencies].reduce(
+            (result, data) => [
+              ...result,
+              ...Object.keys(data || {}).map(async key => {
+                const pkg = packages.find(({ name }) => name === key);
+
+                await symlinkSync(
+                  pkg?.location ||
+                    path.resolve(rootPath, './node_modules', key),
+                  path.resolve(location, './node_modules', key),
+                  remove,
+                );
+              }),
+            ],
+            [],
+          ),
+        );
+      },
+    ),
+  );
+};

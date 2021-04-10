e7dcd7e91518626b08d439f164d1cffcf8347512
HsuTing <hsuting0106@gmail.com>
2021-04-11 01:49:43 +0800

    feat(@mikojs/miko): add bin file

--- packages/miko/src/bin/index.js
+++ packages/miko/src/bin/index.js
@@ -1,11 +1,27 @@
 #! /usr/bin/env node
 
-import commander from '@mikojs/commander';
+import execa from 'execa';
+import { cosmiconfigSync } from 'cosmiconfig';
 
-import { version } from '../../package.json';
+import getParseArgv from '../getParseArgv';
+import getCommands from '../getCommands';
 
-commander({
-  name: '@mikojs/miko',
-  version,
-  description: 'Use a simple config to manage commands.',
-}).parse();
+(async () => {
+  const commands = await getCommands(
+    process.argv,
+    getParseArgv({
+      commands: cosmiconfigSync('miko').search()?.config,
+    }),
+  );
+
+  try {
+    await commands.reduce(async (result, command) => {
+      await result;
+      await execa(command[0], command.slice(1), {
+        stdio: 'inherit',
+      });
+    }, Promise.resolve());
+  } catch (e) {
+    process.exit(1);
+  }
+})();

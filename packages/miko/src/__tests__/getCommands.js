5d3bbfdfb0f78f5d97e9375b2517766d56b6656f
HsuTing <hsuting0106@gmail.com>
2021-04-10 21:45:29 +0800

    perf(@mikojs/miko): simplify getCommands function

--- packages/miko/src/__tests__/getCommands.js
+++ packages/miko/src/__tests__/getCommands.js
@@ -1,4 +1,21 @@
+import commander from '@mikojs/commander';
+
 import getCommands from '../getCommands';
+
+const parseArgv = argv =>
+  new Promise(resolve => {
+    commander({
+      name: 'name',
+      description: 'description',
+      exitOverride: true,
+      commands: {
+        command: {
+          description: 'description',
+          action: () => resolve('command'),
+        },
+      },
+    }).parse(argv);
+  });
 
 describe('get commands', () => {
   test.each`
@@ -7,7 +24,7 @@ describe('get commands', () => {
     ${'miko command && miko command'}                | ${[['command'], ['command']]}
     ${'miko command "miko command"'}                 | ${[['command', 'command']]}
     ${'miko command "miko command && miko command"'} | ${[['command', 'command && command']]}
-  `('$commandStr', ({ commandStr, expected }) => {
-    expect(getCommands(commandStr, () => 'command')).toEqual(expected);
+  `('$commandStr', async ({ commandStr, expected }) => {
+    expect(await getCommands(commandStr, parseArgv)).toEqual(expected);
   });
 });
--- packages/miko/src/getCommands.js
+++ packages/miko/src/getCommands.js
@@ -1,5 +1,18 @@
 import stringArgv from 'string-argv';
 
-const getCommands = (commandStr, parseArgv) => stringArgv(commandStr);
+const getCommands = (commandStr, parseArgv) =>
+  stringArgv(commandStr).reduce(async (subResult, key, index, allKeys) => {
+    const result = await subResult;
+    const command = [...result.slice(-1)[0], ...(key === '&&' ? [] : [key])];
+
+    return [
+      ...result.slice(0, -1),
+      ...(command[0] !== 'miko' ||
+      (key !== '&&' && allKeys.length - 1 !== index)
+        ? [command]
+        : await getCommands(await parseArgv(['node', ...command]), parseArgv)),
+      ...(key === '&&' ? [[]] : []),
+    ];
+  }, Promise.resolve([[]]));
 
 export default getCommands;

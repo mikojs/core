8444f5df0f537e7fc1dcbb8e1c0b54a59576d71e
HsuTing <hsuting0106@gmail.com>
2021-04-11 21:37:25 +0800

    perf(@mikojs/miko): remove addNode

--- packages/miko/src/getCommands.js
+++ packages/miko/src/getCommands.js
@@ -13,44 +13,41 @@ const commandsToString = commands =>
     )
     .join(' && ');
 
-const addNode = command =>
-  command[0] === 'miko' ? ['node', ...command] : ['node', 'miko', ...command];
-
 const getCommands = async (argv, parseArgv) =>
-  (await parseArgv(argv)).reduce(async (promiseResult, key, index, keys) => {
-    const result = await promiseResult;
-    const [command] = result.slice(-1);
+  argv[0] !== 'miko'
+    ? argv
+    : (await parseArgv(['node', ...argv])).reduce(
+        async (promiseResult, key, index, keys) => {
+          const result = await promiseResult;
+          const [command] = result.slice(-1);
 
-    if (key !== '&&')
-      command.push(
-        key === 'miko' || !/miko/.test(key)
-          ? key
-          : commandsToString(
-              await key
-                .split(/[ ]+&&[ ]+/)
-                .reduce(
-                  async (promiseSubResult, subKey) => [
-                    ...(await promiseSubResult),
-                    ...(await getCommands(
-                      addNode(stringArgv(subKey)),
-                      parseArgv,
-                    )),
-                  ],
-                  Promise.resolve([]),
-                ),
-            ),
+          if (key !== '&&')
+            command.push(
+              key === 'miko' || !/miko/.test(key)
+                ? key
+                : commandsToString(
+                    await key
+                      .split(/[ ]+&&[ ]+/)
+                      .reduce(
+                        async (promiseSubResult, subKey) => [
+                          ...(await promiseSubResult),
+                          ...(await getCommands(stringArgv(subKey), parseArgv)),
+                        ],
+                        Promise.resolve([]),
+                      ),
+                  ),
+            );
+
+          return [
+            ...result.slice(0, -1),
+            ...(command[0] !== 'miko' ||
+            (key !== '&&' && keys.length - 1 !== index)
+              ? [command]
+              : await getCommands(command, parseArgv)),
+            ...(key === '&&' ? [[]] : []),
+          ];
+        },
+        Promise.resolve([[]]),
       );
-
-    return [
-      ...result.slice(0, -1),
-      ...(command[0] !== 'miko' || (key !== '&&' && keys.length - 1 !== index)
-        ? [command]
-        : await getCommands(
-            addNode(await parseArgv(addNode(command))),
-            parseArgv,
-          )),
-      ...(key === '&&' ? [[]] : []),
-    ];
-  }, Promise.resolve([[]]));
 
 export default getCommands;

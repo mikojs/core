154e6cf2ee1e1e4f1a68930030a9f455d3164117
HsuTing <hsuting0106@gmail.com>
2021-04-13 15:27:27 +0800

    style(@mikojs/miko): modify code style

--- packages/miko/src/spliceEnv.js
+++ packages/miko/src/spliceEnv.js
@@ -1,18 +1,16 @@
 export default originalArgv => {
   const { env, argv } = originalArgv.reduce(
-    (result, key) => {
-      if (result.isEnv && /^[^ ]+=/.test(key))
-        return {
-          ...result,
-          env: [...result.env, key],
-        };
-
-      return {
-        ...result,
-        isEnv: false,
-        argv: [...result.argv, key],
-      };
-    },
+    (result, key) =>
+      result.isEnv && /^[^ ]+=/.test(key)
+        ? {
+            ...result,
+            env: [...result.env, key],
+          }
+        : {
+            ...result,
+            isEnv: false,
+            argv: [...result.argv, key],
+          },
     {
       env: [],
       argv: [],

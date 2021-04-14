355fe0eb1d673375a2c95c20f261fdba35d7b81c
HsuTing <hsuting0106@gmail.com>
2021-04-14 14:25:18 +0800

    ci(@mikojs/core): add .github workflows

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   '*.js': ['miko lint prettier', 'prettier --write'],
   '*.md': ['prettier --write --parser markdown'],
   '*.json': ['prettier --write --parser json'],
+  '*.yml': ['prettier --write --parser yaml'],
   'package.json': [
     'prettier-package-json --write',
     'prettier --write --parser json',

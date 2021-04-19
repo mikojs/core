168d2ed0cf7ed4844fd71a3b84f13d464d5044a0
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2021-04-20 01:04:38 +0800

    (@mikojs/core) Remove lerna-run link-bin command (#787)
    * fix(@mikojs/core): use shell
    
    * feat(@mikojs/core): add build.sh arguments
    
    * refactor(@mikojs/lerna-run): remove link-bin
    
    * feat(@mikojs/commander): merge transform function

--- packages/commander/src/index.js
+++ packages/commander/src/index.js
@@ -1,12 +1,42 @@
 import commander from 'commander';
 
-import transform from './transform';
+const OPTIONS = [
+  'version',
+  'description',
+  'arguments',
+  'allowUnknownOption',
+  'exitOverride',
+  'options',
+  'requiredOptions',
+  'action',
+];
 
 const addConfg = (prevProgram, config) => {
-  const program = transform(config).reduce(
-    (result, [key, ...args]) => result[key](...args),
-    prevProgram,
-  );
+  const program = OPTIONS.reduce((result, key) => {
+    const value = config[key];
+
+    if (!value) return result;
+
+    if (['options', 'requiredOptions'].includes(key))
+      return value.reduce(
+        (subResult, { flags, description }) =>
+          subResult[key.replace(/s$/, '')](flags, description),
+        result,
+      );
+
+    return (
+      {
+        version: () =>
+          result.version(value, '-v --version', 'Output the version number.'),
+        description: () =>
+          result
+            .description(value)
+            .helpOption('-h, --help', 'Display help for command.'),
+        arguments: () => result.arguments(value),
+        action: () => result.action(value),
+      }[key]?.() || result[key]()
+    );
+  }, prevProgram);
 
   Object.keys(config.commands || {}).forEach(key =>
     addConfg(program.command(key), config.commands[key]),

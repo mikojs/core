be94a262cc6263dcba136db965e14984975b7aa0
HsuTing <hsuting0106@gmail.com>
2021-04-21 13:11:41 +0800

    feat(@mikojs/plugin-workspaces-link-flowconfig): could link packages

--- plugins/plugin-workspaces-link-flowconfig/src/index.js
+++ plugins/plugin-workspaces-link-flowconfig/src/index.js
@@ -13,17 +13,33 @@ export default {
         workspaces,
       }) => {
         await Promise.all(
-          workspaces.reduce((result, { cwd }) => {
-            if (cwd === rootCwd) return result;
+          workspaces.reduce(
+            (result, { cwd, manifest: { dependencies, devDependencies } }) =>
+              cwd === rootCwd
+                ? result
+                : [
+                    ...result,
+                    symlinkSync(
+                      path.resolve(rootCwd, './.flowconfig'),
+                      path.resolve(cwd, './.flowconfig'),
+                    ),
+                    ...[
+                      ...dependencies.values(),
+                      ...devDependencies.values(),
+                    ].map(({ identHash, scope, name }) => {
+                      const pkgName = !scope ? name : `@${scope}/${name}`;
 
-            return [
-              ...result,
-              symlinkSync(
-                path.resolve(rootCwd, './.flowconfig'),
-                path.resolve(cwd, './.flowconfig'),
-              ),
-            ];
-          }, []),
+                      return symlinkSync(
+                        workspaces.find(
+                          ({ locator }) => locator.identHash === identHash,
+                        )?.cwd ||
+                          path.resolve(rootCwd, './node_modules', pkgName),
+                        path.resolve(cwd, './node_modules', pkgName),
+                      );
+                    }),
+                  ],
+            [],
+          ),
         );
       },
     },

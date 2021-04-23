c19ccb4ca40b40007cb8514b79391473544351bf
HsuTing <hsuting0106@gmail.com>
2021-04-23 13:56:37 +0800

    fix(@mikojs/plugin-flow-typed-link): fix command error

--- plugins/plugin-flow-typed-link/src/index.js
+++ plugins/plugin-flow-typed-link/src/index.js
@@ -7,31 +7,31 @@ import symlinkSync from './symlinkSync';
 
 export default {
   name,
-  factory: () => ({}),
-  commands: [
-    class Link extends BaseCommand {
-      static paths = [['flow-typed', 'link']];
+  factory: () => ({
+    commands: [
+      class Link extends BaseCommand {
+        static paths = [['flow-typed', 'link']];
 
-      static usage = {
-        description,
-        details: `
-        `,
-        examples: [],
-      };
+        static usage = {
+          description,
+          details: `
+          `,
+          examples: [],
+        };
 
-      execute = async () => {
-        const configuration = await Configuration.find(
-          this.context.cwd,
-          this.context.plugins,
-        );
-        const { project, workspace } = await Project.find(
-          configuration,
-          this.context.cwd,
-        );
+        execute = async () => {
+          const configuration = await Configuration.find(
+            this.context.cwd,
+            this.context.plugins,
+          );
+          const { project, workspace } = await Project.find(
+            configuration,
+            this.context.cwd,
+          );
 
-        // TODO
-        symlinkSync(project, workspace);
-      };
-    },
-  ],
+          symlinkSync(project, workspace);
+        };
+      },
+    ],
+  }),
 };

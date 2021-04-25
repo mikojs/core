e1ed2331714d7d0dc3131e8722d300a9717be7f2
HsuTing <hsuting0106@gmail.com>
2021-04-25 20:03:19 +0800

    fix(@mikojs/core): throw warning when yarn plugins execute fail

--- .yarn/plugins/@mikojs/require.js
+++ .yarn/plugins/@mikojs/require.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const { warn } = console;
+
 module.exports = filePath => {
   const name = [
     '@mikojs',
@@ -9,6 +11,18 @@ module.exports = filePath => {
   try {
     return require(name);
   } catch (e) {
+    if (
+      !new RegExp(
+        `Cannot find module '${path.resolve(
+          __dirname,
+          '../../../node_modules',
+          name,
+          './lib/index.js',
+        )}'`,
+      ).test(e.message)
+    )
+      warn(e.message);
+
     return {
       name,
       factory: () => ({}),

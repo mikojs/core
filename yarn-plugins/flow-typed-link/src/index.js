e9bd1c217a5bec11c0c1ec3e087e399bbed03173
HsuTing <hsuting0106@gmail.com>
2021-04-26 15:32:18 +0800

    fix(@mikojs/yarn-plugin-flow-typed-link): fix output object error

--- yarn-plugins/flow-typed-link/src/index.js
+++ yarn-plugins/flow-typed-link/src/index.js
@@ -4,57 +4,54 @@ import { BaseCommand } from '@yarnpkg/cli';
 import { Configuration, Project } from '@yarnpkg/core';
 import { Command } from 'clipanion';
 
-import { name, description } from '../package.json';
+import { description } from '../package.json';
 
 import symlinkSync from './symlinkSync';
 
 /* eslint new-cap: ['error', { capIsNewExceptionPattern: 'Command' }] */
 export default {
-  name,
-  factory: () => ({
-    commands: [
-      class Link extends BaseCommand {
-        static usage = Command.Usage({
-          description,
-          details: `
-          `,
-          examples: [],
-        });
+  commands: [
+    class Link extends BaseCommand {
+      static usage = Command.Usage({
+        description,
+        details: `
+        `,
+        examples: [],
+      });
 
-        @Command.Path('flow-typed', 'link')
-        execute = async () => {
-          const { cwd, plugins } = this.context;
-          const configuration = await Configuration.find(cwd, plugins);
-          const {
-            project: { workspaces },
-            workspace: {
-              manifest: { dependencies, devDependencies },
+      @Command.Path('flow-typed', 'link')
+      execute = async () => {
+        const { cwd, plugins } = this.context;
+        const configuration = await Configuration.find(cwd, plugins);
+        const {
+          project: { workspaces },
+          workspace: {
+            manifest: { dependencies, devDependencies },
+          },
+        } = await Project.find(configuration, cwd);
+        const { projectCwd } = configuration;
+
+        if (projectCwd === cwd) return;
+
+        await symlinkSync(
+          path.resolve(projectCwd, './.flowconfig'),
+          path.resolve(cwd, './.flowconfig'),
+        );
+        await Promise.all(
+          [...dependencies.values(), ...devDependencies.values()].map(
+            ({ identHash, scope, name }) => {
+              const pkgName = !scope ? name : `@${scope}/${name}`;
+
+              return symlinkSync(
+                workspaces.find(
+                  ({ locator }) => locator.identHash === identHash,
+                )?.cwd || path.resolve(projectCwd, './node_modules', pkgName),
+                path.resolve(cwd, './node_modules', pkgName),
+              );
             },
-          } = await Project.find(configuration, cwd);
-          const { projectCwd } = configuration;
-
-          if (projectCwd === cwd) return;
-
-          await symlinkSync(
-            path.resolve(projectCwd, './.flowconfig'),
-            path.resolve(cwd, './.flowconfig'),
-          );
-          await Promise.all(
-            [...dependencies.values(), ...devDependencies.values()].map(
-              ({ identHash, scope, name }) => {
-                const pkgName = !scope ? name : `@${scope}/${name}`;
-
-                return symlinkSync(
-                  workspaces.find(
-                    ({ locator }) => locator.identHash === identHash,
-                  )?.cwd || path.resolve(projectCwd, './node_modules', pkgName),
-                  path.resolve(cwd, './node_modules', pkgName),
-                );
-              },
-            ),
-          );
-        };
-      },
-    ],
-  }),
+          ),
+        );
+      };
+    },
+  ],
 };

486f7ad6d8c7c80391da6ce95a05f606b0dc353c
HsuTing <hsuting0106@gmail.com>
2021-04-26 10:42:56 +0800

    fix(@mikojs/plugin-prettier-package-json): fix testing fail

--- plugins/plugin-prettier-package-json/src/__tests__/index.js
+++ plugins/plugin-prettier-package-json/src/__tests__/index.js
@@ -1,17 +1,12 @@
-import path from 'path';
-
+import { Configuration, Project } from '@yarnpkg/core';
 import { execute } from '@yarnpkg/shell';
 
 import pluginPrettierPackageJson from '..';
 
 test('plugin prettier package json', async () => {
-  await pluginPrettierPackageJson.factory().hooks.afterAllInstalled({
-    workspaces: [
-      {
-        cwd: path.resolve(__dirname, '../..'),
-      },
-    ],
-  });
+  await pluginPrettierPackageJson.hooks.afterAllInstalled(
+    new Project(__dirname, new Configuration(__dirname)),
+  );
 
   expect(execute).toHaveBeenCalledTimes(1);
 });

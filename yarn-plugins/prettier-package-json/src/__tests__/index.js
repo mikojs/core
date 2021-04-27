6250cc6ee68c5eeed48880020f6f7ab2cb7158c4
HsuTing <hsuting0106@gmail.com>
2021-04-27 10:19:13 +0800

    test(@mikojs/yarn-plugin-prettier-package-json): add more testing

--- yarn-plugins/prettier-package-json/src/__tests__/index.js
+++ yarn-plugins/prettier-package-json/src/__tests__/index.js
@@ -1,12 +1,21 @@
 import { Configuration, Project } from '@yarnpkg/core';
 import { execute } from '@yarnpkg/shell';
+import pluginPnp from '@yarnpkg/plugin-pnp';
 
 import prettierPackageJson from '..';
 
 test('prettier package json', async () => {
-  await prettierPackageJson.hooks.afterAllInstalled(
-    new Project(__dirname, new Configuration(__dirname)),
-  );
+  const cwd = process.cwd();
+  const modules = new Map();
+  const plugins = new Set();
+
+  modules.set('@yarnpkg/plugin-pnp', pluginPnp);
+  plugins.add('@yarnpkg/plugin-pnp');
+
+  const configuration = await Configuration.find(cwd, { modules, plugins });
+  const { project } = await Project.find(configuration, cwd);
+
+  await prettierPackageJson.hooks.afterAllInstalled(project);
 
   expect(execute).toHaveBeenCalledTimes(1);
 });

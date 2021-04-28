3156d3145df73374c8043450bc16bc18534617ee
HsuTing <hsuting0106@gmail.com>
2021-04-28 09:32:49 +0800

    feat(@mikojs/yarn-plugin-utils): could auto add tsconfig to build yarn plugin

--- yarn-plugins/utils/src/index.js
+++ yarn-plugins/utils/src/index.js
@@ -1,5 +1,43 @@
-import Build from './commands/build';
+import fs from 'fs';
+import path from 'path';
+
+import { BaseCommand as Command } from '@yarnpkg/cli';
+
+import findBuilder from './utils/findBuilder';
 
 export default {
-  commands: [Build],
+  commands: [
+    class Build extends Command {
+      static usage = Command.Usage({
+        category: 'Plugin-related commands',
+        description: 'build yarn plugins without tsconfig.json',
+        details: `
+          Tsconfig.json is required for @yarnpkg/builder, but it is not needed for plugins which does not use typescript. This command would generate a tsconfig.json before building plugin. Tsconfig.json would be removed after building finished.
+        `,
+        examples: [['Build yarn plugin', 'yarn plugin build']],
+      });
+
+      @Command.Path('plugin', 'build')
+      execute = async () => {
+        const { cwd } = this.context;
+        const tsconfigFilePath = path.resolve(cwd, './tsconfig.json');
+
+        fs.writeFileSync(
+          tsconfigFilePath,
+          JSON.stringify(
+            {
+              compilerOptions: {
+                allowJs: true,
+              },
+              include: ['src/**/*'],
+            },
+            null,
+            2,
+          ),
+        );
+        await this.cli.run(['node', findBuilder(), 'build', 'plugin']);
+        fs.unlinkSync(tsconfigFilePath);
+      };
+    },
+  ],
 };

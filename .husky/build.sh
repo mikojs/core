149d4ead1b3fc17ef795fb557b58fd50ea124317
HsuTing <hsuting0106@gmail.com>
2021-05-03 17:39:27 +0800

    perf(@mikojs/core): remove build.sh

--- .husky/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-yarn lerna exec \
-  "babel src -d lib --delete-dir-on-start --verbose --root-mode upward" \
-  --scope @mikojs/miko \
-  --scope @mikojs/eslint-config-miko \
-  --stream \
-  --include-dependencies \
-  $@

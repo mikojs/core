b068a2af5e40129b0214b53f668be6585b0e4556
HsuTing <hsuting0106@gmail.com>
2021-05-06 13:29:56 +0800

    perf(@mikojs/yarn-plugin-utils): remove mockStdout

--- yarn-plugins/miko/src/__tests__/index.js
+++ yarn-plugins/miko/src/__tests__/index.js
@@ -1,11 +1,10 @@
-import { generateCli, mockStdout } from '@mikojs/yarn-plugin-utils/src/testing';
+import { generateCli } from '@mikojs/yarn-plugin-utils/src/testing';
 
 import testings from './__ignore__/testings';
 
 describe('miko', () => {
   beforeEach(() => {
     jest.resetModules();
-    mockStdout.write.mockClear();
   });
 
   test.each(testings)('%s', async (info, config, args) => {

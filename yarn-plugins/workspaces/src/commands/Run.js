c6aca4b607e3b3e11943a8fcc1b07c5ff2166c4b
HsuTing <hsuting0106@gmail.com>
2021-05-13 15:52:15 +0800

    feat(@mikojs/yarn-plugin-workspaces): could build usage

--- yarn-plugins/workspaces/src/commands/Run.js
+++ yarn-plugins/workspaces/src/commands/Run.js
@@ -2,18 +2,12 @@ import chalk from 'chalk';
 import { BaseCommand as Command } from '@yarnpkg/cli';
 
 import Base from './Base';
+import buildUsage from '../utils/buildUsage';
 
 export default class Run extends Base {
-  static usage = Command.Usage({
-    category: 'Workspace-related commands',
-    description: chalk`run script with {cyan \`@yarnpkg/plugin-workspace-tools\`}`,
-    details: chalk`
-      Because {cyan \`@yarnpkg/plugin-workspace-tools\`} would run script in the all workspaces included the root workspace, the root workspace is not needed for the most case. Using this command would avoid to running script in the root workspace.
-
-      On the other hand, this command also add some helpful options to find the specific workspaces.
-    `,
-    examples: [['Run script', 'yarn workspaces run build']],
-  });
+  static usage = Command.Usage(buildUsage('run script', [
+    ['Run script', 'yarn workspaces run build'],
+  ]));
 
   @Command.Path('workspaces', 'run')
   execute = this.buildExecute('run')

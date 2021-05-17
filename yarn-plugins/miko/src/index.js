b4a13823635170f9758f24be48e79164a21555d6
HsuTing <hsuting0106@gmail.com>
2021-05-18 00:41:52 +0800

    feat(@mikojs/yarn-plugin-miko): could get commands from @mikojs/yarn-plugin-config

--- yarn-plugins/miko/src/index.js
+++ yarn-plugins/miko/src/index.js
@@ -1,6 +1,35 @@
+import { BaseCommand as Command } from '@yarnpkg/cli';
+import stringArgv from 'string-argv';
+
 import loadConfig from './utils/loadConfig';
 
-const getCommands = () => ({});
+const getCommands = (config, prevKey = []) =>
+  Object.keys(config).reduce((result, key) => {
+    const { command, commands = {}, ...usage } = config[key];
+
+    return [
+      ...result,
+      class CustomCommand extends Command {
+        static usage = Command.Usage({
+          ...usage,
+          category: 'Miko-related commands',
+        });
+
+        @Command.Proxy()
+        args = [];
+
+        @Command.Path(...prevKey, key)
+        execute = async () =>
+          this.cli.run([
+            ...stringArgv(
+              typeof command === 'string' ? command : await command(),
+            ),
+            ...this.args,
+          ]);
+      },
+      ...getCommands(commands, [...prevKey, key]),
+    ];
+  }, []);
 
 export default {
   commands: getCommands(loadConfig()),

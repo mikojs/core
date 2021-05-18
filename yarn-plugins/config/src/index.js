e748ba3db6225103e7b97c602355e1d62cb34b85
HsuTing <hsuting0106@gmail.com>
2021-05-19 00:04:52 +0800

    feat(@mikojs/yarn-plugin-config): add clean command

--- yarn-plugins/config/src/index.js
+++ yarn-plugins/config/src/index.js
@@ -1,1 +1,8 @@
-export default {};
+import chalk from 'chalk';
+
+export default {
+  clean: {
+    description: chalk`clean the all files which are ignored by {cyan \`git\`}`,
+    command: 'git clean -dxf',
+  },
+};

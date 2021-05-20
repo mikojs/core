7044db0ffebace2f8b6a908cd604b95512395fb6
HsuTing <hsuting0106@gmail.com>
2021-05-21 01:37:48 +0800

    fix(@mikojs/yarn-plugin-alias): fix local require

--- yarn-plugins/alias/src/utils/loadPlugin.js
+++ yarn-plugins/alias/src/utils/loadPlugin.js
@@ -1,6 +1,11 @@
+import path from 'path';
+
 export default () =>
   [
     () => require('@yarnpkg/plugin-commands'),
+    () => require(
+      path.resolve('./.yarn/plugins/@yarnpkg/plugin-commands'),
+    ).factory(),
     () => ({}),
   ].reduce((result, getPlugin) => {
     try {

4eadce5bc4979e077fe2f52ca63e406de35e96fb
HsuTing <hsuting0106@gmail.com>
2021-05-28 01:17:09 +0800

    feat(@mikojs/babel-preset-base): add default babel config

--- packages/babel-preset-base/src/index.js
+++ packages/babel-preset-base/src/index.js
@@ -1,2 +1,19 @@
-export default {
-};
+import { declare } from '@babel/helper-plugin-utils';
+
+export default declare(
+  ({ assertVersion }) => {
+    assertVersion(7);
+
+    return {
+      presets: [
+        [
+          '@babel/env',
+          {
+            useBuiltIns: 'usage',
+            corejs: 3,
+          },
+        ],
+      ],
+    };
+  },
+);

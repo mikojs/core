e30e16c77f0dc93a998049a5779e9e8b013f1241
HsuTing <hsuting0106@gmail.com>
2021-05-31 23:58:49 +0800

    chore(@mikojs/core): remove not used workspaces

--- packages/commands/src/index.js
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-
-export default {
-  clean: {
-    description: chalk`clean the all files which are ignored by {cyan \`git\`}`,
-    command: 'git clean -dxf',
-  },
-};

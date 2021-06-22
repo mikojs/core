eb8af5a8f62d9ded48daa9b048e53b529cb77d58
HsuTing <hsuting0106@gmail.com>
2021-06-23 01:46:43 +0800

    feat(@mikojs/yarn-plugin-miko): dev command could trigger dev hook

--- yarn-plugins/miko/src/commands/Dev.js
+++ yarn-plugins/miko/src/commands/Dev.js
@@ -1,7 +1,12 @@
 import { BaseCommand as Command } from '@yarnpkg/cli';
+import { Configuration } from '@yarnpkg/core';
 
 export default class Dev extends Command {
   @Command.Path('dev')
   async execute() {
+    const { cwd, plugins } = this.context;
+    const configuration = await Configuration.find(cwd, plugins);
+
+    await configuration.triggerHook(({ dev }) => dev);
   }
 }

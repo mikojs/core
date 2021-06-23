5c67a2a9888fbc6405dac01f8f8b462e1c6367c6
HsuTing <hsuting0106@gmail.com>
2021-06-23 16:58:22 +0800

    feat(@mikojs/yarn-plugin-miko): could get changed files and workspaces

--- yarn-plugins/miko/src/commands/Dev.js
+++ yarn-plugins/miko/src/commands/Dev.js
@@ -1,12 +1,35 @@
 import { BaseCommand as Command } from '@yarnpkg/cli';
-import { Configuration } from '@yarnpkg/core';
+import { Configuration, Project, execUtils, structUtils } from '@yarnpkg/core';
 
 export default class Dev extends Command {
   @Command.Path('dev')
-  async execute() {
+  execute = async () => {
     const { cwd, plugins } = this.context;
     const configuration = await Configuration.find(cwd, plugins);
+    const { projectCwd } = configuration;
+    const { project: { workspaces } } = await Project.find(configuration, projectCwd);
+    const { stdout } = await execUtils.execvp(
+      'git',
+      ['diff', '--name-only'],
+      {
+        cwd: projectCwd,
+        strict: true,
+      },
+    );
+    const changedFiles = stdout.split(/\r?\n/).filter(Boolean);
 
-    await configuration.triggerHook(({ dev }) => dev);
+    await configuration.triggerHook(
+      ({ dev }) => dev,
+      changedFiles,
+      workspaces.reduce(
+        (result, { relativeCwd, locator }) => !changedFiles.some(changedFile => changedFile.startsWith(relativeCwd))
+          ? result
+          : [
+            ...result,
+            structUtils.stringifyIdent(locator),
+          ],
+        [],
+      ),
+    );
   }
 }

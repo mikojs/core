838c5804c83b25c60edfef31d23fae75cb3203d8
HsuTing <hsuting0106@gmail.com>
2021-07-10 23:32:57 +0800

    feat(@mikojs/yarn-plugin-miko): add jest-worker

--- yarn-plugins/miko/src/commands/Build.js
+++ yarn-plugins/miko/src/commands/Build.js
@@ -1,5 +1,6 @@
 import { BaseCommand as Command } from '@yarnpkg/cli';
 import { Configuration, Project, structUtils } from '@yarnpkg/core';
+import { Worker } from 'jest-worker';
 
 export default class Build extends Command {
   @Command.Path('build')
@@ -12,6 +13,7 @@ export default class Build extends Command {
 
     await configuration.triggerHook(
       ({ build }) => build,
+      Worker,
       workspaces.filter(workspace =>
         structUtils.stringifyIdent(workspace.locator) !== structUtils.stringifyIdent(locator)
       ),

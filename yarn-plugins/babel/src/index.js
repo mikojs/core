521fbcc9d32d8087cd9b57e679522130abedac61
HsuTing <hsuting0106@gmail.com>
2021-08-29 00:34:28 +0800

    feat(@mikojs/yarn-plugin-babel): could build with babel

--- yarn-plugins/babel/src/index.js
+++ yarn-plugins/babel/src/index.js
@@ -2,15 +2,25 @@ import { structUtils } from '@yarnpkg/core';
 
 export default {
   hooks: {
-    build: ({ workspaces }) => {
-      workspaces
-        .filter(({ manifest }) =>
-          Array.from(manifest.devDependencies.values())
-            .some(locator => structUtils.stringifyIdent(locator) === '@babel/cli')
-        )
-        .forEach(({ cwd }) => {
-          console.log(cwd);
-        });
+    build: async ({ cli, workspaces }) => {
+      await Promise.all(
+        workspaces
+          .filter(({ manifest }) =>
+            Array.from(manifest.devDependencies.values())
+              .some(locator => structUtils.stringifyIdent(locator) === '@babel/cli')
+          )
+          .map(({ cwd }) => cli.run([
+            'babel',
+            'src',
+            '-d',
+            'lib',
+            '--verbose',
+            '--root-mode',
+            'upward',
+          ], {
+            cwd,
+          }))
+      );
     },
   },
 };

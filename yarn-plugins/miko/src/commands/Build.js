bab1b3eb92e4a2d9f49b0064223321b16837719f
HsuTing <hsuting0106@gmail.com>
2021-08-29 20:52:10 +0800

    refactor(@mikojs/yarn-plugin-miko): remove not used

--- yarn-plugins/miko/src/commands/Build.js
+++ yarn-plugins/miko/src/commands/Build.js
@@ -5,10 +5,9 @@ export default class Build extends Command {
   @Command.Path('build')
   execute = async () => {
     const { cwd, plugins } = this.context;
-    const { run } = this.cli;
     const configuration = await Configuration.find(cwd, plugins);
     const { projectCwd } = configuration;
-    const { project: { workspaces }, locator } = await Project.find(configuration, projectCwd);
+    const { project: { workspaces } } = await Project.find(configuration, projectCwd);
 
     await configuration.triggerHook(
       ({ build }) => build,

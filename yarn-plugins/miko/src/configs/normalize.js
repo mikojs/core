0cba18684d487f0429a317b1ea88a706152427f4
HsuTing <hsuting0106@gmail.com>
2021-09-23 00:06:50 +0800

    feat(@mikojs/yarn-plugin-miko): could normalize configs

--- yarn-plugins/miko/src/configs/normalize.js
+++ yarn-plugins/miko/src/configs/normalize.js
@@ -1,1 +1,16 @@
-export default configs => [];
+const normalize = (config, alias) =>
+  (config instanceof Array
+    ? config
+    : [config]
+  ).map(
+    configStrOrObj => alias[configStrOrObj] || configStrOrObj
+  );
+
+export default ({
+  alias = {},
+  scripts = {},
+}) => Object.keys(scripts)
+  .reduce((result, key) => [
+    ...result,
+    [key, normalize(scripts[key], alias)],
+  ], []);

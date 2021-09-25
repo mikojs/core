52745506ba304ed273ed1f364cf7cc35aebe7c09
HsuTing <hsuting0106@gmail.com>
2021-09-25 23:34:54 +0800

    refactor(@mikojs/yarn-plugin-miko): remove alias

--- yarn-plugins/miko/src/scripts/normalize.js
+++ yarn-plugins/miko/src/scripts/normalize.js
@@ -1,24 +1,4 @@
-import invariant from 'fbjs/lib/invariant';
-
-const aliasToTasks = alias =>
-  Object.keys(alias).reduce((result, key) => {
-    const task =
-      typeof alias[key] !== 'string' ? alias[key] : { command: alias[key] };
-
-    [
-      [!(task instanceof Array), `\`config.alias.${key}\` can\`t be an array.`],
-      [task.command, `\`config.alias.${key}.command\` is required.`],
-    ].forEach(invariantArgv => {
-      invariant(...invariantArgv);
-    });
-
-    return {
-      ...result,
-      [key]: task,
-    };
-  }, {});
-
-const normalizeScripts = (scripts, tasks) =>
+export default scripts =>
   Object.keys(scripts).reduce(
     (result, key) => [
       ...result,
@@ -40,6 +20,3 @@ const normalizeScripts = (scripts, tasks) =>
     ],
     [],
   );
-
-export default ({ alias = {}, scripts = {} }) =>
-  normalizeScripts(scripts, aliasToTasks(alias));

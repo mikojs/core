1c5a7787682dbdc691895cb5719285adaffa51aa
HsuTing <hsuting0106@gmail.com>
2021-09-26 18:22:38 +0800

    refactor(@mikojs/yarn-plugin-miko): remove json config

--- yarn-plugins/miko/src/utils/loadConfigs.js
+++ yarn-plugins/miko/src/utils/loadConfigs.js
@@ -6,18 +6,12 @@ const baseFs = new NodeFS();
 const loadConfigs = cwd =>
   cwd === '/'
     ? {}
-    : ['./.mikorc.js', './miko.config.js', './miko.json'].reduce(
-        (result, configName) => {
-          const configPath = ppath.resolve(
-            cwd,
-            npath.toPortablePath(configName),
-          );
+    : ['./.mikorc.js', './miko.config.js'].reduce((result, configName) => {
+        const configPath = ppath.resolve(cwd, npath.toPortablePath(configName));
 
-          return result || !baseFs.existsSync(configPath)
-            ? result
-            : miscUtils.dynamicRequire(configPath);
-        },
-        null,
-      ) || loadConfigs(ppath.resolve(cwd, '..'));
+        return result || !baseFs.existsSync(configPath)
+          ? result
+          : miscUtils.dynamicRequire(configPath);
+      }, null) || loadConfigs(ppath.resolve(cwd, '..'));
 
 export default loadConfigs;

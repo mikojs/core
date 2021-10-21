4584e8bd1d4dfe085680d476e3092f30f0dd5892
HsuTing <hsuting0106@gmail.com>
2021-10-21 13:49:30 +0800

    feat(@mikojs/yarn-plugin-babel): could build babel workspace

--- yarn-plugins/babel/src/hooks/build.js
+++ yarn-plugins/babel/src/hooks/build.js
@@ -1,13 +1,31 @@
 import { structUtils } from '@yarnpkg/core';
 
-export default ({ workspaces }) => {
-  workspaces.filter(({ locator }) => {
+export default async ({ cli, workspaces }) => {
+  const babelWorkspaces = workspaces.reduce((result, { locator }) => {
     const name = structUtils.stringifyIdent(locator);
-
-    return (
+    const isBabelWorkspace =
       /^(@(?!babel\/)[^/]+\/)([^/]*babel-(preset|plugin)(?:-|\/|$)|[^/]+\/)/.test(
         name,
-      ) || /^babel-(preset|plugin)/.test(name)
-    );
-  });
+      ) || /^babel-(preset|plugin)/.test(name);
+
+    return !isBabelWorkspace ? result : [...result, name];
+  }, []);
+
+  if (babelWorkspaces.length !== 0)
+    await cli.run([
+      'workspaces',
+      'foreach',
+      '-pAv',
+      '--include',
+      babelWorkspaces.join(','),
+      'exec',
+      'run',
+      'babel',
+      'src',
+      '-d',
+      'lib',
+      '--verbose',
+      '--root-mode',
+      'upward',
+    ]);
 };

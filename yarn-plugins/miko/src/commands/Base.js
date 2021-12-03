9178678f8dc19119850e86b9c161ce673b65171d
HsuTing <hsuting0106@gmail.com>
2021-12-03 13:23:53 +0800

    fix(@mikojs/yarn-plugin-miko): fix project

--- yarn-plugins/miko/src/commands/Base.js
+++ yarn-plugins/miko/src/commands/Base.js
@@ -9,9 +9,8 @@ export default class Base extends Command {
     const { cwd, plugins } = this.context;
     const configuration = await Configuration.find(cwd, plugins);
     const { projectCwd } = configuration;
-    const {
-      project: { workspaces },
-    } = await Project.find(configuration, projectCwd);
+    const { project } = await Project.find(configuration, projectCwd);
+    const { workspaces } = project;
 
     await project.restoreInstallState();
     await configuration.triggerHook(hooks => hooks[name], {

f127574408fef5824db535df6b88ba52e41a8a40
HsuTing <hsuting0106@gmail.com>
2022-02-14 13:52:38 +0800

    feat(@mikojs/yarn-plugin-builder): add builder hook

--- yarn-plugins/builder/src/hooks/build.js
+++ yarn-plugins/builder/src/hooks/build.js
@@ -1,1 +1,38 @@
-export default tasks => {};
+import { scriptUtils } from '@yarnpkg/core';
+
+export default tasks =>
+  tasks.add({
+    title: 'Run builder plugin',
+    task: (_, task) =>
+      task.newListr([
+        {
+          title: 'Preparing builder plugin',
+          task: async ctx => {
+            const { workspaces } = ctx;
+
+            ctx.useBuilderWorkspaces = [];
+
+            await workspaces.reduce(async (result, workspace) => {
+              await result;
+
+              const binaries = await scriptUtils.getWorkspaceAccessibleBinaries(
+                workspace,
+              );
+
+              if (binaries.has('builder'))
+                ctx.useBuilderWorkspaces.push(workspace);
+            }, Promise.resolve());
+          },
+        },
+        {
+          title: 'Building yarn plugins in workspaces',
+          enabled: ({ useBuilderWorkspaces }) =>
+            useBuilderWorkspaces?.length !== 0,
+          task: ({ useBuilderWorkspaces, runWithWorkspaces }) =>
+            runWithWorkspaces(
+              ['builder', 'build', 'plugin'],
+              useBuilderWorkspaces,
+            ),
+        },
+      ]),
+  });

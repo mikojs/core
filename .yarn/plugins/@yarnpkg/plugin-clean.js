92242ee8005925add8012434ba116ab3f6664113
HsuTing <hsuting0106@gmail.com>
2022-02-19 22:58:39 +0800

    style(@mikojs/core): add comment

--- .yarn/plugins/@yarnpkg/plugin-clean.js
+++ .yarn/plugins/@yarnpkg/plugin-clean.js
@@ -1,8 +1,14 @@
+/**
+ * FIXME
+ * https://github.com/arcanis/clipanion/blob/e32ee0143363414f0ef796fac11c954cac14ba48/sources/advanced/options/utils.ts#L7
+ * https://github.com/arcanis/clipanion/blob/e32ee0143363414f0ef796fac11c954cac14ba48/sources/advanced/Cli.ts#L377
+ * Owing to Symbol, this is different between `factory require` and `.yarn/cache`.
+ */
 module.exports = {
   name: '@yarnpkg/plugin-clean',
-  factory: fn => {
-    const { structUtils } = fn('@yarnpkg/core');
-    const { xfs, ppath } = fn('@yarnpkg/fslib');
+  factory: require => {
+    const { structUtils } = require('@yarnpkg/core');
+    const { xfs, ppath } = require('@yarnpkg/fslib');
 
     return {
       hooks: {

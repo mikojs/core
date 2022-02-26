a5308cad0bfbda154d5e2733f9fa4220d383c189
HsuTing <hsuting0106@gmail.com>
2022-02-26 22:51:23 +0800

    docs(@mikojs/yarn-plugin-miko): modify usage

--- yarn-plugins/miko/src/utils/buildUsage.js
+++ yarn-plugins/miko/src/utils/buildUsage.js
@@ -3,12 +3,12 @@ import { Command } from 'clipanion';
 export default paths =>
   Command.Usage({
     category: 'Miko-related commands',
-    description: 'some custom scripts for development life cycle',
+    description: 'run script for development life cycle',
     details: `
-      This commands are from \`@mikojs/yarn-plugin-miko\`.
+      Those commands are based on \`@mikojs/yarn-plugin-miko\`. You could find all commands in \`Examples\`.
     `,
     examples: paths.map(path => [
-      `Run \`${path.join(':')}\` stage in development life cycle`,
+      `Run \`${path.join(' ')}\` stage in development life cycle`,
       `$0 ${path.join(' ')}`,
     ]),
   });
